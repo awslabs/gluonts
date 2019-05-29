4dbd28d12a3af348e156b411b580899e606388c3
Michael Bohlke-Schneider <mschneid83@gmail.com>
2019-05-29 17:42:28 +0200

    Update gluonts/block/encoder.py
    Co-Authored-By: Lorenzo Stella <lostella@users.noreply.github.com>

--- gluonts/block/encoder.py
+++ gluonts/block/encoder.py
@@ -58,7 +58,7 @@ class Seq2SeqEncoder(nn.HybridBlock):
             static code,
             shape (batch_size, num_static_features)
 
-        dynamic_code : Tensor
+        Tensor
             dynamic code,
             shape (batch_size, sequence_length, num_dynamic_features)
         """
