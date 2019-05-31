ba3e49eae0f32ad19ac7d6f6bd86e2d341cf67b5
Sheng Zha <szha@users.noreply.github.com>
2019-05-30 23:38:42 -0700

    use different ga id (#40)

--- docs/_static/google_analytics.js
+++ docs/_static/google_analytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-96378503-8', 'auto');
+  ga('create', 'UA-96378503-16', 'auto');
   ga('send', 'pageview');
