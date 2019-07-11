c11513d6a8319d35e7aa2dceb92bb34fb955e956
Alexander Alexandrov <alxale@amazon.com>
2019-07-11 15:58:36 +0200

    Simplify Prophet wrapper
    - Rename input parameter from `prophet` to `prophet_params`.
    - Remove fallback logic in case of inputs with NaNs.
    - Remove dummy ProphetEstimator class.

--- src/gluonts/model/prophet/__init__.py
+++ src/gluonts/model/prophet/__init__.py
@@ -1,5 +1,4 @@
 # Relative imports
-from ._estimator import ProphetEstimator
 from ._predictor import ProphetPredictor, PROPHET_IS_INSTALLED
 
-__all__ = ['ProphetEstimator', 'ProphetPredictor', 'PROPHET_IS_INSTALLED']
+__all__ = ['ProphetPredictor', 'PROPHET_IS_INSTALLED']
