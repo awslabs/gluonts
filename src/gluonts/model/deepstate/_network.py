a865d7fb866e81bef3669f2d284d15e5ce148b96
Syama Rangapuram <rangapur@amazon.de>
2019-08-06 16:27:27 +0200

    [Deepstate] Add the missing headers

--- src/gluonts/model/deepstate/_network.py
+++ src/gluonts/model/deepstate/_network.py
@@ -1,3 +1,16 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 # Standard library imports
 from typing import List, Optional
 
@@ -49,7 +62,7 @@ class DeepStateNetwork(mx.gluon.HybridBlock):
             )
             self.prior_cov_diag_model = mx.gluon.nn.Dense(
                 units=self.issm.latent_dim(),
-                activation="sigmoid",  # TODO: put explicit upper bound
+                activation="sigmoid",  # TODO: puot explicit upper bound
                 flatten=False,
             )
             self.lstm = mx.gluon.rnn.HybridSequentialRNNCell()
