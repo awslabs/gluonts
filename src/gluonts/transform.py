15e2b4c43344168fe25d56946100d893a8717513
Lorenzo Stella <lorenzostella@gmail.com>
2019-08-06 19:05:42 +0200

    fix mistake

--- src/gluonts/transform.py
+++ src/gluonts/transform.py
@@ -103,7 +103,7 @@ def _shift_timestamp_helper(
         # this line looks innocent, but can create a date which is out of
         # bounds values over year 9999 raise a ValueError
         # values over 2262-04-11 raise a pandas OutOfBoundsDatetime
-        result = ts + offset * ts.freq
+        return ts + offset * ts.freq
     except (ValueError, pd._libs.OutOfBoundsDatetime) as ex:
         raise GluonTSDateBoundsError(ex)
 
