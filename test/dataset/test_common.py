9668f733b61cd77d158b3995fac35ca2423c7dd0
Jasper Schulz <schjaspe@amazon.de>
2019-09-02 15:12:28 +0200

    Fixed ProcessStartField.

--- test/dataset/test_common.py
+++ test/dataset/test_common.py
@@ -23,11 +23,12 @@ from gluonts.dataset.common import ProcessStartField
         ("B", "2019-11-01"),
         ("W", "2019-10-27"),
         ("M", "2019-10-31"),
-        ("Y", "2019"),
+        ("12M", "2019-10-31"),
+        ("A-DEC", "2018-12-31"),
     ],
 )
 def test_process_start_field(freq, expected):
     process = ProcessStartField.process
     given = "2019-11-01 12:34:56"
 
-    assert process(given, freq) == pd.Timestamp(expected)
+    assert process(given, freq) == pd.Timestamp(expected, freq)
