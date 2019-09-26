8bb133a5d53810213c807a68930e0678a0aaac6e
Caner Turkmen <turkmen.ac@gmail.com>
2019-09-26 10:50:00 +0200

    add transform objects for temporal point processes (#294)

--- src/gluonts/transform.py
+++ src/gluonts/transform.py
@@ -25,6 +25,7 @@ import pandas as pd
 from gluonts.core.component import DType, validated
 from gluonts.core.exception import GluonTSDateBoundsError, assert_data_error
 from gluonts.dataset.common import DataEntry, Dataset
+from gluonts.dataset.field_names import FieldName
 from gluonts.dataset.stat import ScaleHistogram
 from gluonts.time_feature import TimeFeature
 from gluonts.runtime_params import GLUONTS_MAX_IDLE_TRANSFORMS
@@ -137,7 +138,9 @@ class UniformSplitSampler(InstanceSampler):
         self.lookup = np.arange(2 ** 13)
 
     def __call__(self, ts: np.ndarray, a: int, b: int) -> np.ndarray:
-        assert a <= b
+        assert (
+            a <= b
+        ), "First index must be less than or equal to the last index."
         while ts.shape[-1] >= len(self.lookup):
             self.lookup = np.arange(2 * len(self.lookup))
         mask = np.random.uniform(low=0.0, high=1.0, size=b - a + 1) < self.p
@@ -156,6 +159,42 @@ class TestSplitSampler(InstanceSampler):
 
     def __call__(self, ts: np.ndarray, a: int, b: int) -> np.ndarray:
         return np.array([b])
+
+
+class ContinuousTimePointSampler:
+    """
+    Abstract class for "continuous time" samplers, which, given a lower bound
+    and upper bound, sample "points" (events) in continuous time from a
+    specified interval.
+    """
+
+    def __init__(self, num_instances: int) -> None:
+        self.num_instances = num_instances
+
+    def __call__(self, a: float, b: float) -> np.ndarray:
+        """
+        Returns random points in the real interval between :code:`a` and
+        :code:`b`.
+
+        Parameters
+        ----------
+        a
+            The lower bound (minimum time value that a sampled point can take)
+        b
+            Upper bound. Must be greater than a.
+        """
+        raise NotImplementedError()
+
+
+class ContinuousTimeUniformSampler(ContinuousTimePointSampler):
+    """
+    Implements a simple random sampler to sample points in the continuous
+    interval between :code:`a` and :code:`b`.
+    """
+
+    def __call__(self, a: float, b: float) -> np.ndarray:
+        assert a <= b, "Interval start time must be before interval end time."
+        return np.random.rand(self.num_instances) * (b - a) + a
 
 
 class ExpectedNumInstanceSampler(InstanceSampler):
@@ -1308,6 +1347,170 @@ class CanonicalInstanceSplitter(FlatMapTransformation):
             )
 
             yield d
+
+
+class ContinuousTimeInstanceSplitter(FlatMapTransformation):
+    """
+    Selects training instances by slicing "intervals" from a continous-time
+    process instantiation. Concretely, the input data is expected to describe an
+    instantiation from a point (or jump) process, with the "target"
+    identifying inter-arrival times and other features (marks), as described
+    in detail below.
+
+    The splitter will then take random points in continuous time from each
+    given observation, and return a (variable-length) array of points in
+    the past (context) and the future (prediction) intervals.
+
+    The transformation is analogous to its discrete counterpart
+    `InstanceSplitter` except that
+
+    - It does not allow "incomplete" records. That is, the past and future
+      intervals sampled are always complete
+    - Outputs a (T, C) layout.
+    - Does not accept `time_series_fields` (i.e., only accepts target fields) as these
+      would typically not be available in TPP data.
+
+    The target arrays are expected to have (2, T) layout where the first axis
+    corresponds to the (i) interarrival times between consecutive points, in
+    order and (ii) integer identifiers of marks (from {0, 1, ..., :code:`num_marks`}).
+    The returned arrays will have (T, 2) layout.
+
+    For example, the array below corresponds to a target array where points with timestamps
+    0.5, 1.1, and 1.5 were observed belonging to categories (marks) 3, 1 and 0
+    respectively: :code:`[[0.5, 0.6, 0.4], [3, 1, 0]]`.
+
+    Parameters
+    ----------
+    past_interval_length
+        length of the interval seen before making prediction
+    future_interval_length
+        length of the interval that must be predicted
+    train_sampler
+        instance sampler that provides sampling indices given a time-series
+    target_field
+        field containing the target
+    start_field
+        field containing the start date of the of the point process observation
+    end_field
+        field containing the end date of the point process observation
+    forecast_start_field
+        output field that will contain the time point where the forecast starts
+    """
+
+    def __init__(
+        self,
+        past_interval_length: float,
+        future_interval_length: float,
+        train_sampler: ContinuousTimePointSampler,
+        target_field: str = FieldName.TARGET,
+        start_field: str = FieldName.START,
+        end_field: str = "end",
+        forecast_start_field: str = FieldName.FORECAST_START,
+    ) -> None:
+
+        assert (
+            future_interval_length > 0
+        ), "Prediction interval must have length greater than 0."
+
+        self.train_sampler = train_sampler
+        self.past_interval_length = past_interval_length
+        self.future_interval_length = future_interval_length
+        self.target_field = target_field
+        self.start_field = start_field
+        self.end_field = end_field
+        self.forecast_start_field = forecast_start_field
+
+    # noinspection PyMethodMayBeStatic
+    def _mask_sorted(self, a: np.ndarray, lb: float, ub: float):
+        start = np.searchsorted(a, lb)
+        end = np.searchsorted(a, ub)
+        return np.arange(start, end)
+
+    def flatmap_transform(
+        self, data: DataEntry, is_train: bool
+    ) -> Iterator[DataEntry]:
+
+        assert data[self.start_field].freq == data[self.end_field].freq
+
+        total_interval_length = (
+            data[self.end_field] - data[self.start_field]
+        ) / data[self.start_field].freq.delta
+
+        # sample forecast start times in continuous time
+        if is_train:
+            if total_interval_length < (
+                self.future_interval_length + self.past_interval_length
+            ):
+                sampling_times: np.ndarray = np.array([])
+            else:
+                sampling_times = self.train_sampler(
+                    self.past_interval_length,
+                    total_interval_length - self.future_interval_length,
+                )
+        else:
+            sampling_times = np.array([total_interval_length])
+
+        ia_times = data[self.target_field][0, :]
+        marks = data[self.target_field][1:, :]
+
+        ts = np.cumsum(ia_times)
+        assert ts[-1] < total_interval_length, (
+            "Target interarrival times provided are inconsistent with "
+            "start and end timestamps."
+        )
+
+        # select field names that will be included in outputs
+        keep_cols = {
+            k: v
+            for k, v in data.items()
+            if k not in [self.target_field, self.start_field, self.end_field]
+        }
+
+        for future_start in sampling_times:
+
+            r: DataEntry = dict()
+
+            past_start = future_start - self.past_interval_length
+            future_end = future_start + self.future_interval_length
+
+            assert past_start >= 0
+
+            past_mask = self._mask_sorted(ts, past_start, future_start)
+
+            past_ia_times = np.diff(np.r_[0, ts[past_mask] - past_start])[
+                np.newaxis
+            ]
+
+            r[f"past_{self.target_field}"] = np.concatenate(
+                [past_ia_times, marks[:, past_mask]], axis=0
+            ).transpose()
+
+            r["past_valid_length"] = np.array([len(past_mask)])
+
+            r[self.forecast_start_field] = (
+                data[self.start_field]
+                + data[self.start_field].freq.delta * future_start
+            )
+
+            if is_train:  # include the future only if is_train
+                assert future_end <= total_interval_length
+
+                future_mask = self._mask_sorted(ts, future_start, future_end)
+
+                future_ia_times = np.diff(
+                    np.r_[0, ts[future_mask] - future_start]
+                )[np.newaxis]
+
+                r[f"future_{self.target_field}"] = np.concatenate(
+                    [future_ia_times, marks[:, future_mask]], axis=0
+                ).transpose()
+
+                r["future_valid_length"] = np.array([len(future_mask)])
+
+            # include other fields
+            r.update(keep_cols.copy())
+
+            yield r
 
 
 class SelectFields(MapTransformation):
