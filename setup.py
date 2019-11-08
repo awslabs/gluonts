773e50aced4275d5c81c4bf7582550adfbea0281
Lorenzo Stella <lorenzostella@gmail.com>
2019-11-08 17:51:29 +0100

    Fixes to setup.py to make it work on Windows

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import distutils.cmd
 import distutils.log
 import itertools
 import logging
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -19,8 +20,8 @@ GPU_SUPPORT = 0 == int(
     subprocess.call(
         "nvidia-smi",
         shell=True,
-        stdout=open("/dev/null", "w"),
-        stderr=open("/dev/null", "w"),
+        stdout=open(os.devnull, "w"),
+        stderr=open(os.devnull, "w"),
     )
 )
 
