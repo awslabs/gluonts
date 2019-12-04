f66c283b6c4d3186f9a5827ba1d8a8faca42f7e7
Michael Bohlke-schneider <bohlkem@amazon.com>
2019-12-04 11:59:56 +0100

    Removed replaced get_granularity with buildin pandas function

--- src/gluonts/model/deepvar/_estimator.py
+++ src/gluonts/model/deepvar/_estimator.py
@@ -19,6 +19,7 @@ import re
 import pandas as pd
 import numpy as np
 from mxnet.gluon import HybridBlock
+from pandas.tseries.frequencies import to_offset
 
 # First-party imports
 from gluonts.core.component import validated
@@ -83,27 +84,9 @@ class FourierDateFeatures(TimeFeature):
         return np.vstack([np.cos(steps), np.sin(steps)])
 
 
-def get_granularity(freq_str: str) -> Tuple[int, str]:
-    """
-    Splits a frequency string such as "7D" into the multiple 7 and the base
-    granularity "D".
-    Parameters
-    ----------
-    freq_str
-        Frequency string of the form [multiple][granularity] such as "12H",
-        "5min", "1D" etc.
-    """
-    freq_regex = r"\s*((\d+)?)\s*([^\d]\w*)"
-    m = re.match(freq_regex, freq_str)
-    assert m is not None, "Cannot parse frequency string: %s" % freq_str
-    groups = m.groups()
-    multiple = int(groups[1]) if groups[1] is not None else 1
-    granularity = groups[2]
-    return multiple, granularity
-
-
 def time_features_from_frequency_str(freq_str: str) -> List[TimeFeature]:
-    multiple, granularity = get_granularity(freq_str)
+    offset = to_offset(freq_str)
+    multiple, granularity = offset.n, offset.name
 
     features = {
         "M": ["weekofyear"],
@@ -126,7 +109,8 @@ def time_features_from_frequency_str(freq_str: str) -> List[TimeFeature]:
 def get_lags_for_frequency(
     freq_str: str, num_lags: Optional[int] = None
 ) -> List[int]:
-    multiple, granularity = get_granularity(freq_str)
+    offset = to_offset(freq_str)
+    multiple, granularity = offset.n, offset.name
 
     if granularity == "M":
         lags = [[1, 12]]
