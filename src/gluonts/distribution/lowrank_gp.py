7530f8efdd07f4277f15d4369996316f1baba86f
Michael Bohlke-schneider <bohlkem@amazon.com>
2019-12-05 21:44:59 +0100

    Included missing headers

--- src/gluonts/distribution/lowrank_gp.py
+++ src/gluonts/distribution/lowrank_gp.py
@@ -1,14 +1,29 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+# Standard library imports
 from collections import OrderedDict
 from typing import Tuple, Optional
 
+# Third-party imports
+from mxnet import gluon
+
+# First-party imports
 from gluonts.distribution.lowrank_multivariate_gaussian import (
     inv_softplus,
     sigma_minimum,
 )
 from gluonts.distribution import LowrankMultivariateGaussian
-
-from mxnet import gluon
-
 from gluonts.core.component import validated
 from gluonts.distribution import bijection
 from gluonts.distribution.distribution_output import (
