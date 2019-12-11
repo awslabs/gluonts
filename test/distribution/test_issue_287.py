d05bc201d694f5be2eaa5eaf20ea4a81c0bcabe9
Aaron Spieler <25365592+AaronSpieler@users.noreply.github.com>
2019-12-11 13:27:17 +0100

    Test changed to .5 instead of close to 1
    Co-Authored-By: Lorenzo Stella <lorenzostella@gmail.com>

--- test/distribution/test_issue_287.py
+++ test/distribution/test_issue_287.py
@@ -30,6 +30,6 @@ def test_issue_287(distr_out_class: DistributionOutput):
     args_proj.initialize(init=mx.init.Constant(-1e2))
     distr_args = args_proj(network_output)
     distr = distr_output.distribution(distr_args)
-    x = mx.nd.array([1.0 - 1e-7])  # open interval (0,1)
+    x = mx.nd.array([0.5])
     ll = distr.log_prob(x)
     assert np.isfinite(ll.asnumpy())
