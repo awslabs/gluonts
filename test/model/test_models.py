f727e79e99516fd6e526ba57ad13286ba1bcd962
Aaron Spieler <aspiele@amazon.com>
2020-01-28 15:42:58 +0100

    Increase timeout for nbeats ensemble model.

--- test/model/test_models.py
+++ test/model/test_models.py
@@ -310,7 +310,6 @@ def deepstate_estimator(hybridize: bool = False, batches_per_epoch=1):
 
 
 @flaky(max_runs=3, min_passes=1)
-@pytest.mark.timeout(30)
 @pytest.mark.parametrize(
     "Estimator, hyperparameters, accuracy",
     [
