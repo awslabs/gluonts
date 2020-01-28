e9bc8c30aff3bd3eb4d51c88ce78d0ef803fa0d1
Aaron Spieler <aspiele@amazon.com>
2020-01-28 15:28:53 +0100

    Increasing timeout for ensemble model.

--- test/model/test_models.py
+++ test/model/test_models.py
@@ -310,6 +310,7 @@ def deepstate_estimator(hybridize: bool = False, batches_per_epoch=1):
 
 
 @flaky(max_runs=3, min_passes=1)
+@pytest.mark.timeout(30)
 @pytest.mark.parametrize(
     "Estimator, hyperparameters, accuracy",
     [
