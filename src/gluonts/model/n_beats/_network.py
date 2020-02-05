More elegant stop_gradient fix for sMAPE.

src/gluonts/model/n_beats/_network.py
@@ -509,7 +509,6 @@ class NBEATSNetwork(mx.gluon.HybridBlock):
             # connect last block
             return forecast + self.net_blocks[-1](backcast)
 
-    # TODO: somehow interpretable mode not learning?
     def smape_loss(self, F, forecast: Tensor, future_target: Tensor) -> Tensor:
         r"""
         .. math::
@@ -519,7 +518,8 @@ class NBEATSNetwork(mx.gluon.HybridBlock):
         According to paper: https://arxiv.org/abs/1905.10437.
         """
 
-        denominator = F.abs(future_target) + F.abs(forecast)
+        # Stop gradient required for stable learning
+        denominator = F.stop_gradient(F.abs(future_target) + F.abs(forecast))
         flag = denominator == 0
 
         smape = (200 / self.prediction_length) * F.mean(