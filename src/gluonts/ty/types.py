cd2b79252aad258e436a1027f73a5cb855724394
Jasper Schulz <schjaspe@amazon.de>
2020-02-24 17:34:51 +0100

    Removed wrongly added files.

--- src/gluonts/ty/types.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Dict, Iterable
-
-# Dictionary used for data flowing through the transformations.
-DataEntry = Dict[str, Any]
-
-# A Dataset is an iterable of DataEntry.
-Dataset = Iterable[DataEntry]
