1cdfced14c38f89b8d9e387aadb9ca5404c05bd2
Ehsan M. Kermani <ehsanmo1367@gmail.com>
2020-02-28 16:13:13 -0800

    Add LSTNet (#596)
    * Add lstnet
    
    Add initial test
    
    Make smoke test pass
    
    Fix reshape bug and parameters validation
    
    Add more tests
    
    Fix unregistered hybrid rnn layer
    
    Add docs
    
    LSTNet scaling support
    
    Scaling and doc
    
    Fix dtype
    
    * Address the review comments
    
    Cleanup
    
    * Support horizon and prediction_length
    
    * Add more tests
    
    * Fix seq pred case and address comments
    
    * Simplify ar highway

---
+++ src/gluonts/model/lstnet/_estimator.py
@@ -0,0 +1,220 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+# Standard library imports
+from typing import Optional
+
+# Third-party imports
+import numpy as np
+from mxnet.gluon import HybridBlock, loss
+
+# First-party imports
+from gluonts.core.component import validated, DType
+from gluonts.model.estimator import GluonEstimator
+from gluonts.model.lstnet._network import LSTNetTrain, LSTNetPredict
+from gluonts.model.predictor import Predictor, RepresentableBlockPredictor
+from gluonts.trainer import Trainer
+from gluonts.dataset.field_names import FieldName
+from gluonts.support.util import copy_parameters
+from gluonts.transform import (
+    Chain,
+    ExpectedNumInstanceSampler,
+    InstanceSplitter,
+    Transformation,
+    AsNumpyArray,
+    AddObservedValuesIndicator,
+)
+
+
+class LSTNetEstimator(GluonEstimator):
+    """
+    Constructs an LSTNet estimator for multivariate time-series data.
+
+    The model has been described in this paper:
+    https://arxiv.org/abs/1703.07015
+
+    Note that this implementation will change over time and we furthre work on
+    this method.
+
+    Parameters
+    ----------
+    freq
+        Frequency of the data to train and predict
+    context_length
+        The maximum number of steps to unroll the RNN for computing the predictions
+        (Note that it is constraints by the Conv1D output size)
+    num_series
+        Number of time-series (variats)
+    skip_size
+        Skip size for the skip RNN layer
+    ar_window
+        Auto-regressive window size for the linear part
+    channels
+        Number of channels for first layer Conv1D
+    kernel_size
+        Kernel size for first layer Conv1D (default: 6)
+    prediction_length
+        Length of the prediction p where given `(y_1, ..., y_t)` the model predicts
+        `(y_{t+1}, ..., y_{t+p})` (default: None)
+    horizon
+        Length of the prediction horizon `h` where given `(y_1, ..., y_t)` the model
+        predicts `y_{t+h}` (default: None)
+        Cannot use both `prediction_length` and `horizon` at the same time
+    trainer
+        Trainer object to be used (default: Trainer())
+    dropout_rate
+        Dropout regularization parameter (default: 0.2)
+    output_activation
+        The last activation to be used for output.
+        Accepts either `None` (default no activation), `sigmoid` or `tahn`
+    rnn_cell_type
+        Type of the RNN cell. Either `lstm` or `gru` (default: `gru`)
+    rnn_num_layers
+        Number of RNN layers to be used
+    skip_rnn_cell_type
+        Type of the RNN cell for the skip layer. Either `lstm` or `gru` (default: `gru`)
+    skip_rnn_num_layers
+        Number of RNN layers to be used for skip part
+    scaling
+        Whether to automatically scale the target values (default: True)
+    dtype
+        Data type (default: np.float32)
+
+    Notes
+    -----
+    `horizon` and `prediction_length` cannot be used at the same time. Only one of them
+    must be provided.
+    """
+
+    @validated()
+    def __init__(
+        self,
+        freq: str,
+        context_length: int,
+        num_series: int,
+        skip_size: int,
+        ar_window: int,
+        channels: int,
+        kernel_size: int = 6,
+        prediction_length: Optional[int] = None,
+        horizon: Optional[int] = None,
+        trainer: Trainer = Trainer(),
+        dropout_rate: Optional[float] = 0.2,
+        output_activation: Optional[str] = None,
+        rnn_cell_type: str = "gru",
+        rnn_num_layers: int = 10,
+        skip_rnn_cell_type: str = "gru",
+        skip_rnn_num_layers: int = 5,
+        scaling: bool = True,
+        dtype: DType = np.float32,
+    ) -> None:
+        super().__init__(trainer=trainer, dtype=dtype)
+        self.freq = freq
+        self.num_series = num_series
+        self.skip_size = skip_size
+        self.ar_window = ar_window
+        self.horizon = horizon
+        self.prediction_length = prediction_length
+        self.future_length = (
+            horizon if horizon is not None else prediction_length
+        )
+        self.context_length = context_length
+        self.channels = channels
+        self.kernel_size = kernel_size
+        self.dropout_rate = dropout_rate
+        self.output_activation = output_activation
+        self.rnn_cell_type = rnn_cell_type
+        self.rnn_num_layers = rnn_num_layers
+        self.skip_rnn_cell_type = skip_rnn_cell_type
+        self.skip_rnn_num_layers = skip_rnn_num_layers
+        self.scaling = scaling
+        self.dtype = dtype
+
+    def create_transformation(self) -> Transformation:
+        return Chain(
+            trans=[
+                AsNumpyArray(
+                    field=FieldName.TARGET, expected_ndim=2, dtype=self.dtype
+                ),
+                AddObservedValuesIndicator(
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.OBSERVED_VALUES,
+                    dtype=self.dtype,
+                ),
+                InstanceSplitter(
+                    target_field=FieldName.TARGET,
+                    is_pad_field=FieldName.IS_PAD,
+                    start_field=FieldName.START,
+                    forecast_start_field=FieldName.FORECAST_START,
+                    train_sampler=ExpectedNumInstanceSampler(num_instances=1),
+                    time_series_fields=[FieldName.OBSERVED_VALUES],
+                    past_length=self.context_length,
+                    future_length=self.future_length,
+                    output_NTC=False,  # output NCT for first layer conv1d
+                ),
+            ]
+        )
+
+    def create_training_network(self) -> HybridBlock:
+        return LSTNetTrain(
+            num_series=self.num_series,
+            channels=self.channels,
+            kernel_size=self.kernel_size,
+            rnn_cell_type=self.rnn_cell_type,
+            rnn_num_layers=self.rnn_num_layers,
+            skip_rnn_cell_type=self.skip_rnn_cell_type,
+            skip_rnn_num_layers=self.skip_rnn_num_layers,
+            skip_size=self.skip_size,
+            ar_window=self.ar_window,
+            context_length=self.context_length,
+            horizon=self.horizon,
+            prediction_length=self.prediction_length,
+            dropout_rate=self.dropout_rate,
+            output_activation=self.output_activation,
+            scaling=self.scaling,
+            dtype=self.dtype,
+        )
+
+    def create_predictor(
+        self, transformation: Transformation, trained_network: HybridBlock
+    ) -> Predictor:
+        prediction_network = LSTNetPredict(
+            num_series=self.num_series,
+            channels=self.channels,
+            kernel_size=self.kernel_size,
+            rnn_cell_type=self.rnn_cell_type,
+            rnn_num_layers=self.rnn_num_layers,
+            skip_rnn_cell_type=self.skip_rnn_cell_type,
+            skip_rnn_num_layers=self.skip_rnn_num_layers,
+            skip_size=self.skip_size,
+            ar_window=self.ar_window,
+            context_length=self.context_length,
+            horizon=self.horizon,
+            prediction_length=self.prediction_length,
+            dropout_rate=self.dropout_rate,
+            output_activation=self.output_activation,
+            scaling=self.scaling,
+            dtype=self.dtype,
+        )
+
+        copy_parameters(trained_network, prediction_network)
+
+        return RepresentableBlockPredictor(
+            input_transform=transformation,
+            prediction_net=prediction_network,
+            batch_size=self.trainer.batch_size,
+            freq=self.freq,
+            prediction_length=self.horizon or self.prediction_length,
+            ctx=self.trainer.ctx,
+            dtype=self.dtype,
+        )
