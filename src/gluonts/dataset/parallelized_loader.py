790d114d6a79745d3e2fa4b6a32522a1c93af0d1
Aaron Spieler <aspiele@amazon.com>
2020-03-09 17:48:14 +0100

    Removing unused functions.

--- src/gluonts/dataset/parallelized_loader.py
+++ src/gluonts/dataset/parallelized_loader.py
@@ -19,15 +19,21 @@ import multiprocessing.queues
 from multiprocessing.reduction import ForkingPickler
 from multiprocessing.pool import ThreadPool
 import threading
+from typing import Optional
+
 import numpy as np
+from gluonts.core.component import DType
+from gluonts.dataset.common import Dataset
+from gluonts.transform import Transformation
 
 try:
     import multiprocessing.resource_sharer
 except ImportError:
     pass
 
-from mxnet.gluon.data import sampler as _sampler
+from mxnet.gluon.data import sampler as _sampler, Sampler
 from mxnet import nd, context
+import mxnet as mx
 
 if sys.platform == "darwin" or sys.platform == "win32":
 
@@ -69,64 +75,7 @@ else:
 ForkingPickler.register(nd.NDArray, reduce_ndarray)
 
 
-class ConnectionWrapper(object):
-    """Connection wrapper for multiprocessing that supports sending
-    NDArray via shared memory."""
-
-    def __init__(self, conn):
-        self._conn = conn
-
-    def send(self, obj):
-        """Send object"""
-        buf = io.BytesIO()
-        ForkingPickler(buf, pickle.HIGHEST_PROTOCOL).dump(obj)
-        self.send_bytes(buf.getvalue())
-
-    def recv(self):
-        """Receive object"""
-        buf = self.recv_bytes()
-        return pickle.loads(buf)
-
-    def __getattr__(self, name):
-        """Emmulate conn"""
-        attr = self.__dict__.get("_conn", None)
-        return getattr(attr, name)
-
-
-class Queue(multiprocessing.queues.Queue):
-    """Wrapper for multiprocessing queue that dumps NDArray with shared memory."""
-
-    def __init__(self, *args, **kwargs):
-        if sys.version_info[0] <= 2:
-            super(Queue, self).__init__(*args, **kwargs)
-        else:
-            super(Queue, self).__init__(
-                *args, ctx=multiprocessing.get_context(), **kwargs
-            )
-        self._reader = ConnectionWrapper(self._reader)
-        self._writer = ConnectionWrapper(self._writer)
-        self._send = self._writer.send
-        self._recv = self._reader.recv
-
-
-class SimpleQueue(multiprocessing.queues.SimpleQueue):
-    """Wrapper for multiprocessing SimpleQueue that dumps NDArray with shared memory.
-       SimpleQueue don't use threading internally.
-    """
-
-    def __init__(self, *args, **kwargs):
-        if sys.version_info[0] <= 2:
-            super(SimpleQueue, self).__init__(*args, **kwargs)
-        else:
-            super(SimpleQueue, self).__init__(
-                *args, ctx=multiprocessing.get_context(), **kwargs
-            )
-        self._reader = ConnectionWrapper(self._reader)
-        self._writer = ConnectionWrapper(self._writer)
-        self._send = self._writer.send
-        self._recv = self._reader.recv
-
-
+# TODO: modify
 def default_batchify_fn(data):
     """Collate data into batch."""
     if isinstance(data[0], nd.NDArray):
@@ -139,6 +88,7 @@ def default_batchify_fn(data):
         return nd.array(data, dtype=data.dtype)
 
 
+# TODO: modify
 def default_mp_batchify_fn(data):
     """Collate data into batch. Use shared memory for stacking."""
     if isinstance(data[0], nd.NDArray):
@@ -179,7 +129,7 @@ def _worker_initializer(dataset):
     _worker_dataset = dataset
 
 
-def _worker_fn(samples, batchify_fn, dataset=None):
+def _worker_fn(samples, batchify_fn, transformation, dataset=None):
     """Function for processing data in worker process."""
     # pylint: disable=unused-argument
     # it is required that each worker process has to fork a new MXIndexedRecordIO handle
@@ -191,7 +141,7 @@ def _worker_fn(samples, batchify_fn, dataset=None):
     return buf.getvalue()
 
 
-def _thread_worker_fn(samples, batchify_fn, dataset):
+def _thread_worker_fn(samples, batchify_fn, transformation, dataset):
     """Threadpool worker function for processing data."""
     return batchify_fn([dataset[i] for i in samples])
 
@@ -207,11 +157,12 @@ class _MultiWorkerIter(object):
         worker_pool,
         batchify_fn,
         batch_sampler,
-        pin_memory=False,
-        pin_device_id=0,
-        worker_fn=_worker_fn,
-        prefetch=0,
-        dataset=None,
+        pin_memory: Optional[bool] = False,
+        pin_device_id: Optional[bool] = 0,
+        worker_fn: Optional[callable] = _worker_fn,
+        dataset: Optional[Dataset] = None,
+        prefetch: Optional[bool] = 0,
+        timeout: Optional[int] = 120,
     ):
         self._worker_pool = worker_pool
         self._batchify_fn = batchify_fn  # Need to customize
@@ -224,6 +175,7 @@ class _MultiWorkerIter(object):
         self._pin_memory = pin_memory
         self._pin_device_id = pin_device_id
         self._dataset = dataset
+        self._timeout = timeout
         # pre-fetch
         for _ in range(prefetch):
             self._push_next()
@@ -298,25 +250,31 @@ class DataLoader(object):
     """Loads data from a dataset and returns mini-batches of data.
     Parameters
     ----------
-    dataset : Dataset
-        Source dataset. Note that numpy and mxnet arrays can be directly used
-        as a Dataset.
-    batch_size : int
+    dataset
+        The dataset from which to load data.
+    transform
+        A transformation to apply to each entry in the dataset.
+    batch_size
         Size of mini-batch.
-    shuffle : bool
+    ctx
+        MXNet context to use to store data.
+    dtype
+        Floating point type to use.
+
+    shuffle
         Whether to shuffle the samples.
-    sampler : Sampler
+    sampler
         The sampler to use. Either specify sampler or shuffle, not both.
-    last_batch : {'keep', 'discard', 'rollover'}
+    last_batch
         How to handle the last batch if batch_size does not evenly divide
         `len(dataset)`.
         keep - A batch with less samples than previous batches is returned.
         discard - The last batch is discarded if its incomplete.
         rollover - The remaining samples are rolled over to the next epoch.
-    batch_sampler : Sampler
+    batch_sampler
         A sampler that returns mini-batches. Do not specify batch_size,
         shuffle, sampler, and last_batch if batch_sampler is specified.
-    batchify_fn : callable
+    batchify_fn
         Callback function to allow users to specify how to merge samples
         into a batch. Defaults to `default_batchify_fn`::
             def default_batchify_fn(data):
@@ -328,15 +286,15 @@ class DataLoader(object):
                 else:
                     data = np.asarray(data)
                     return nd.array(data, dtype=data.dtype)
-    num_workers : int, default 0
+    num_workers
         The number of multiprocessing workers to use for data preprocessing.
-    pin_memory : boolean, default False
+    pin_memory
         If ``True``, the dataloader will copy NDArrays into pinned memory
         before returning them. Copying from CPU pinned memory to GPU is faster
         than from normal CPU memory.
-    pin_device_id : int, default 0
+    pin_device_id
         The device id to use for allocating pinned memory if pin_memory is ``True``
-    prefetch : int, default is `num_workers * 2`
+    prefetch
         The number of prefetching batches only works if `num_workers` > 0.
         If `prefetch` > 0, it allow worker process to prefetch certain batches before
         acquiring data from iterators.
@@ -344,7 +302,7 @@ class DataLoader(object):
         but will consume more shared_memory. Using smaller number may forfeit the purpose of using
         multiple worker processes, try reduce `num_workers` in this case.
         By default it defaults to `num_workers * 2`.
-    thread_pool : bool, default False
+    thread_pool
         If ``True``, use threading pool instead of multiprocessing pool. Using threadpool
         can avoid shared memory usage. If `DataLoader` is more IO bounded or GIL is not a killing
         problem, threadpool version may achieve better performance than multiprocessing.
@@ -352,23 +310,37 @@ class DataLoader(object):
 
     def __init__(
         self,
-        dataset,
-        batch_size=None,
-        shuffle=False,
-        sampler=None,
-        last_batch=None,
-        batch_sampler=None,
-        batchify_fn=None,
-        num_workers=0,
-        pin_memory=False,
-        pin_device_id=0,
-        prefetch=None,
-        thread_pool=False,
+        dataset: Dataset,
+        transform: Transformation,
+        is_train: bool,
+        ctx: mx.Context,
+        dtype: DType = np.float32,
+        batch_size: int = None,
+        shuffle: bool = False,
+        sampler: Optional[Sampler] = None,
+        last_batch: Optional[str] = None,
+        batch_sampler: Optional[Sampler] = None,
+        batchify_fn: Optional[callable] = None,
+        num_workers: Optional[int] = 0,
+        pin_memory: Optional[bool] = False,
+        pin_device_id: Optional[int] = 0,
+        prefetch: Optional[int] = None,
+        thread_pool: Optional[bool] = False,
     ):
-        self._dataset = dataset
-        self._pin_memory = pin_memory
-        self._pin_device_id = pin_device_id
-        self._thread_pool = thread_pool
+        self._dataset = list(dataset)  # convert dataset to list
+        self.ctx = ctx
+        self.dtype = dtype
+        self.is_train = is_train
+        self.transform = transform
+
+        self.pin_memory = pin_memory
+        self.pin_device_id = pin_device_id
+        self.thread_pool = thread_pool
+
+        assert last_batch in {"keep", "discard", "rollover"}, (
+            f"Invalid argument for last_batch: {last_batch}. "
+            f"Expected one of : 'keep', 'discard' or 'rollover' "
+        )
 
         if batch_sampler is None:
             if batch_size is None:
@@ -400,40 +372,40 @@ class DataLoader(object):
                 "not be specified if batch_sampler is specified."
             )
 
-        self._batch_sampler = batch_sampler
-        self._num_workers = num_workers if num_workers >= 0 else 0
-        self._worker_pool = None
-        self._prefetch = max(
-            0, int(prefetch) if prefetch is not None else 2 * self._num_workers
+        self.batch_sampler = batch_sampler
+        self.num_workers = num_workers if num_workers >= 0 else 0
+        self.worker_pool = None
+        self.prefetch = max(
+            0, int(prefetch) if prefetch is not None else 2 * self.num_workers
         )
-        if self._num_workers > 0:
-            if self._thread_pool:
-                self._worker_pool = ThreadPool(self._num_workers)
+        if self.num_workers > 0:
+            if self.thread_pool:
+                self.worker_pool = ThreadPool(self.num_workers)
             else:
-                self._worker_pool = multiprocessing.Pool(
-                    self._num_workers,
+                self.worker_pool = multiprocessing.Pool(
+                    self.num_workers,
                     initializer=_worker_initializer,
                     initargs=[self._dataset],
                 )
         if batchify_fn is None:
             if num_workers > 0:
-                self._batchify_fn = default_mp_batchify_fn
+                self.batchify_fn = default_mp_batchify_fn
             else:
-                self._batchify_fn = default_batchify_fn
+                self.batchify_fn = default_batchify_fn
         else:
-            self._batchify_fn = batchify_fn
+            self.batchify_fn = batchify_fn
 
     def __iter__(self):
-        if self._num_workers == 0:
+        if self.num_workers == 0:
 
             def same_process_iter():
-                for batch in self._batch_sampler:
-                    ret = self._batchify_fn(
+                for batch in self.batch_sampler:
+                    ret = self.batchify_fn(
                         [self._dataset[idx] for idx in batch]
                     )
-                    if self._pin_memory:
+                    if self.pin_memory:
                         ret = _as_in_context(
-                            ret, context.cpu_pinned(self._pin_device_id)
+                            ret, context.cpu_pinned(self.pin_device_id)
                         )
                     yield ret
 
@@ -441,22 +413,22 @@ class DataLoader(object):
 
         # multi-worker
         return _MultiWorkerIter(
-            self._worker_pool,
-            self._batchify_fn,
-            self._batch_sampler,
-            pin_memory=self._pin_memory,
-            pin_device_id=self._pin_device_id,
-            worker_fn=_thread_worker_fn if self._thread_pool else _worker_fn,
-            prefetch=self._prefetch,
-            dataset=self._dataset if self._thread_pool else None,
+            self.worker_pool,
+            self.batchify_fn,
+            self.batch_sampler,
+            pin_memory=self.pin_memory,
+            pin_device_id=self.pin_device_id,
+            worker_fn=_thread_worker_fn if self.thread_pool else _worker_fn,
+            prefetch=self.prefetch,
+            dataset=self._dataset if self.thread_pool else None,
         )
 
     def __len__(self):
-        return len(self._batch_sampler)
+        return len(self.batch_sampler)
 
     def __del__(self):
-        if self._worker_pool:
+        if self.worker_pool:
             # manually terminate due to a bug that pool is not automatically terminated
             # https://bugs.python.org/issue34172
-            assert isinstance(self._worker_pool, multiprocessing.pool.Pool)
-            self._worker_pool.terminate()
+            assert isinstance(self.worker_pool, multiprocessing.pool.Pool)
+            self.worker_pool.terminate()
