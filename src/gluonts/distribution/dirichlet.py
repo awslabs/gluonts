2e20016827ae7a3708227092d2b2918e595a11d3
Lorenzo Stella <lorenzostella@gmail.com>
2020-03-30 20:43:17 +0200

    remove asserts on loc and scale (#734)
    Co-authored-by: Lorenzo Stella <stellalo@amazon.de>

--- src/gluonts/distribution/dirichlet.py
+++ src/gluonts/distribution/dirichlet.py
@@ -148,7 +148,6 @@ class DirichletOutput(DistributionOutput):
         self.mask = None
 
     def distribution(self, distr_args, loc=None, scale=None) -> Distribution:
-        assert loc is None and scale is None
         distr = Dirichlet(distr_args)
         return distr
 
