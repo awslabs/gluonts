d30fde5dd12e78cd764b101ace95e5fab50bceb9
Aaron Spieler <aspiele@amazon.com>
2020-04-03 15:38:42 +0200

    Catch exception when shared memory resources are already freed.

--- src/gluonts/dataset/parallelized_loader.py
+++ src/gluonts/dataset/parallelized_loader.py
@@ -414,9 +414,13 @@ class _MultiWorkerIter(object):
     def __del__(self):
         # Explicitly load the content from shared memory to delete it
         # Unfortunately it seems the way the data is pickled prevents efficient implicit GarbageCollection
-        for k in list(self._data_buffer.keys()):
-            res = pickle.loads(self._data_buffer.pop(k).get(self._timeout))
-            del res
+        try:
+            for k in list(self._data_buffer.keys()):
+                res = pickle.loads(self._data_buffer.pop(k).get(self._timeout))
+                del res
+        except FileNotFoundError:
+            # The resources were already released
+            pass
 
 
 # TODO: think about how a multiprocessing.Manager() would complement this implementation
