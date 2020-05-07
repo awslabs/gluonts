Fix negative binomial's scaling (#719)

src/gluonts/distribution/neg_binomial.py
@@ -130,7 +130,7 @@ class NegativeBinomialOutput(DistributionOutput):
         else:
             F = getF(mu)
             mu = F.broadcast_mul(mu, scale)
-            alpha = F.broadcast_mul(alpha, F.sqrt(scale + 1.0))
+            alpha = F.broadcast_add(alpha, F.broadcast_div(scale - 1, mu))
             return NegativeBinomial(mu, alpha, F)
 
     @property