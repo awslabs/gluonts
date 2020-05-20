Fix scale computation

src/gluonts/representation/global_relative_binning.py
@@ -134,7 +134,9 @@ class GlobalRelativeBinning(Representation):
         # Calculate local scale if scale is not already supplied.
         if scale is None:
             scale = F.expand_dims(
-                F.sum(data, axis=-1) / F.sum(observed_indicator, axis=-1), -1
+                F.sum(data * observed_indicator, axis=-1)
+                / F.sum(observed_indicator, axis=-1),
+                -1,
             )
             # Clip scale on the bottom to prevent division by zero.
             scale = F.clip(scale, 1e-20, np.inf)

src/gluonts/representation/local_absolute_binning.py
@@ -71,7 +71,9 @@ class LocalAbsoluteBinning(Representation):
         if scale is None:
             # Even though local binning implicitly scales the data, we still return the scale as an input to the model.
             scale = F.expand_dims(
-                F.sum(data, axis=-1) / F.sum(observed_indicator, axis=-1), -1
+                F.sum(data * observed_indicator, axis=-1)
+                / F.sum(observed_indicator, axis=-1),
+                -1,
             )
 
             bin_centers_hyb = np.ones((len(data), self.num_bins)) * (-1)

src/gluonts/representation/representation.py
@@ -97,7 +97,7 @@ class Representation(nn.HybridBlock):
             Tuple consisting of the transformed data, the computed scale, 
             and additional parameters to be passed to post_transform.
         """
-        raise NotImplementedError
+        return data, F.ones_like(data), []
 
     def post_transform(
         self, F, samples: Tensor, scale: Tensor, rep_params: List[Tensor]