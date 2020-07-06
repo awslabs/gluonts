corrected true gradients in the test file and edge cases of the log_prob calculations

src/gluonts/mx/distribution/nan_mixture.py
@@ -71,19 +71,30 @@ class NanMixture(MixtureDistribution):
         )
 
     def log_prob(self, x: Tensor) -> Tensor:
-
         F = self.F
 
-        # masking NaN's with ones
+        # masking zero-valued mixture_probs to prevent NaN edge case gradients
+        nonzero_mixture_probs = F.where(
+            self.mixture_probs == 0,
+            self.mixture_probs.ones_like(),
+            self.mixture_probs,
+        )
+
+        log_mix_weights = F.where(
+            self.mixture_probs == 0,
+            -1.0 / self.mixture_probs.zeros_like(),
+            F.log(nonzero_mixture_probs),
+        )
+
+        # TODO does the replacement value has to be in the support of the distribution? maybe sample from it
+        # masking data NaN's with ones to prevent NaN gradients
         x_non_nan = F.where(x != x, F.ones_like(x), x)
-
-        log_mix_weights = F.log(self.mixture_probs)
-
         non_nan_comp_log_likelihood = F.where(
             x != x,
             -x.ones_like() / 0.0,
             self.components[0].log_prob(x_non_nan),
         )
+
         # compute log probabilities of components
         component_log_likelihood = F.stack(
             *[non_nan_comp_log_likelihood, self.components[1].log_prob(x)],

test/distribution/test_nan_mixture.py
@@ -81,7 +81,7 @@ def test_nan_mixture(distr: Distribution, p: Tensor, serialize_fn) -> None:
     # TODO check histogram of non-nan values
     # TODO check mean and stddev
 
-    x = mx.nd.array([[[np.nan, 10.5, -0.5]], [[np.nan, 10.5, -0.5]]])
+    x = mx.nd.array([[[np.nan, 10.5, -0.5]], [[np.nan, 10.5, np.nan]]])
     print("p")
     print(p)
     print()
@@ -107,17 +107,17 @@ def test_nan_mixture(distr: Distribution, p: Tensor, serialize_fn) -> None:
         nll = nan_mixture.loss(x)
     nll.backward()
 
-    p_grad_true = mx.nd.where(x != x, 1 / p, 1 / (p - 1))
+    p_grad_true = mx.nd.where(x != x, -1 / p, 1 / (1 - p))
 
-    mu_grad_non_nan = -(x - mu) / mx.nd.square(sigma)
-    mu_grad_true = mx.nd.where(x != x, mu.zeros_like(), mu_grad_non_nan)
+    mu_grad_true = -(x - mu) / mx.nd.square(sigma)
+    mu_grad_true = mx.nd.where(x != x, mu.zeros_like(), mu_grad_true)
+    mu_grad_true = mx.nd.where(p == 1, mu.zeros_like(), mu_grad_true)
 
-    sigma_grad_non_nan = -(
+    sigma_grad_true = -(
         mx.nd.square(mu) - 2 * mu * x - mx.nd.square(sigma) + mx.nd.square(x)
     ) / (sigma ** 3)
-    sigma_grad_true = mx.nd.where(
-        x != x, sigma.zeros_like(), sigma_grad_non_nan
-    )
+    sigma_grad_true = mx.nd.where(x != x, sigma.zeros_like(), sigma_grad_true)
+    sigma_grad_true = mx.nd.where(p == 1, sigma.zeros_like(), sigma_grad_true)
 
     print(p.grad)
     print(p_grad_true)