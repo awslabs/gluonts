fixed edge cases of the gradients

src/gluonts/mx/distribution/nan_mixture.py
@@ -61,6 +61,7 @@ class NanMixture(MixtureDistribution):
     def __init__(self, nan_prob: Tensor, distribution: Distribution) -> None:
         F = getF(nan_prob)
         mixture_probs = F.stack(1 - nan_prob, nan_prob, axis=-1)
+        self.nan_prob = nan_prob
         super().__init__(
             mixture_probs=mixture_probs,
             components=[
@@ -89,23 +90,27 @@ class NanMixture(MixtureDistribution):
         # TODO does the replacement value has to be in the support of the distribution? maybe sample from it
         # masking data NaN's with ones to prevent NaN gradients
         x_non_nan = F.where(x != x, F.ones_like(x), x)
+
+        # calculate likelihood for values which are not NaN, only if nan_prob is not 1
         non_nan_comp_log_likelihood = F.where(
-            x != x,
+            F.logical_or(
+                x != x,
+                F.logical_and(x == x, self.nan_prob.broadcast_like(x) == 1),
+            ),
             -x.ones_like() / 0.0,
             self.components[0].log_prob(x_non_nan),
         )
-
-        # compute log probabilities of components
+        # stack log probabilities of components
         component_log_likelihood = F.stack(
             *[non_nan_comp_log_likelihood, self.components[1].log_prob(x)],
             axis=-1
         )
-
         # compute mixture log probability by log-sum-exp
         summands = log_mix_weights + component_log_likelihood
         max_val = F.max_axis(summands, axis=-1, keepdims=True)
         # catch edge case max_val = -inf
         max_val = F.where(max_val == -np.inf, max_val.zeros_like(), max_val)
+
         sum_exp = F.sum(
             F.exp(F.broadcast_minus(summands, max_val)), axis=-1, keepdims=True
         )

test/distribution/test_nan_mixture.py
@@ -81,12 +81,12 @@ def test_nan_mixture(distr: Distribution, p: Tensor, serialize_fn) -> None:
     # TODO check histogram of non-nan values
     # TODO check mean and stddev
 
-    x = mx.nd.array([[[np.nan, 10.5, -0.5]], [[np.nan, 10.5, np.nan]]])
-    print("p")
-    print(p)
-    print()
-    print("x")
-    print(x)
+    x = mx.nd.array([[[np.nan, 3.5, -0.5]], [[np.nan, 3.5, np.nan]]])
+    # print("p")
+    # print(p)
+    # print()
+    # print("x")
+    # print(x)
     # check log_prob
     log_prob = nan_mixture.log_prob(x)
     log_prob_true = mx.nd.log(mx.nd.where(x != x, p, (1 - p) * distr.prob(x)))
@@ -104,10 +104,19 @@ def test_nan_mixture(distr: Distribution, p: Tensor, serialize_fn) -> None:
     with mx.autograd.record():
         distr = Gaussian(mu=mu, sigma=sigma,)
         nan_mixture = NanMixture(nan_prob=p, distribution=distr)
-        nll = nan_mixture.loss(x)
+        nll = -nan_mixture.log_prob(x)
     nll.backward()
 
     p_grad_true = mx.nd.where(x != x, -1 / p, 1 / (1 - p))
+    # gradient is undefined for these cases:
+    p_grad_true = mx.nd.where(
+        mx.nd.logical_or(
+            mx.nd.logical_and(x != x, p == 0),
+            mx.nd.logical_and(x == x, p == 1),
+        ),
+        0.0 / p_grad_true.zeros_like(),
+        p_grad_true,
+    )
 
     mu_grad_true = -(x - mu) / mx.nd.square(sigma)
     mu_grad_true = mx.nd.where(x != x, mu.zeros_like(), mu_grad_true)
@@ -119,12 +128,17 @@ def test_nan_mixture(distr: Distribution, p: Tensor, serialize_fn) -> None:
     sigma_grad_true = mx.nd.where(x != x, sigma.zeros_like(), sigma_grad_true)
     sigma_grad_true = mx.nd.where(p == 1, sigma.zeros_like(), sigma_grad_true)
 
-    print(p.grad)
-    print(p_grad_true)
-    print(mu.grad)
-    print(mu_grad_true)
-    print(sigma.grad)
-    print(sigma_grad_true)
-    assert np.allclose(p.grad, p_grad_true, atol=1e-1)
-    assert np.allclose(mu.grad, mu_grad_true, atol=1e-1)
-    assert np.allclose(sigma.grad, p_grad_true, atol=1e-1)
+    # print(p.grad)
+    # print(p_grad_true)
+    # print(mu.grad)
+    # print(mu_grad_true)
+    # print(sigma.grad)
+    # print(sigma_grad_true)
+
+    assert np.allclose(
+        p.grad.asnumpy(), p_grad_true.asnumpy(), atol=1e-1, equal_nan=True
+    )
+    assert np.allclose(mu.grad.asnumpy(), mu_grad_true.asnumpy(), atol=1e-1)
+    assert np.allclose(
+        sigma.grad.asnumpy(), sigma_grad_true.asnumpy(), atol=1e-1
+    )