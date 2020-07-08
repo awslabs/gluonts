33b81d6c613ae4ff6efb64261a523b351fb4f74e
richardk <richardkurle@gmail.com>
2020-07-08 19:25:42 +0200

    made link nonlinearities more configurable (MLP dims and activations)

--- src/gluonts/nursery/torch_arsgls_rbpf/models/gls_parameters/switch_link_functions.py
+++ src/gluonts/nursery/torch_arsgls_rbpf/models/gls_parameters/switch_link_functions.py
@@ -1,51 +1,28 @@
+from typing import Dict, Sequence, Tuple
 from box import Box
 from torch import nn
 
 from torch_extensions.mlp import MLP
 
 
-# class IndividualLink(nn.ModuleDict):
-#     def __init__(self, dim_in, names_to_dims_out: dict, names_to_dims_hidden: dict,
-#                  names_to_activations: dict):
-#         super().__init__()
-#         assert tuple(names_to_dims_out.keys()) == \
-#                tuple(names_to_dims_hidden.keys()) == \
-#                tuple(names_to_activations.keys())
-#         assert all(isinstance(val, int) for val in names_to_dims_out)
-#         assert all(isinstance(val, (tuple, list)) for val in names_to_dims_hidden)
-#         assert all(isinstance(val, (nn.Module, tuple, list)) for val in names_to_activations)
-#
-#         names_to_activations = {
-#             name: val if isinstance(val, (tuple, list))
-#             else (names_to_activations,) * len(names_to_dims_hidden[name])
-#             for name, val in names_to_activations.items()
-#         }
-#         for name in names_to_dims_out.keys():
-#             self.update({
-#                 name: MLP(
-#                     dim_in=dim_in,
-#                     dims_hidden=tuple(names_to_dims_hidden[name]) + (names_to_dims_out[name]),
-#                     activations=tuple(names_to_activations[name]) + (nn.Softmax(dim=-1),),
-#                 )
-#             })
-#
-#     def forward(self, switch):
-#         return Box({name: link(switch) for name, link in self.items()})
-
-
 class IndividualLink(nn.ModuleDict):
-    def __init__(self, dim_in, names_and_dims_out: dict):
+    def __init__(
+            self,
+            dim_in,
+            names_and_dims_out: Dict[str, int],
+            dims_hidden: Tuple[int] = tuple(),
+            activations_hidden: nn.Module = nn.ReLU(),
+    ):
         super().__init__()
+        if isinstance(activations_hidden, nn.Module):
+            activations_hidden = (activations_hidden,) * len(dims_hidden)
         for name, dim_out in names_and_dims_out.items():
             self.update(
                 {
                     name: MLP(
                         dim_in=dim_in,
-                        dims_hidden=(64, dim_out,),
-                        activations=(
-                            nn.LeakyReLU(0.1, inplace=True),
-                            nn.Softmax(dim=-1),
-                        ),
+                        dims=dims_hidden + (dim_out,),
+                        activations=activations_hidden + (nn.Softmax(dim=-1),),
                     )
                 }
             )
@@ -54,15 +31,23 @@ class IndividualLink(nn.ModuleDict):
         return Box({name: link(switch) for name, link in self.items()})
 
 
-# TODO: allow this configurable. Changed back to without hidden, as KVAE uses this without.
 class SharedLink(nn.Module):
-    def __init__(self, dim_in, dim_out, names: (list, tuple)):
+    def __init__(
+            self,
+            dim_in,
+            dim_out,
+            names: (list, tuple),
+            dims_hidden: Tuple[int] = tuple(),
+            activations_hidden: nn.Module = nn.ReLU(),
+    ):
         super().__init__()
+        if isinstance(activations_hidden, nn.Module):
+            activations_hidden = (activations_hidden,) * len(dims_hidden)
         self.names = names
         self.link = MLP(
             dim_in=dim_in,
-            dims_hidden=(dim_out,),
-            activations=(nn.Softmax(dim=-1),),
+            dims=dims_hidden + (dim_out,),
+            activations=activations_hidden + (nn.Softmax(dim=-1),),
         )
 
     def forward(self, switch):
