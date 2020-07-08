953c25512a47198a6443f8fa61fcec61157e38a8
Pascal Iversen <66594767+PascalIversen@users.noreply.github.com>
2020-07-08 17:30:32 +0200

    style fix

--- test/distribution/test_nan_mixture.py
+++ test/distribution/test_nan_mixture.py
@@ -254,6 +254,8 @@ cat_samples = np.random.choice(
 cat_samples = np.where(
     np.random.uniform(size=(BATCH_SIZE)) > nan_prob, cat_samples, np.nan
 )
+
+
 @pytest.mark.skip("Skip test that takes long time to run")
 def test_nanmixture_inference() -> None:
     nmdo = NanMixtureOutput(GaussianOutput())
