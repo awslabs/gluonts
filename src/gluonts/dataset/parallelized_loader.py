4aec7eed62bb92eef51af8b420e97bade28f62c1
Aaron Spieler <25365592+AaronSpieler@users.noreply.github.com>
2020-07-23 14:21:00 +0200

    Removing mp worker del method. (#944)

--- src/gluonts/dataset/parallelized_loader.py
+++ src/gluonts/dataset/parallelized_loader.py
@@ -528,17 +528,6 @@ class _MultiWorkerIter(object):
                 return
             yield next_batch
 
-    def __del__(self):
-        # Explicitly load the content from shared memory to delete it
-        # Unfortunately it seems the way the data is pickled prevents efficient implicit GarbageCollection
-        try:
-            for k in list(self._data_buffer.keys()):
-                res = pickle.loads(self._data_buffer.pop(k).get(self._timeout))
-                del res
-        except FileNotFoundError:
-            # The resources were already released
-            pass
-
 
 class ParallelDataLoader(object):
     """
