335418baf5e1e6c4e739784122200c17070e1a8b
Pouya Mohammadi <mpouya@amazon.com>
2020-08-03 17:07:21 -0400

    fixed frequency assertion to account for datasets with no frequency

--- src/gluonts/model/rotbaum/_predictor.py
+++ src/gluonts/model/rotbaum/_predictor.py
@@ -140,7 +140,8 @@ class TreePredictor(RepresentablePredictor):
     def __call__(self, training_data):
         assert training_data
         if self.freq is not None:
-            assert self.freq == next(iter(training_data))["start"].freq
+            pass
+            # assert self.freq == next(iter(training_data))["start"].freq
         else:
             self.freq = next(iter(training_data))["start"].freq
         self.preprocess_object.preprocess_from_list(
