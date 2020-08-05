e6709edfb44d02b8691bf3d202de5729c1f04ce7
Pouya Mohammadi <mpouya@amazon.com>
2020-08-05 13:01:16 -0400

    Rotbaum test now uses accuracy_test function

--- test/model/rotbaum/test_model.py
+++ test/model/rotbaum/test_model.py
@@ -17,50 +17,24 @@ from itertools import chain
 import pytest
 
 # First-party imports
-from gluonts.model.rotbaum import TreePredictor, TreeEstimator
-from gluonts.evaluation import Evaluator
-from gluonts.evaluation.backtest import backtest_metrics
+from gluonts.model.rotbaum import TreeEstimator
 
 # TODO: function.
-# TODO: implement using accuracy_test like other gluonts models
 
-# @pytest.fixture()
-# def hyperparameters(dsinfo):
-#     return dict(
-#         context_length=2,
-#         quantiles=[0.1, 0.5, 0.9],
-#         num_workers=0,
-#     )
-
-# @pytest.mark.parametrize("quantiles", [[0.1, 0.5, 0.9], [0.5]])
-# def test_accuracy(
-#     accuracy_test, hyperparameters, quantiles
-# ):
-#     hyperparameters.update(
-#         quantiles=quantiles, max_workers=32
-#     )
-
-#     accuracy_test(TreeEstimator, hyperparameters, accuracy=0.20)
-
-def test_accuracy(accuracy_test, dsinfo):
-
-    estimator = TreeEstimator(
+@pytest.fixture()
+def hyperparameters(dsinfo):
+    return dict(
         context_length=2,
-        prediction_length=dsinfo["prediction_length"],
-        freq=dsinfo["freq"],
-    )
-    predictor = estimator.train(dsinfo.train_ds)
-
-    agg_metrics, item_metrics = backtest_metrics(
-        test_dataset=dsinfo.test_ds,
-        predictor=predictor,
-        evaluator=Evaluator(quantiles=[0.1, 0.5, 0.9]),
+        quantiles=[0.1, 0.5, 0.9],
+        num_workers=0,
     )
 
-    if dsinfo["name"] == "constant":
-        for q in [0.1, 0.5, 0.9]:
-            assert agg_metrics[f"wQuantileLoss[{q}]"] == 0
-    if dsinfo["name"] == "synthetic":
-        assert 1.1 < agg_metrics[f"wQuantileLoss[0.5]"] < 1.2
-        accuracy = 10.0
-        assert agg_metrics["ND"] <= accuracy
+@pytest.mark.parametrize("quantiles", [[0.1, 0.5, 0.9], [0.5]])
+def test_accuracy(
+    accuracy_test, hyperparameters, quantiles
+):
+    hyperparameters.update(
+        quantiles=quantiles, max_workers=32
+    )
+
+    accuracy_test(TreeEstimator, hyperparameters, accuracy=0.20)
