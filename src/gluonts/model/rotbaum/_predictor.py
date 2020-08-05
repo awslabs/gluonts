51db70f2c7c15980e8f5057434b27c5af2a2c7f7
Pouya Mohammadi <mpouya@amazon.com>
2020-08-05 12:55:51 -0400

    fixed imports that were broken from merge

--- src/gluonts/model/rotbaum/_predictor.py
+++ src/gluonts/model/rotbaum/_predictor.py
@@ -25,6 +25,7 @@ import logging
 
 # First-party imports
 from gluonts.core.component import validated
+from gluonts.dataset.common import Dataset
 from gluonts.model.forecast import Forecast, SampleForecast
 from gluonts.model.forecast_generator import log_once
 from gluonts.model.predictor import RepresentablePredictor
