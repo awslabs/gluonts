317e9c7f829f4d53a5c84478e409d951a02c600b
Pascal Iversen <66594767+PascalIversen@users.noreply.github.com>
2020-09-22 15:29:08 +0200

    refactoring

--- src/gluonts/torch/model/predictor.py
+++ src/gluonts/torch/model/predictor.py
@@ -90,9 +90,3 @@ class PyTorchPredictor(Predictor):
                 output_transform=self.output_transform,
                 num_samples=num_samples,
             )
-
-    @classmethod
-    def deserialize(
-        cls, path: Path, ctx: Optional[mx.Context] = None
-    ) -> "Predictor":
-        raise NotImplementedError
