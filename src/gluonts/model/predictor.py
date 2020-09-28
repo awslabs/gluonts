06b756ba31f7b634f327bcfabe775e24a4e1288a
Pascal Iversen <66594767+PascalIversen@users.noreply.github.com>
2020-09-28 14:28:07 +0200

    rebase

--- src/gluonts/model/predictor.py
+++ src/gluonts/model/predictor.py
@@ -47,6 +47,7 @@ from gluonts.dataset.common import DataEntry, Dataset, ListDataset
 
 from gluonts.model.forecast import Forecast
 
+
 if TYPE_CHECKING:  # avoid circular import
     from gluonts.model.estimator import Estimator  # noqa
 
