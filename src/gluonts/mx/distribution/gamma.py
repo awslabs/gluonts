67ec113a75b74f15833ed77e3902409c7dc75376
eeehrlic <eeehrlic@amazon.com>
2020-09-28 15:14:38 +0200

    correcting NaN conditionals within certain distributions' log_prob() to handle samples outside distribution's support

--- src/gluonts/mx/distribution/gamma.py
+++ src/gluonts/mx/distribution/gamma.py
@@ -68,11 +68,26 @@ class Gamma(Distribution):
         F = self.F
         alpha, beta = self.alpha, self.beta
 
-        return (
-            alpha * F.log(beta)
-            - F.gammaln(alpha)
-            + (alpha - 1) * F.log(x)
-            - beta * x
+        def gamma_log_prob(x, alpha, beta):
+            return (
+                alpha * F.log(beta)
+                - F.gammaln(alpha)
+                + (alpha - 1) * F.log(x)
+                - beta * x
+            )
+
+        """
+        The gamma_log_prob(x) above returns NaNs for x<=0. Wherever there are NaN in either of the F.where() conditional
+        vectors, then F.where() returns NaN at that entry as well, due to its indicator function multiplication: 
+        1*f(x) + np.nan*0 = nan, since np.nan*0 return nan. 
+        Therefore replacing gamma_log_prob(x) with gamma_log_prob(abs(x) mitigates nan returns in cases of x<=0 without 
+        altering the value in cases of x>0. 
+        This is a known issue in pytorch as well https://github.com/pytorch/pytorch/issues/12986.
+        """
+        return F.where(
+            x > 0,
+            gamma_log_prob(F.abs(x), alpha, beta),
+            -np.inf * F.ones_like(x),
         )
 
     @property
