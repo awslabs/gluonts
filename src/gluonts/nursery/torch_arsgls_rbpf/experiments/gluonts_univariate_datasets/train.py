4501a675111c8f7eb92264c7457e63f71623dcd8
richardk <richardkurle@gmail.com>
2020-09-29 12:35:37 +0200

    remove assert for multi-GPU (as it has been resolved), corrected number of val bachtes, add ModelCheckpoint that also saves the last model (default only saves best).

--- src/gluonts/nursery/torch_arsgls_rbpf/experiments/gluonts_univariate_datasets/train.py
+++ src/gluonts/nursery/torch_arsgls_rbpf/experiments/gluonts_univariate_datasets/train.py
@@ -4,6 +4,7 @@ import numpy as np
 import mxnet as mx
 import torch
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 
 import consts
 from experiments.gluonts_univariate_datasets.config_arsgls import (
@@ -30,19 +31,6 @@ if __name__ == "__main__":
     parser.add_argument("-dtype", type=str, default="float64")
     args = parser.parse_args()
 
-    if not len(args.gpus) <= 1:
-        raise Exception(
-            "multi-GPU does not work anymore since we switched to "
-            "Pytorch-Lightning. The reason is that the SSMs are implemented "
-            "with time-first not batch-first. Although torch DataParallel can "
-            "handle this (dim arg), pytorch lightning does not."
-            "Will add support for this later through one of these options: "
-            "1) Make PR to lightning that allows time-first. "
-            "2) Re-write SSMs for batch-first. "
-            "3) In lightning Wrapper, just transpose before SSMs, "
-            "leaving SSM implementations as they are. (favorite solution)"
-        )
-
     # random seeds
     seed = args.run_nr if args.run_nr is not None else 0
     mx.random.seed(seed)
@@ -60,8 +48,12 @@ if __name__ == "__main__":
         gpus=args.gpus,
         default_root_dir=os.path.join(consts.log_dir, config.dataset_name),
         gradient_clip_val=config.grad_clip_norm,
-        limit_val_batches=(500 // config.batch_size_val),  # + 1,
+        limit_val_batches=int(np.ceil((500 / config.batch_size_val))),
         max_epochs=config.n_epochs,
+        checkpoint_callback=ModelCheckpoint(
+            monitor="val_checkpoint_on",
+            save_last=True,
+        ),
     )
 
     trainer.fit(model)
