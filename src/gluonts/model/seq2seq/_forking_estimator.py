fix scaling option default, fix scale shape

src/gluonts/model/seq2seq/_forking_estimator.py
@@ -124,7 +124,7 @@ class ForkingSeq2SeqEstimator(GluonEstimator):
     trainer
         trainer (default: Trainer())
     scaling
-        Whether to automatically scale the target values. (default: True)
+        Whether to automatically scale the target values. (default: False if quantile_output is used, True otherwise)
     scaling_decoder_dynamic_feature
         Whether to automatically scale the dynamic features for the decoder. (default: False)
     dtype
@@ -155,7 +155,7 @@ class ForkingSeq2SeqEstimator(GluonEstimator):
         enable_encoder_dynamic_feature: bool = True,
         enable_decoder_dynamic_feature: bool = True,
         trainer: Trainer = Trainer(),
-        scaling: bool = True,
+        scaling: Optional[bool] = None,
         scaling_decoder_dynamic_feature: bool = False,
         dtype: DType = np.float32,
         num_forking: Optional[int] = None,
@@ -222,7 +222,9 @@ class ForkingSeq2SeqEstimator(GluonEstimator):
         )
         self.enable_encoder_dynamic_feature = enable_encoder_dynamic_feature
         self.enable_decoder_dynamic_feature = enable_decoder_dynamic_feature
-        self.scaling = scaling
+        self.scaling = (
+            scaling if scaling is not None else (quantile_output is None)
+        )
         self.scaling_decoder_dynamic_feature = scaling_decoder_dynamic_feature
         self.dtype = dtype
 

src/gluonts/model/seq2seq/_forking_network.py
@@ -94,25 +94,20 @@ class ForkingSeq2SeqNetworkBase(gluon.HybridBlock):
         self.quantile_output = quantile_output
         self.scaling = scaling
         self.scaling_decoder_dynamic_feature = scaling_decoder_dynamic_feature
-        self.scaling_decoder_dynamic_feature_axis = 1
         self.dtype = dtype
         self.num_forking = (
             num_forking if num_forking is not None else context_length
         )
 
         if self.scaling:
-            self.scaler = MeanScaler(keepdims=True)
+            self.scaler = MeanScaler()
         else:
-            self.scaler = NOPScaler(keepdims=True)
+            self.scaler = NOPScaler()
 
         if self.scaling_decoder_dynamic_feature:
-            self.scaler_decoder_dynamic_feature = MeanScaler(
-                keepdims=True, axis=self.scaling_decoder_dynamic_feature_axis
-            )
+            self.scaler_decoder_dynamic_feature = MeanScaler(axis=1)
         else:
-            self.scaler_decoder_dynamic_feature = NOPScaler(
-                keepdims=True, axis=self.scaling_decoder_dynamic_feature_axis
-            )
+            self.scaler_decoder_dynamic_feature = NOPScaler(axis=1)
 
         with self.name_scope():
             if self.quantile_output:
@@ -167,9 +162,7 @@ class ForkingSeq2SeqNetworkBase(gluon.HybridBlock):
 
         # in addition to embedding features, use the log scale as it can help prediction too
         # (batch_size, num_feat_static = sum(embedding_dimension) + 1)
-        feat_static_real = F.concat(
-            embedded_cat, F.log(scale.squeeze(axis=1)), dim=1
-        )
+        feat_static_real = F.concat(embedded_cat, F.log(scale), dim=1)
 
         # Passing past_observed_values as a feature would allow the network to
         # make that distinction and possibly ignore the masked values.
@@ -266,7 +259,9 @@ class ForkingSeq2SeqTrainingNetwork(ForkingSeq2SeqNetworkBase):
         else:
             assert self.distr_output is not None
             distr_args = self.distr_args_proj(dec_output)
-            distr = self.distr_output.distribution(distr_args, scale=scale)
+            distr = self.distr_output.distribution(
+                distr_args, scale=scale.expand_dims(axis=1)
+            )
             loss = distr.loss(future_target)
 
         # mask the loss based on observed indicator