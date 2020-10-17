f97ed119ff9aba193b0381979cba4935a1a9336e
richardk <richardkurle@gmail.com>
2020-10-17 20:47:42 +0200

    add functions (lots of duplication) for predictions with marginalisation of states, producing smoother forecasts.

--- src/gluonts/nursery/torch_arsgls_rbpf/models/sgls_rbpf.py
+++ src/gluonts/nursery/torch_arsgls_rbpf/models/sgls_rbpf.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Sequence, Union, Tuple
 from dataclasses import dataclass
 
 from box import Box
@@ -253,37 +253,39 @@ class SwitchingGaussianLinearSystemBaseRBSMC(BaseRBSMCGaussianLinearSystem):
         )
 
         x_t = x_dist_t.mean if deterministic else x_dist_t.sample()
-        lats_t = LatentsSGLS(
-            log_weights=lats_tm1.log_weights,  # does not change w/o evidence.
-            gls_params=None,  # not used outside this function
-            variables=GLSVariablesSGLS(
-                x=x_t, m=None, V=None, Cov=None, switch=s_t,
-            ),
-        )
-
+        (m_t, V_t) = (None, None)
         # emission_dist = self.emit(lats_t=lats_t, ctrl_t=ctrl_t)
         emission_dist_t = torch.distributions.MultivariateNormal(
             loc=matvec(gls_params_t.C, x_t)
             + (gls_params_t.d if gls_params_t.d is not None else 0.0),
-            scale_tril=cholesky(gls_params_t.Q),
+            scale_tril=gls_params_t.LQ,
         )
         emissions_t = (
             emission_dist_t.mean if deterministic else emission_dist_t.sample()
         )
 
+        # NOTE: Should compute Cov if need forecast joint distribution.
+        lats_t = LatentsSGLS(
+            log_weights=lats_tm1.log_weights,  # does not change w/o evidence.
+            gls_params=None,  # not used outside this function
+            variables=GLSVariablesSGLS(
+                x=x_t, m=m_t, V=V_t, Cov=None, switch=s_t,
+            ),
+        )
         return Prediction(latents=lats_t, emissions=emissions_t)
 
     def _sample_initial_latents(self, n_particle, n_batch,) -> LatentsSGLS:
         state_prior = self.state_prior_model(
             None, batch_shape_to_prepend=(n_particle, n_batch)
         )
-        x_initial = state_prior.sample()
         s_initial = None  # initial step has no switch sample.
+        x_initial = state_prior.sample()
+        (m, V) = (None, None)
         return LatentsSGLS(
             log_weights=torch.zeros_like(state_prior.loc[..., 0]),
             gls_params=None,  # initial step has none
             variables=GLSVariablesSGLS(
-                x=x_initial, m=None, V=None, Cov=None, switch=s_initial,
+                x=x_initial, m=m, V=V, Cov=None, switch=s_initial,
             ),
         )
 
@@ -295,11 +297,24 @@ class SwitchingGaussianLinearSystemBaseRBSMC(BaseRBSMCGaussianLinearSystem):
         gls_params_t = self.gls_base_parameters(
             switch=lats_t.variables.switch, controls=ctrl_t,
         )
-        return torch.distributions.MultivariateNormal(
-            loc=matvec(gls_params_t.C, lats_t.variables.x)
-            + (gls_params_t.d if gls_params_t.d is not None else 0.0),
-            scale_tril=cholesky(gls_params_t.Q),
-        )
+
+        if lats_t.variables.m is not None:  # marginalise states
+            mpy_t, Vpy_t = filter_forward_predictive_distribution(
+                m=lats_t.variables.m,
+                V=lats_t.variables.V,
+                Q=gls_params_t.Q,
+                C=gls_params_t.C,
+                d=gls_params_t.d,
+            )
+            return MultivariateNormal(
+                loc=mpy_t, scale_tril=cholesky(Vpy_t),
+            )
+        else:  # emit from state sample
+            return torch.distributions.MultivariateNormal(
+                loc=matvec(gls_params_t.C, lats_t.variables.x)
+                + (gls_params_t.d if gls_params_t.d is not None else 0.0),
+                scale_tril=gls_params_t.LQ,
+            )
 
     def _make_encoder_dists(
         self, tar_t: torch.Tensor, ctrl_t: ControlInputsSGLS,
@@ -359,3 +374,153 @@ class SwitchingGaussianLinearSystemBaseRBSMC(BaseRBSMCGaussianLinearSystem):
                 [switch_model_dist, switch_encoder_dist]
             )
         return switch_proposal_dist
+
+    # TODO: everything below code duplication.
+    #  Currently quick solution only for re-producing pendulum plots
+    def predict_marginals(
+        self,
+        # prediction_length would be misleading as prediction includes past.
+        n_steps_forecast: int,
+        past_targets: [Sequence[torch.Tensor], torch.Tensor],
+        past_controls: Optional[
+            Union[Sequence[ControlInputs], ControlInputs]
+        ] = None,
+        future_controls: Optional[Sequence[ControlInputs]] = None,
+        deterministic: bool = False,
+        smooth_past: bool = False,
+    ) -> Tuple[Sequence[Prediction], Sequence[Prediction]]:  # past & future
+        if smooth_past:
+            latents_inferred = self.smooth(
+                past_targets=past_targets, past_controls=past_controls,
+            )
+        else:
+            latents_inferred = self.filter(
+                past_targets=past_targets, past_controls=past_controls,
+            )
+
+        emission_dist_inferred = [
+            self.emit(lats_t=latents_inferred[t], ctrl_t=past_controls[t])
+            for t in range(len(latents_inferred))
+        ]
+        predictions_inferred = [
+            Prediction(latents=l, emissions=e)
+            for l, e in zip(latents_inferred, emission_dist_inferred)
+        ]
+
+        predictions_forecast = self.forecast_marginals(
+            n_steps_forecast=n_steps_forecast,
+            initial_latent=latents_inferred[-1],
+            future_controls=future_controls,
+            deterministic=deterministic,
+        )
+        return predictions_inferred, predictions_forecast
+
+    def forecast_marginals(
+        self,
+        n_steps_forecast: int,
+        initial_latent: LatentsSGLS,
+        future_controls: Optional[
+            Union[Sequence[ControlInputs], ControlInputs]
+        ] = None,
+        deterministic: bool = False,
+    ) -> Sequence[Prediction]:
+
+        initial_latent, future_controls = self._prepare_forecast(
+            initial_latent=initial_latent,
+            controls=future_controls,
+            deterministic=deterministic,
+        )
+
+        return self._marginal_trajectory_from_initial(
+            n_steps_forecast=n_steps_forecast,
+            initial_latent=initial_latent,
+            future_controls=future_controls,
+            deterministic=deterministic,
+        )
+
+    def _marginal_trajectory_from_initial(
+        self,
+        n_steps_forecast: int,
+        initial_latent: LatentsSGLS,
+        future_controls: Optional[
+            Union[Sequence[ControlInputs], ControlInputs]
+        ] = None,
+        deterministic: bool = False,
+    ) -> Sequence[Prediction]:
+        # initial_latent is considered t == -1
+
+        if future_controls is not None:
+            assert n_steps_forecast == len(future_controls)
+
+        controls = (
+            [None] * n_steps_forecast
+            if future_controls is None
+            else future_controls
+        )
+        samples = [None] * n_steps_forecast
+
+        for t in range(n_steps_forecast):
+            samples[t] = self.marginal_step(
+                lats_tm1=samples[t - 1].latents if t > 0 else initial_latent,
+                ctrl_t=controls[t],
+                deterministic=deterministic,
+            )
+        return samples
+
+    def marginal_step(
+            self,
+            lats_tm1: LatentsSGLS,
+            ctrl_t: ControlInputsSGLS,
+            deterministic: bool = False,
+    ) -> Prediction:
+        # TODO: duplication with sample_step. Requires refactoring.
+        n_batch = lats_tm1.variables.m.shape[1]
+
+        if lats_tm1.variables.switch is None:
+            switch_model_dist_t = self._make_switch_prior_dist(
+                n_particle=self.n_particle,
+                n_batch=n_batch,
+                lat_vars_tm1=lats_tm1.variables,
+                ctrl_t=ctrl_t,
+            )
+        else:
+            switch_model_dist_t = self._make_switch_transition_dist(
+                lat_vars_tm1=lats_tm1.variables, ctrl_t=ctrl_t,
+            )
+
+        s_t = (
+            switch_model_dist_t.mean
+            if deterministic
+            else switch_model_dist_t.sample()
+        )
+        gls_params_t = self.gls_base_parameters(switch=s_t, controls=ctrl_t,)
+
+        x_t = None
+        m_t, V_t = filter_forward_prediction_step(
+            m=lats_tm1.variables.m,
+            V=lats_tm1.variables.V,
+            R=gls_params_t.R,
+            A=gls_params_t.A,
+            b=gls_params_t.b,
+        )
+        mpy_t, Vpy_t = filter_forward_predictive_distribution(
+            m=m_t,
+            V=V_t,
+            Q=gls_params_t.Q,
+            C=gls_params_t.C,
+            d=gls_params_t.d,
+        )
+        emission_dist_t = torch.distributions.MultivariateNormal(
+            loc=mpy_t,
+            scale_tril=cholesky(Vpy_t),
+        )
+
+        # NOTE: Should compute Cov if need forecast joint distribution.
+        lats_t = LatentsSGLS(
+            log_weights=lats_tm1.log_weights,  # does not change w/o evidence.
+            gls_params=None,  # not used outside this function
+            variables=GLSVariablesSGLS(
+                x=x_t, m=m_t, V=V_t, Cov=None, switch=s_t,
+            ),
+        )
+        return Prediction(latents=lats_t, emissions=emission_dist_t)
