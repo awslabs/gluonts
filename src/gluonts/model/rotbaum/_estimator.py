e53f336ff68c44fda8aae051b4c8737c43c8b3ef
Lorenzo Stella <lorenzostella@gmail.com>
2020-11-11 23:26:07 +0100

    Fix rotbaum (#1141)
    * fix inherited classes for rotbaum
    
    * remove call in favor of train

--- src/gluonts/model/rotbaum/_estimator.py
+++ src/gluonts/model/rotbaum/_estimator.py
@@ -11,20 +11,17 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-# Third-party imports
-from mxnet.gluon import HybridBlock
-
 # First-party imports
 from gluonts.core.component import validated
 from gluonts.dataset.common import Dataset
-from gluonts.model.estimator import GluonEstimator, Predictor, TrainOutput
+from gluonts.model.estimator import Estimator, Predictor
 from gluonts.transform import FilterTransformation, Transformation
 
 # Relative imports
 from ._predictor import TreePredictor
 
 
-class ThirdPartyEstimator(GluonEstimator):
+class ThirdPartyEstimator(Estimator):
     """
     An `Estimator` that uses an external fitting mechanism, thus eliminating
     the need for a Trainer. Differs from DummyEstimator in that DummyEstimator
@@ -45,23 +42,7 @@ class ThirdPartyEstimator(GluonEstimator):
     def train(
         self, training_data: Dataset, validation_dataset=None
     ) -> Predictor:
-        return self.predictor(training_data)
-
-    def train_model(self):
-        return TrainOutput(
-            transformation=self.create_transformation(),
-            trained_net=self.create_training_network(),
-            predictor=self.create_predictor(),
-        )
-
-    def create_predictor(self) -> Predictor:
-        return self.predictor
-
-    def create_transformation(self) -> Transformation:
-        return FilterTransformation(lambda x: True)
-
-    def create_training_network(self) -> HybridBlock:
-        return HybridBlock()
+        return self.predictor.train(training_data)
 
 
 class TreeEstimator(ThirdPartyEstimator):
