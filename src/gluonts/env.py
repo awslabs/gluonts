fe1075c1f428c0cb3d06206e973a0068c5dca238
Jasper Zschiegner <schjaspe@amazon.de>
2021-01-11 22:36:02 +0100

    Re-added comment.

--- src/gluonts/env.py
+++ src/gluonts/env.py
@@ -17,6 +17,10 @@ from .core.context import Context
 
 
 class Environment(Context):
+    # Maximum number of times a transformation can receive an input without
+    # returning an output. This parameter is intended to catch infinite loops
+    # or inefficiencies, when transformations never or rarely return
+    # something.
     max_idle_transforms: int = os.environ.get(
         "GLUONTS_MAX_IDLE_TRANSFORMS", "100"
     )
