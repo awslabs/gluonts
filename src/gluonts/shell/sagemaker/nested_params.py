9039132c5ae3ebe445ed44bd9b7b18a15ca685d1
Jasper Zschiegner <schjaspe@amazon.de>
2021-01-19 13:32:53 +0100

    Using serde.flat.

--- src/gluonts/shell/sagemaker/nested_params.py
+++ src/gluonts/shell/sagemaker/nested_params.py
@@ -16,84 +16,9 @@ from itertools import count
 from pydoc import locate
 from typing import List
 
-from toolz.dicttoolz import keymap
+from toolz.dicttoolz import keymap, valmap
 
 from gluonts.core import serde
-
-
-def asdict(trie):
-    if not isinstance(trie, defaultdict):
-        return trie
-    return {k: asdict(v) for k, v in trie.items()}
-
-
-class Trie:
-    def __init__(self, dct=None):
-        trie = lambda: defaultdict(trie)
-        self.trie = trie()
-
-        if dct is not None:
-            for key, val in dct.items():
-                self[key] = val
-
-    def __setitem__(self, path, value):
-        trie = self.trie
-        for part in path[:-1]:
-            trie = trie[part]
-
-        trie[path[-1]] = value
-
-    def asdict(self):
-        return asdict(self.trie)
-
-
-def load_references(parameters: dict) -> dict:
-    """Return a copy of `parameters`, where we load objects, where keys end in
-    `@`:
-
-    >>> load_references({"@": "operator.add"})
-    {'@': <built-in function add>}
-    """
-    copy = dict(parameters)
-    copy.update(
-        {
-            key: locate(value)
-            for key, value in parameters.items()
-            if key[-1] == "@"
-        }
-    )
-    return copy
-
-
-def eval_nested(data):
-    if isinstance(data, dict) and "@" in data:
-        Type = data.pop("@")
-
-        args = []
-        for idx in map(str, count(start=0)):
-            if idx not in data:
-                break
-            args.append(eval_nested(data.pop(idx)))
-
-        kwargs = {key: eval_nested(value) for key, value in data.items()}
-        return Type(*args, **kwargs)
-
-    return data
-
-
-def load(mapping: dict) -> object:
-    def split_path(s):
-        return tuple(s.split("."))
-
-    pathes = keymap(split_path, mapping)
-    objects = load_references(pathes)
-    nested = Trie(objects).asdict()
-    return eval_nested(nested)
-
-
-def decode_nested_parameters(parameters: dict) -> dict:
-    inputs = split_by_prefix(parameters)
-    return {prefix: load(args) for prefix, args in inputs.items()}
 
 
 def split_by_prefix(data: dict) -> dict:
@@ -118,33 +43,10 @@ def split_by_prefix(data: dict) -> dict:
     return dict(namespace)
 
 
-class Path:
-    def __init__(self, *parts):
-        self.parts = list(parts)
-
-    def __truediv__(self, other):
-        return Path(*self.parts + [other])
-
-    def __repr__(self):
-        return ".".join(self.parts)
-
-
-def _encode(data, path, result):
-    if isinstance(data, dict) and data.get("__kind__") == "instance":
-        result[str(path / "@")] = data["class"]
-
-        for n, arg in enumerate(data["args"]):
-            _encode(arg, path / f"{n}", result)
-
-        for name, value in data["kwargs"].items():
-            _encode(value, path / name, result)
-    else:
-        result[str(path)] = data
+def decode_nested_parameters(parameters: dict) -> dict:
+    inputs = split_by_prefix(parameters)
+    return valmap(serde.flat.decode, inputs)
 
 
 def encode_nested_parameters(obj) -> dict:
-    encoded = serde.encode(obj)
-
-    result: dict = {}
-    _encode(encoded, Path(), result)
-    return result
+    return serde.flat.encode(obj)
