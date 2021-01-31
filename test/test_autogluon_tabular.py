b4f64f9e843b31baad91fc4afe650045cee4fe51
caixiyan <xc984@nyu.edu>
2021-01-31 18:02:10 +0800

    added a test function for dataframe conversion. However, the decimal precision is changed during the process of dataframe conversion. Therefore, I rounded the decimals into 2.

--- test/test_autogluon_tabular.py
+++ test/test_autogluon_tabular.py
@@ -14,12 +14,47 @@
 
 import pandas as pd
 import numpy as np
-from gluonts.nursery.autogluon_tabular import LocalTabularPredictor
+from gluonts.nursery.autogluon_tabular import LocalTabularPredictor, get_prediction_dataframe
 from gluonts.dataset.common import ListDataset
 from gluonts.dataset.util import to_pandas
 
+
 def test_autogluon_tabular():
     # create a dataset
+    dataset = ListDataset(
+        [
+            {
+                "start": pd.Timestamp("1750-01-04 00:00:00", freq="W-SUN"),
+                "target": np.array(
+                    [1089.2, 1078.91, 1099.88, 35790.55, 34096.95, 34906.95],
+                ),
+            },
+            {
+                "start": pd.Timestamp("1750-01-04 00:00:00", freq="W-SUN"),
+                "target": np.array(
+                    [1099.2, 1098.91, 1069.88, 35990.55, 34076.95, 34766.95],
+                ),
+            },
+        ],
+        freq="W-SUN",
+    )
+    prediction_length = 5
+    freq = "W-SUN"
+    predictor = LocalTabularPredictor(
+        freq=freq, prediction_length=prediction_length, use_lag=True, lags=[]
+    )
+    forecasts_it = predictor.predict(dataset)
+    forecasts = list(forecasts_it)
+
+    for entry, forecast in zip(dataset, forecasts):
+        ts = to_pandas(entry)
+        start_timestamp = ts.index[-1] + pd.tseries.frequencies.to_offset(freq)
+        assert forecast.samples.shape[1] == prediction_length
+        assert forecast.start_date == start_timestamp
+    return forecasts
+
+
+def test_get_dataframe():
     dataset = ListDataset(
         [
             {
@@ -39,19 +74,15 @@ def test_autogluon_tabular():
         freq="W-SUN",
     )
     prediction_length = 5
-    freq = "W-SUN"
-    predictor = LocalTabularPredictor(
-        freq=freq, prediction_length=prediction_length, use_lag=True, lags=[]
-    )
-    forecasts_it = predictor.predict(dataset)
-    forecasts = list(forecasts_it)
+    ts = to_pandas(list(dataset)[0])
+    df = get_prediction_dataframe(ts, prediction_length, [1, 2, 5], train=True)
+    df[['lag1', 'lag2', 'lag5']] = df[['lag1', 'lag2', 'lag5']].apply(lambda x: round(x, 2))
+    ls = list(df.loc[5, ['lag1', 'lag2', 'lag5']].values)
+    assert np.array(list(i == j for i, j in zip(ls, [34096.95, 35790.55, 1089.2]))).all()
+    return df
 
-    for entry, forecast in zip(dataset, forecasts):
-        ts = to_pandas(entry)
-        start_timestamp = ts.index[-1] + pd.tseries.frequencies.to_offset(freq)
-        assert forecast.samples.shape[1] == prediction_length
-        assert forecast.start_date == start_timestamp
-    return forecasts
+
 
 if __name__ == "__main__":
     print(test_autogluon_tabular())
+    print(test_get_dataframe())
