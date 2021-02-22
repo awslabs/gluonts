409821075f52819e294ef48e2d4a7eddbfb4b58f
Eduard Uffelmann <uffeledu@amazon.com>
2021-02-22 15:33:58 +0100

    added logging import

--- src/gluonts/dataset/common.py
+++ src/gluonts/dataset/common.py
@@ -10,6 +10,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+import logging
 from enum import Enum
 from functools import lru_cache
 from pathlib import Path
