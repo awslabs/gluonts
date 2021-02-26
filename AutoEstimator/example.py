9d0068b0cdaba9f78ca77d440bf6808bec86b60a
YuntianYeAWS <66420613+YuntianYeAWS@users.noreply.github.com>
2021-02-26 22:26:13 +0800

    Update and rename main.py to example.py

--- AutoEstimator/example.py
+++ AutoEstimator/example.py
@@ -6,8 +6,11 @@ from automodel import AutoEstimator
 
 from dataset import dataset
 prediction_length = dataset.metadata.prediction_length
+
+#create dictionary of hyperparameter 
 dictionary_of_hyperparameters = {}
 
+#Model and Metric are normally neccesary: default model:SimpleFeedForwardEstimator, default metric: MAPE
 dictionary_of_hyperparameters['model'] = ag.Categorical(SimpleFeedForwardEstimator,MQCNNEstimator)
 dictionary_of_hyperparameters['metric'] = 'MAPE'
 
@@ -15,11 +18,14 @@ dictionary_of_hyperparameters['metric'] = 'MAPE'
 dictionary_of_hyperparameters['epochs']  = 5
 dictionary_of_hyperparameters['learning_rate'] = ag.Real(1e-3, 1e-2, log=True)
 
+#Dataset Hyperparameters: usually unmutable, except for context length in this case
 dictionary_of_hyperparameters['freq'] = dataset.metadata.freq
 dictionary_of_hyperparameters['prediction_length'] = dataset.metadata.prediction_length
 dictionary_of_hyperparameters['context_length'] = ag.Int(2 * prediction_length, 10 * prediction_length)
-#deepar, ssf, mqcnn in terms of wQuantileLoss
+
+#create a new estimator by register AutoEstimator with a dictionary of hyperparameter 
 estimator = AutoEstimator(dictionary_of_hyperparameters)
 
+#train with train data and test data
 estimator.train(dataset_train=dataset.train,dataset_test=dataset.test)
 # best_estimator = estimator.get_best_estimator()
