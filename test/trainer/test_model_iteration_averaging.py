fix edge case in iteration based model averaging

test/trainer/test_model_iteration_averaging.py
@@ -53,10 +53,7 @@ def initialize_model() -> nn.HybridBlock:
         context_length=T,
         freq=freq,
         trainer=Trainer(
-            ctx="cpu",
-            epochs=1,
-            learning_rate=1e-3,
-            num_batches_per_epoch=1,
+            ctx="cpu", epochs=1, learning_rate=1e-3, num_batches_per_epoch=1,
         ),
     )
 
@@ -73,8 +70,13 @@ def initialize_model() -> nn.HybridBlock:
 def test_NTA_V1(n: int, last_n_trigger: bool):
     model = initialize_model()
     params = model.collect_params()
-    avg_strategy = NTA(n=n, last_n_trigger=last_n_trigger)
     loss_list = [5, 4, 3, 2, 3, 3, 3, 3, 3, 3, 3]
+    avg_strategy = NTA(
+        epochs=len(loss_list),
+        n=n,
+        last_n_trigger=last_n_trigger,
+        fallback_alpha=0.0,
+    )
     for i, loss in enumerate(loss_list):
         for k, v in params.items():
             for arr in v.list_data():