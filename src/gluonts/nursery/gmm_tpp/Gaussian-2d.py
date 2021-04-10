f89b4af30ccb35eb191d638a48261f7280bea72b
Yifei Ma <yifeim@amazon.com>
2021-04-10 01:21:01 -0700

    style check

--- src/gluonts/nursery/gmm_tpp/Gaussian-2d.py
+++ src/gluonts/nursery/gmm_tpp/Gaussian-2d.py
@@ -39,9 +39,9 @@ gnd_cov_L_ = np.random.RandomState(1).randn(n_clusters, ndims, ndims) * 0.03
 
 
 z = (np.random.RandomState(2).rand(n_sample) * n_clusters).astype(int)
-x = gnd_mu_[z] + (gnd_cov_L_[z] @
-                  np.random.RandomState(3).randn(n_sample, ndims)[:,:,None]
-                 ).squeeze(axis=-1)
+x = gnd_mu_[z] + (
+    gnd_cov_L_[z] @ np.random.RandomState(3).randn(n_sample, ndims)[:, :, None]
+).squeeze(axis=-1)
 
 
 # In[6]:
@@ -49,11 +49,11 @@ x = gnd_mu_[z] + (gnd_cov_L_[z] @
 
 def plot_cov(mean, cov, **kw):
     vec, v2, _ = np.linalg.svd(cov)
-    val = v2**0.5
+    val = v2 ** 0.5
     for r in range(len(val)):
-        handle = pl.plot(*zip(
-            mean - vec[:,r]*val[r],
-            mean + vec[:,r]*val[r]), **kw)
+        handle = pl.plot(
+            *zip(mean - vec[:, r] * val[r], mean + vec[:, r] * val[r]), **kw
+        )
     return handle
 
 
@@ -61,7 +61,7 @@ def plot_cov(mean, cov, **kw):
 
 
 def Gaussian_log_pdf(ex, g_mean, g_cov):
-    model = GMMModel(1, g_mean.size, g_mean[None,:], g_cov[None,:,:])
+    model = GMMModel(1, g_mean.size, g_mean[None, :], g_cov[None, :, :])
     model.initialize()
     return model(mx.nd.array(ex))[0].asnumpy()
 
@@ -79,28 +79,43 @@ def mixture_full_log_pdf(x, mu_, kR_):
 
 
 g_mean = x.mean(axis=0)
-g_cov = ((x-g_mean[None,:])[:,:,None] @ (x-g_mean[None,:])[:,None,:]).mean(axis=0)
+g_cov = (
+    (x - g_mean[None, :])[:, :, None] @ (x - g_mean[None, :])[:, None, :]
+).mean(axis=0)
 
 
 # In[10]:
 
 
-handle0, = pl.plot(x[:,0], x[:,1], '.', zorder=-1, label='empirical')
+(handle0,) = pl.plot(x[:, 0], x[:, 1], ".", zorder=-1, label="empirical")
 
 for c in range(n_clusters):
-    handle1, = plot_cov(gnd_mu_[c], gnd_cov_L_[c] @ gnd_cov_L_[c].T,
-                       color='k', lw=4, label='gnd')
+    (handle1,) = plot_cov(
+        gnd_mu_[c],
+        gnd_cov_L_[c] @ gnd_cov_L_[c].T,
+        color="k",
+        lw=4,
+        label="gnd",
+    )
 
-handle2, = plot_cov(g_mean, g_cov, color='C1', lw=4, label='Gaussian')
+(handle2,) = plot_cov(g_mean, g_cov, color="C1", lw=4, label="Gaussian")
 
-pl.legend([handle0, handle1, handle2], [
-    'empirical',
-    'gnd loglik={:.3f}'.format(mixture_full_log_pdf(
-        x, gnd_mu_, np.linalg.inv(gnd_cov_L_)).mean()),
-    'Gaussian loglik={:.3f}'.format(Gaussian_log_pdf(
-        x, g_mean, np.linalg.inv(np.linalg.cholesky(g_cov))).mean())
-], loc='upper right')
-pl.axis('square')
+pl.legend(
+    [handle0, handle1, handle2],
+    [
+        "empirical",
+        "gnd loglik={:.3f}".format(
+            mixture_full_log_pdf(x, gnd_mu_, np.linalg.inv(gnd_cov_L_)).mean()
+        ),
+        "Gaussian loglik={:.3f}".format(
+            Gaussian_log_pdf(
+                x, g_mean, np.linalg.inv(np.linalg.cholesky(g_cov))
+            ).mean()
+        ),
+    ],
+    loc="upper right",
+)
+pl.axis("square")
 
 
 # # full rank
@@ -122,38 +137,49 @@ for epoch in range(100):
 
 mu_ = model.mu_.data().asnumpy()
 kR_ = model.kR_.data().asnumpy()
-cov_ = np.linalg.inv(kR_.swapaxes(1,2) @ kR_)
+cov_ = np.linalg.inv(kR_.swapaxes(1, 2) @ kR_)
 
 
 # In[13]:
 
 
-handle0, = pl.plot(x[:,0], x[:,1], '.', zorder=-1, label='empirical')
+(handle0,) = pl.plot(x[:, 0], x[:, 1], ".", zorder=-1, label="empirical")
 
 for c in range(n_clusters):
-    handle1, = plot_cov(gnd_mu_[c], gnd_cov_L_[c] @ gnd_cov_L_[c].T,
-                       color='k', lw=4, label='gnd')
+    (handle1,) = plot_cov(
+        gnd_mu_[c],
+        gnd_cov_L_[c] @ gnd_cov_L_[c].T,
+        color="k",
+        lw=4,
+        label="gnd",
+    )
 
-handle2, = plot_cov(g_mean, g_cov, color='C1', lw=4, label='Gaussian')
+(handle2,) = plot_cov(g_mean, g_cov, color="C1", lw=4, label="Gaussian")
 
 for c in range(n_clusters):
-    handle3, = plot_cov(mu_[c], cov_[c], color='C1', lw=2,
-                        label='EM full rank')
+    (handle3,) = plot_cov(
+        mu_[c], cov_[c], color="C1", lw=2, label="EM full rank"
+    )
 
-pl.legend([handle0, handle1, handle2, handle3], [
-    'empirical',
-    'gnd loglik={:.3f}'.format(mixture_full_log_pdf(
-        x, gnd_mu_, np.linalg.inv(gnd_cov_L_)).mean()),
-    'Gaussian loglik={:.3f}'.format(Gaussian_log_pdf(
-        x, g_mean, np.linalg.inv(np.linalg.cholesky(g_cov))).mean()),
-    'mixture loglik={:.3f}'.format(mixture_full_log_pdf(
-        x, mu_, kR_).mean()),
-], loc='upper right')
-pl.axis('square')
+pl.legend(
+    [handle0, handle1, handle2, handle3],
+    [
+        "empirical",
+        "gnd loglik={:.3f}".format(
+            mixture_full_log_pdf(x, gnd_mu_, np.linalg.inv(gnd_cov_L_)).mean()
+        ),
+        "Gaussian loglik={:.3f}".format(
+            Gaussian_log_pdf(
+                x, g_mean, np.linalg.inv(np.linalg.cholesky(g_cov))
+            ).mean()
+        ),
+        "mixture loglik={:.3f}".format(
+            mixture_full_log_pdf(x, mu_, kR_).mean()
+        ),
+    ],
+    loc="upper right",
+)
+pl.axis("square")
 
 
 # In[ ]:
-
-
-
-
--- src/gluonts/nursery/gmm_tpp/exponential-1d.py
+++ src/gluonts/nursery/gmm_tpp/exponential-1d.py
@@ -7,7 +7,8 @@
 import numpy as np
 import scipy as sp
 import pylab as pl
-pl.rcParams['figure.figsize'] = 4,4
+
+pl.rcParams["figure.figsize"] = 4, 4
 
 
 # In[2]:
@@ -20,7 +21,7 @@ from GMMBase import *
 
 
 lamb = 0.5
-xmax = 4/lamb
+xmax = 4 / lamb
 n_sample = int(1e4)
 n_clusters = 3
 epochs = 1000
@@ -29,30 +30,38 @@ epochs = 1000
 # In[4]:
 
 
-x = np.random.RandomState(0).exponential(1./lamb, n_sample)
+x = np.random.RandomState(0).exponential(1.0 / lamb, n_sample)
 
 
 # In[5]:
 
 
 def Gaussian_pdf(ex):
-    return np.exp(- 0.5*(ex-x.mean())**2/x.std()**2
-                  - 0.5*np.log(2*np.pi*x.std()**2))
+    return np.exp(
+        -0.5 * (ex - x.mean()) ** 2 / x.std() ** 2
+        - 0.5 * np.log(2 * np.pi * x.std() ** 2)
+    )
 
 
 # In[6]:
 
 
 ex = np.linspace(0, xmax)
-pl.hist(x, ex, density=True, fill=False, histtype='step',
-        label='empirical');
-pl.plot(ex, lamb*np.exp(-lamb*ex), label='exponential')
+pl.hist(x, ex, density=True, fill=False, histtype="step", label="empirical")
+pl.plot(ex, lamb * np.exp(-lamb * ex), label="exponential")
 
-pl.plot(ex, Gaussian_pdf(ex), label='Gaussian')
-pl.errorbar([x.mean()], Gaussian_pdf([x.mean()]), None, [x.std()],
-            'x', capsize=2, label='Gaussian mode')
+pl.plot(ex, Gaussian_pdf(ex), label="Gaussian")
+pl.errorbar(
+    [x.mean()],
+    Gaussian_pdf([x.mean()]),
+    None,
+    [x.std()],
+    "x",
+    capsize=2,
+    label="Gaussian mode",
+)
 
-pl.legend(loc='upper right')
+pl.legend(loc="upper right")
 pl.xlim(xmax=xmax)
 
 
@@ -66,8 +75,8 @@ model.initialize()
 trainer = GMMTrainer(model)
 
 for epoch in range(100):
-    trainer(x[:,None])
-    print(epoch, model(mx.nd.array(x[:,None]))[0].mean().asscalar())
+    trainer(x[:, None])
+    print(epoch, model(mx.nd.array(x[:, None]))[0].mean().asscalar())
 
 
 # In[8]:
@@ -75,7 +84,7 @@ for epoch in range(100):
 
 mu_ = model.mu_.data().asnumpy()
 kR_ = model.kR_.data().asnumpy()
-cov_ = np.linalg.inv(kR_.swapaxes(1,2) @ kR_)
+cov_ = np.linalg.inv(kR_.swapaxes(1, 2) @ kR_)
 s2_ = np.array([np.diag(c) for c in cov_])
 
 
@@ -85,7 +94,7 @@ s2_ = np.array([np.diag(c) for c in cov_])
 def mixture_pdf(ex):
     model = GMMModel(n_clusters, 1, mu_, kR_)
     model.initialize()
-    log_marg = model(mx.nd.array(ex, dtype='float32'))[0]
+    log_marg = model(mx.nd.array(ex, dtype="float32"))[0]
     return log_marg.exp().asnumpy()
 
 
@@ -93,21 +102,24 @@ def mixture_pdf(ex):
 
 
 ex = np.linspace(0, xmax)
-pl.hist(x, ex, density=True, fill=False, histtype='step',
-        label='empirical pdf')
-pl.plot(ex, lamb*np.exp(-lamb*ex), label='exponential pdf')
+pl.hist(
+    x, ex, density=True, fill=False, histtype="step", label="empirical pdf"
+)
+pl.plot(ex, lamb * np.exp(-lamb * ex), label="exponential pdf")
 
-pl.plot(ex, mixture_pdf(ex[:,None]), '--', label='mixture pdf')
-pl.errorbar(mu_.squeeze(-1), mixture_pdf(mu_),
-            None, s2_.squeeze(-1)**0.5,
-            'x', capsize=2, label='mixture modes')
+pl.plot(ex, mixture_pdf(ex[:, None]), "--", label="mixture pdf")
+pl.errorbar(
+    mu_.squeeze(-1),
+    mixture_pdf(mu_),
+    None,
+    s2_.squeeze(-1) ** 0.5,
+    "x",
+    capsize=2,
+    label="mixture modes",
+)
 
-pl.legend(loc='upper right')
+pl.legend(loc="upper right")
 pl.xlim(xmax=xmax)
 
 
 # In[ ]:
-
-
-
-
