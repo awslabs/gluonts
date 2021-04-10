ec662e1bb44bc656a9bac9c7fa2ef6e7a5839b66
Yifei Ma <yifeim@amazon.com>
2021-04-10 01:19:43 -0700

    style check

--- src/gluonts/nursery/gmm_tpp/GMMBase.py
+++ src/gluonts/nursery/gmm_tpp/GMMBase.py
@@ -4,6 +4,7 @@ import pylab as pl
 import mxnet as mx
 from mxnet import gluon
 
+
 class GMMModel(gluon.HybridBlock):
     """
     log p(x) >= E_{q(z|x)}[ log p(x|z) + log p(z) - log q(z|x) ]
@@ -21,20 +22,26 @@ class GMMModel(gluon.HybridBlock):
     qz: (batch_size, num_clusters)
     """
 
-    def __init__(self, num_clusters, num_hidden, mu_=None, kR_=None, lr_mult=None):
+    def __init__(
+        self, num_clusters, num_hidden, mu_=None, kR_=None, lr_mult=None
+    ):
         super().__init__()
         self.num_hidden = num_hidden
 
         with self.name_scope():
             self.mu_ = self.params.get(
-                'mu_', shape=(num_clusters, num_hidden),
+                "mu_",
+                shape=(num_clusters, num_hidden),
                 lr_mult=lr_mult,
-                init=None if mu_ is None else mx.init.Constant(mu_))
+                init=None if mu_ is None else mx.init.Constant(mu_),
+            )
 
             self.kR_ = self.params.get(
-                'kR_', shape=(num_clusters, num_hidden, num_hidden),
+                "kR_",
+                shape=(num_clusters, num_hidden, num_hidden),
                 lr_mult=lr_mult,
-                init=None if kR_ is None else mx.init.Constant(kR_))
+                init=None if kR_ is None else mx.init.Constant(kR_),
+            )
 
     @staticmethod
     def _get_dx_(F, x, mu_):
@@ -45,11 +52,10 @@ class GMMModel(gluon.HybridBlock):
     def _get_Rx_(F, dx_, kR_):
         """ @Return (batch_size, num_clusters, num_hidden) """
         kR_expand_0 = F.broadcast_like(
-            kR_.expand_dims(0), dx_,
-            lhs_axes=(0,), rhs_axes=(0,))
+            kR_.expand_dims(0), dx_, lhs_axes=(0,), rhs_axes=(0,)
+        )
 
-        Rx_ = F.batch_dot(kR_expand_0,
-                          dx_.expand_dims(-1)).squeeze(axis=-1)
+        Rx_ = F.batch_dot(kR_expand_0, dx_.expand_dims(-1)).squeeze(axis=-1)
         return Rx_
 
     def hybrid_forward(self, F, x, mu_, kR_):
@@ -57,9 +63,11 @@ class GMMModel(gluon.HybridBlock):
         dx_ = self._get_dx_(F, x, mu_)
         Rx_ = self._get_Rx_(F, dx_, kR_)
 
-        log_conditional = (- 0.5 * (Rx_**2).sum(axis=-1)
-                           - 0.5 * self.num_hidden * np.log(2*np.pi)
-                           + F.linalg.slogdet(kR_)[1])
+        log_conditional = (
+            -0.5 * (Rx_ ** 2).sum(axis=-1)
+            - 0.5 * self.num_hidden * np.log(2 * np.pi)
+            + F.linalg.slogdet(kR_)[1]
+        )
         log_marginal = F.log(F.exp(log_conditional).mean(axis=1))
 
         qz = F.softmax(log_conditional)
@@ -69,20 +77,22 @@ class GMMModel(gluon.HybridBlock):
     @staticmethod
     def m_step(x, qz):
         """ M-step computes summary statistics in numpy """
-        x = x.astype('float64')
-        qz = qz.astype('float64')
+        x = x.astype("float64")
+        qz = qz.astype("float64")
 
-        nz = qz.sum(axis=0) # (num_clusters,)
-        sum_x = (qz[:,:,None] * x[:,None,:]).sum(axis=0)
-        sum_x2 = (qz[:,:,None,None] * (x[:,None,:,None] @ x[:,None,None,:])
-                 ).sum(axis=0)
+        nz = qz.sum(axis=0)  # (num_clusters,)
+        sum_x = (qz[:, :, None] * x[:, None, :]).sum(axis=0)
+        sum_x2 = (
+            qz[:, :, None, None] * (x[:, None, :, None] @ x[:, None, None, :])
+        ).sum(axis=0)
         return nz, sum_x, sum_x2
 
 
-class GMMTrainer():
-    """ trainer based on M-step summary statistics
+class GMMTrainer:
+    """trainer based on M-step summary statistics
     can add mini-batch statistics for a full-batch update
     """
+
     def __init__(self, model):
         self.model = model
         self.zero_stats()
@@ -100,9 +110,9 @@ class GMMTrainer():
         self.sum_x2 = self.sum_x2 + sum_x2
 
     def update(self):
-        mu_ = self.sum_x / self.nz[:,None]
-        Ex2 = self.sum_x2 / self.nz[:,None,None]
-        cov_ = Ex2 - mu_[:,:,None] @ mu_[:,None,:]
+        mu_ = self.sum_x / self.nz[:, None]
+        Ex2 = self.sum_x2 / self.nz[:, None, None]
+        cov_ = Ex2 - mu_[:, :, None] @ mu_[:, None, :]
         kR_ = np.linalg.inv(np.linalg.cholesky(cov_))
 
         self.model.mu_.set_data(mu_)
