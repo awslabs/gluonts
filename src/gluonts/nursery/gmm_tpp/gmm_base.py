2f5cb1637a894b5ae1f1038c37e5fc49349ba109
Yifei Ma <yifeim@amazon.com>
2021-04-13 22:32:17 -0700

    learn prior, add pseudo count, init model with data template

--- src/gluonts/nursery/gmm_tpp/gmm_base.py
+++ src/gluonts/nursery/gmm_tpp/gmm_base.py
@@ -31,40 +31,64 @@ class GMMModel(gluon.HybridBlock):
 
     Shapes
     ===
-    x: (batch_size, num_hidden)
+    x: (batch_size, input_dim)
     log_marg: (batch_size,)
     qz: (batch_size, num_clusters)
     """
 
     def __init__(
-        self, num_clusters, num_hidden, mu_=None, kR_=None, lr_mult=None
+        self,
+        *data_template,
+        num_clusters,
+        log_prior_=None,
+        mu_=None,
+        kR_=None,
+        lr_mult=None,
+        ctx=None,
+        hybridize=None,
     ):
         super().__init__()
-        self.num_hidden = num_hidden
+        self.input_dim = data_template[0].shape[1]
+        self.num_clusters = num_clusters
 
         with self.name_scope():
+            self.log_prior_ = self.params.get(
+                "log_prior_",
+                shape=(num_clusters,),
+                lr_mult=lr_mult,
+                init=mx.init.Constant(np.log(1 / self.num_clusters))
+                if log_prior_ is None
+                else mx.init.Constant(log_prior_),
+            )
+
             self.mu_ = self.params.get(
                 "mu_",
-                shape=(num_clusters, num_hidden),
+                shape=(self.num_clusters, self.input_dim),
                 lr_mult=lr_mult,
                 init=None if mu_ is None else mx.init.Constant(mu_),
             )
 
             self.kR_ = self.params.get(
                 "kR_",
-                shape=(num_clusters, num_hidden, num_hidden),
+                shape=(self.num_clusters, self.input_dim, self.input_dim),
                 lr_mult=lr_mult,
                 init=None if kR_ is None else mx.init.Constant(kR_),
             )
 
+        if hybridize:
+            self.hybridize()
+
+        self.initialize(ctx=ctx)
+        self(*[mx.nd.array(x, ctx=ctx) for x in data_template])
+
     @staticmethod
     def _get_dx_(F, x, mu_):
-        """ @Return (batch_size, num_clusters, num_hidden) """
+        """ @Return (batch_size, num_clusters, input_dim) """
         return F.broadcast_minus(x.expand_dims(1), mu_)
 
     @staticmethod
     def _get_Rx_(F, dx_, kR_):
-        """ @Return (batch_size, num_clusters, num_hidden) """
+        """ @Return (batch_size, num_clusters, input_dim) """
         kR_expand_0 = F.broadcast_like(
             kR_.expand_dims(0), dx_, lhs_axes=(0,), rhs_axes=(0,)
         )
@@ -72,19 +96,21 @@ class GMMModel(gluon.HybridBlock):
         Rx_ = F.batch_dot(kR_expand_0, dx_.expand_dims(-1)).squeeze(axis=-1)
         return Rx_
 
-    def hybrid_forward(self, F, x, mu_, kR_):
+    def hybrid_forward(self, F, x, log_prior_, mu_, kR_):
         """ E-step computes log_marginal and q(z|x) """
         dx_ = self._get_dx_(F, x, mu_)
         Rx_ = self._get_Rx_(F, dx_, kR_)
 
         log_conditional = (
             -0.5 * (Rx_ ** 2).sum(axis=-1)
-            - 0.5 * self.num_hidden * np.log(2 * np.pi)
+            - 0.5 * self.input_dim * np.log(2 * np.pi)
             + F.linalg.slogdet(kR_)[1]
-        )
-        log_marginal = F.log(F.exp(log_conditional).mean(axis=1))
+        )  # (batch, num_clusters)
 
-        qz = F.softmax(log_conditional)
+        log_joint = F.broadcast_add(log_conditional, log_prior_.log_softmax())
+        log_marginal = F.log(F.exp(log_joint).sum(axis=1))
+
+        qz = log_joint.softmax(axis=1)
 
         return log_marginal, qz
 
@@ -107,14 +133,18 @@ class GMMTrainer:
     can add mini-batch statistics for a full-batch update
     """
 
-    def __init__(self, model):
+    def __init__(self, model, pseudo_count=0.1, jitter=1e-6):
         self.model = model
+        self.pseudo_count = pseudo_count
+        self.jitter = jitter
         self.zero_stats()
 
     def zero_stats(self):
-        self.nz = 0
-        self.sum_x = 0
-        self.sum_x2 = 0
+        self.nz = self.pseudo_count * np.ones(self.model.num_clusters)
+        self.sum_x = np.zeros(self.model.input_dim)
+        self.sum_x2 = (
+            np.eye(self.model.input_dim) * self.jitter * self.pseudo_count
+        )
 
     def add(self, x):
         _, qz = self.model(mx.nd.array(x))
@@ -129,6 +159,7 @@ class GMMTrainer:
         cov_ = Ex2 - mu_[:, :, None] @ mu_[:, None, :]
         kR_ = np.linalg.inv(np.linalg.cholesky(cov_))
 
+        self.model.log_prior_.set_data(np.log(self.nz / self.nz.sum()))
         self.model.mu_.set_data(mu_)
         self.model.kR_.set_data(kR_)
 
