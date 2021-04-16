974a718f254d1228f6108168267fb51533c52cd4
Jasper <schjaspe@amazon.de>
2021-04-16 10:43:41 +0200

    Add most forecasters as entry points. (#1351)
    * Add most forecasters as entry points. 
    
    Co-authored-by: Michael Bohlke-Schneider <mschneid83@gmail.com>

---
+++ test/test_forecaster_entrypoints.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import pkg_resources
+
+
+def test_forecaster_entrypoints():
+    for entry_point in pkg_resources.iter_entry_points("gluonts_forecasters"):
+        entry_point.load()
