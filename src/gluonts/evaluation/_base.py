3f463993c4d9147a0776f8aeb71c6043bc1f0a1d
Lorenzo Stella <stellalo@amazon.com>
2021-04-29 15:42:38 +0200

    fix formatting issue

--- src/gluonts/evaluation/_base.py
+++ src/gluonts/evaluation/_base.py
@@ -356,7 +356,12 @@ class Evaluator:
                     target_fcst = median_fcst
 
                 try:
-                    val = {k: eval_fn(pred_target, target_fcst,)}
+                    val = {
+                        k: eval_fn(
+                            pred_target,
+                            target_fcst,
+                        )
+                    }
                 except:
                     val = {k: np.nan}
 
