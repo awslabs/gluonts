38d98cd5372662edb511238cd4f1f321f3364879
Jasper Zschiegner <schjaspe@amazon.de>
2021-06-01 15:23:52 +0200

    Change type for fqname_for.

--- src/gluonts/core/_base.py
+++ src/gluonts/core/_base.py
@@ -11,8 +11,10 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
+from typing import Any
 
-def fqname_for(obj: type) -> str:
+
+def fqname_for(obj: Any) -> str:
     """
     Returns the fully qualified name of ``obj``.
 
