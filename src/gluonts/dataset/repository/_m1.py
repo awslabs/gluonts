133d64bfed79e3807b8832e290adbc8bce2cb6bd
Eduard Uffelmann <uffeledu@amazon.com>
2021-06-08 09:29:53 +0200

    refactored

--- src/gluonts/dataset/repository/_m1.py
+++ src/gluonts/dataset/repository/_m1.py
@@ -1,13 +1,15 @@
 import json
 import os
 from pathlib import Path
-from typing import NamedTuple, List, Dict
+from typing import List, Dict
 from urllib import request
 from zipfile import ZipFile
-
-from gluonts.dataset.repository._util import metadata, to_dict, save_to_file
+import copy
 
 from ._tsf_reader import TSFReader
+from ._util import metadata, to_dict, save_to_file
+
+from gluonts.gluonts_tqdm import tqdm
 
 ROOT = "https://zenodo.org/record"
 
@@ -15,31 +17,74 @@ dataset_info = {
     "m1_yearly": {"file": "m1_yearly_dataset.zip", "record": "4656193"},
     "m1_quarterly": {"file": "m1_quarterly_dataset.zip", "record": "4656154"},
     "m1_monthly": {"file": "m1_monthly_dataset.zip", "record": "4656159"},
+    "traffic_hourly": {
+        "file": "traffic_hourly_dataset.zip",
+        "record": "4656132",
+    },
 }
 
 
+def urllib_retrieve_hook(tqdm):
+    """Wraps tqdm instance.
+    Don'tqdm forget to close() or __exit__()
+    the tqdm instance once you're done with it (easiest using `with` syntax).
+    Example
+    -------
+    # >>> with tqdm(...) as tqdm:
+    # ...     reporthook = my_hook(tqdm)
+    # ...     urllib.urlretrieve(..., reporthook=reporthook)
+    """
+    last_b = [0]
+
+    def update_to(block=1, block_size=1, tsize=None):
+        """
+        block  : int, optional
+            Number of blocks transferred so far [default: 1].
+        block_size  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            tqdm.total = tsize
+        tqdm.update((block - last_b[0]) * block_size)
+        last_b[0] = block
+
+    return update_to
+
+
 def download_dataset(description: str, path: Path):
-    request.urlretrieve(
-        f"{ROOT}/{description['record']}/files/" f"{description['file']}",
-        path / description["file"],
+    file = description["file"]
+    with tqdm(
+        [],
+        unit="B",
+        unit_scale=True,
+        unit_divisor=1024,
+        miniters=5,
+        desc=f"download {file}",
+    ) as _tqdm:
+        request.urlretrieve(
+            f"{ROOT}/{description['record']}/files/{file}",
+            path / description["file"],
+            reporthook=urllib_retrieve_hook(_tqdm),
         )
 
 
 def frequency_converter(freq: str):
-    split = freq.split("_")
-    if len(split) == 1:
+    parts = freq.split("_")
+    if len(parts) == 1:
         return freq[0].upper()
-    elif len(split) == 2 and split[0].isnumeric():
-        return f"{split[0]}{split[1].upper()}"
+    elif len(parts) == 2 and parts[0].isnumeric():
+        return f"{parts[0]}{parts[1].upper()}"
     else:
         raise Exception("Unknown frequency")
 
 
 def save_metadata(
-        dataset_path: Path, cardinality: int, freq: str, prediction_length: int
+    dataset_path: Path, cardinality: int, freq: str, prediction_length: int
 ):
-    with open(dataset_path / "metadata.json", "w") as f:
-        f.write(
+    with open(dataset_path / "metadata.json", "w") as file:
+        file.write(
             json.dumps(
                 metadata(
                     cardinality=cardinality,
@@ -50,17 +95,12 @@ def save_metadata(
         )
 
 
-def save_dataset(
-        dataset_path: Path, data: List[Dict], prediction_length: int = None
-):
-    train_file = dataset_path / "data.json"
+def save_dataset(dataset_path: Path, data: List[Dict]):
     save_to_file(
-        train_file,
+        dataset_path / "data.json",
         [
             to_dict(
-                target_values=data_entry["target"]
-                if not prediction_length
-                else data_entry["target"][:-prediction_length],
+                target_values=data_entry["target"],
                 start=str(data_entry["start_timestamp"]),
             )
             for data_entry in data
@@ -68,22 +108,31 @@ def save_dataset(
     )
 
 
-def clean_up_dataset(dataset_path: Path, file_names: [str]):
+def create_train_ds(data: List[dict], prediction_length: int):
+    train_ds = copy.deepcopy(data)
+    for data_entry in data:
+        data_entry["target"] = data_entry["target"][:-prediction_length]
+    return train_ds
+
+
+def clean_up_dataset(dataset_path: Path, file_names: List[str]):
     for file in file_names:
         os.remove(dataset_path / file)
 
 
 def generate_forecasting_dataset(dataset_path: Path, dataset_name: str):
     ds_info = dataset_info[dataset_name]
-    os.makedirs(dataset_path, exist_ok=True)
+    dataset_path.mkdir(exist_ok=True)
 
-    file_path = dataset_path / ds_info["file"]
+    file = ds_info["file"]
+    file_path = dataset_path / file
     download_dataset(ds_info, dataset_path)
     with ZipFile(file_path, "r") as zip:
         file_names = zip.namelist()
         # TODO better exception text
         assert len(file_names) == 1, "Too many files"
-        zip.extractall(path=dataset_path)
+        for member in tqdm(zip.infolist(), desc=f"Extracting {file}"):
+            zip.extract(member, dataset_path)
     reader = TSFReader(dataset_path / file_names[0])
     meta, data = reader.read()
 
@@ -94,9 +143,9 @@ def generate_forecasting_dataset(dataset_path: Path, dataset_name: str):
         frequency_converter(meta.frequency),
         prediction_length,
     )
-
+    train_data = create_train_ds(data, prediction_length)
+    save_dataset(dataset_path / "train", train_data)
     save_dataset(dataset_path / "test", data)
-    save_dataset(dataset_path / "train", data, prediction_length)
 
-    file_names.append(ds_info["file"])
+    file_names.append(file)
     clean_up_dataset(dataset_path, file_names)
