19f381a4e4c6e7dd9bc17861df926a028133fdc4
Jasper <schjaspe@amazon.de>
2021-07-16 17:30:15 +0200

    Add serde module doc. (#1624)

--- src/gluonts/core/serde/__init__.py
+++ src/gluonts/core/serde/__init__.py
@@ -22,6 +22,66 @@ from ._repr import dump_code, load_code
 from .np import *  # noqa
 from .pd import *  # noqa
 
+
+"""
+gluonts.core.serde
+~~~~~~~~~~~~~~~~~~~~~
+
+Simple Serialization/Deserialization framework that uses human-readable
+json-encodings to represent data.
+
+In some ways, it is an alternative to `pickle`, but instead of using a binary
+encoding, `serde` outputs json-compatible text. This has two main advantages:
+It is human-readable and the encoding works across different Python versions.
+
+Values which can be directly represented in `json` are not changed, thus
+`serde.encode` is virtually the same as `json.dumps` for values such as `1`, `
+["a", "b", "c"]` or `{"foo": "bar"}`.
+
+However, more complex values are encoded differently. For these, we create an
+object which uses a special `__kind__` attribute, which can be one of::
+
+    class Kind(str, Enum):
+        Type = "type"
+        Instance = "instance"
+        Stateful = "stateful"
+
+
+A type is just a path to a class or function, an instance is an object which
+can be re-constructed by passing arguments to its constructor, and stateful
+represents objects that are decoded by setting the `__dict__` attribute on
+an otherwise empty instance of the type.
+
+`serde.encode` uses `functools.singledispatch` to encode a given object. It is
+implemented for a variety of existing types, such as named-tuples, paths, and
+`pydantic.BaseModel`.
+
+In addition, one can derive from `serde.Stateful` or `serde.Stateless` to opt
+into one of the behaviours. The latter requires that the class supports
+`__getnewargs_ex__` from the pickle protocol.
+
+To encode custom values, one can use the `serde.encode.register` decorator.
+For example, support for numpy's arrays are added by::
+
+    @encode.register(np.ndarray)
+    def encode_np_ndarray(v: np.ndarray) -> Any:
+        return {
+            "__kind__": Kind.Instance,
+            "class": "numpy.array",
+            "args": encode([v.tolist(), v.dtype]),
+        }
+
+There is no need to implement `decode` for a given type since the encoding
+should contain the information on how the object can be constructed.
+
+Similarly to `json`, `serde` does not support object-identity. This means that
+if an object exists twice in an object graph, it is encoded at each of the
+occurances. Consequently, ciruclar references do not work.
+
+`dump_json` and `load_json` are simple helpers, which use `encode` and
+`decode` internally.
+"""
+
 __all__ = [
     "flat",
     "encode",
