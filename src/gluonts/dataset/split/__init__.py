458da3b9d37ae1095b604c6d8561ea122df9eeb6
Jasper Zschiegner <schjaspe@amazon.de>
2021-07-16 18:20:01 +0200

    Remove setting __module on __all__ items.

--- src/gluonts/dataset/split/__init__.py
+++ src/gluonts/dataset/split/__init__.py
@@ -14,8 +14,3 @@
 __all__ = ["DateSplitter", "OffsetSplitter"]
 
 from .splitter import DateSplitter, OffsetSplitter
-
-# fix Sphinx issues, see https://bit.ly/2K2eptM
-for item in __all__:
-    if hasattr(item, "__module__"):
-        setattr(item, "__module__", __name__)
