2afb098dda5ceb2bd1470a679de148caba7b9731
Jasper <schjaspe@amazon.de>
2021-07-19 11:31:58 +0200

    Remove setting __module on __all__ items. (#1625)

--- src/gluonts/model/seasonal_naive/__init__.py
+++ src/gluonts/model/seasonal_naive/__init__.py
@@ -15,8 +15,3 @@ from ._estimator import SeasonalNaiveEstimator
 from ._predictor import SeasonalNaivePredictor
 
 __all__ = ["SeasonalNaiveEstimator", "SeasonalNaivePredictor"]
-
-# fix Sphinx issues, see https://bit.ly/2K2eptM
-for item in __all__:
-    if hasattr(item, "__module__"):
-        setattr(item, "__module__", __name__)
