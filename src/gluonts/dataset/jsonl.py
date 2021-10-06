2709ffccea0c6d3f18f292c5aa473439b9e7251b
Jasper <schjaspe@amazon.de>
2021-10-06 16:51:46 +0200

    Fix gluonts.json; added bdump/bdumps. (#1721)
    * Fix gluonts.json; added bdump/bdumps.
    
    Fixes: 1708.

--- src/gluonts/dataset/jsonl.py
+++ src/gluonts/dataset/jsonl.py
@@ -22,14 +22,12 @@ from gluonts.dataset.util import get_bounds_for_mp_data_loading
 
 
 def load(file_obj):
-    for line in file_obj:
-        yield json.loads(line)
+    return map(json.loads, file_obj)
 
 
 def dump(objects, file_obj):
     for object_ in objects:
-        file_obj.write(json.dumps(object_))
-        file_obj.write("\n")
+        json.dump(object_, file_obj, nl=True)
 
 
 class Span(NamedTuple):
