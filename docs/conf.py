d9699c8b697a401f11453704fdf11a09c59b456a
Jasper <schjaspe@amazon.de>
2021-10-27 09:20:29 +0200

    Fix renaming.

--- docs/conf.py
+++ docs/conf.py
@@ -239,7 +239,7 @@ def setup(app):
         True,
     )
     app.add_transform(AutoStructify)
-    app.add_javascript("google_analytics.js")
+    app.add_js_file("google_analytics.js")
 
 
 # Napoleon settings
