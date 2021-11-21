5bbdfa2baa9de206aa263d25123af17263061dac
Kashif Rasul <kashif.rasul@gmail.com>
2021-11-21 18:48:06 +0100

    remove unused imports

--- src/gluonts/torch/distributions/implicit_quantile_network.py
+++ src/gluonts/torch/distributions/implicit_quantile_network.py
@@ -11,7 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Optional, Tuple
 from functools import partial
 
 import torch
