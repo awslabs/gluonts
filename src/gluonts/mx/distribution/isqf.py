862142d2ddc47b64f01d503846d5ae99748bcb61
KelvinKan <47009315+KelvinKan@users.noreply.github.com>
2021-11-30 17:56:13 +0100

    fixing flake8 cast issue

--- src/gluonts/mx/distribution/isqf.py
+++ src/gluonts/mx/distribution/isqf.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Dict, List, Optional, Tuple, cast
+from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 
