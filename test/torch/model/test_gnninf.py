5de23b6e1154a01a5e1e985d3a491ec0b5f09ecb
Victor Garcia <vgsatorras@gmail.com>
2022-03-21 14:17:34 +0100

    style fix

--- test/torch/model/test_gnninf.py
+++ test/torch/model/test_gnninf.py
@@ -28,6 +28,7 @@ from gluonts.dataset.common import TrainDatasets
 from gluonts.torch.model.gnninf import GNNInfEstimator
 from gluonts.torch.model.gnninf.networks.model import GNNInfNetwork
 from gluonts.evaluation import make_evaluation_predictions
+
 logger = logging.getLogger(__name__)
 
 
@@ -36,13 +37,14 @@ class GNNInfExperiment:
         self,
         dataset: TrainDatasets,
         estimator: Union[Type[Estimator], Type[Predictor]],
-        batch_size: int=4,
-        num_batches_per_epoch: int=100,
-        epochs: int=1,
-        lr: float=2e-4,
-        gnn_name: str="gnn",
-        nf: int=64,
-        gnn_layers: int=2, device='cpu'
+        batch_size: int = 4,
+        num_batches_per_epoch: int = 100,
+        epochs: int = 1,
+        lr: float = 2e-4,
+        gnn_name: str = "gnn",
+        nf: int = 64,
+        gnn_layers: int = 2,
+        device="cpu",
     ) -> None:
         self.dataset = dataset
         self.estimator = estimator
@@ -50,45 +52,69 @@ class GNNInfExperiment:
         self.num_batches_per_epoch = num_batches_per_epoch
         self.epochs = epochs
         self.lr = lr
-        self.gnn_name = gnn_name # "gnn" or "bpgnn"
+        self.gnn_name = gnn_name  # "gnn" or "bpgnn"
         self.nf = nf
         self.gnn_layers = gnn_layers
         self.device = device
-        self.target_dim = int(self.dataset.metadata.feat_static_cat[0].cardinality)
+        self.target_dim = int(
+            self.dataset.metadata.feat_static_cat[0].cardinality
+        )
 
     def run(self) -> None:
-        metadata, train_dataset, test_dataset = self.dataset.metadata, self.dataset.train, self.dataset.test
+        metadata, train_dataset, test_dataset = (
+            self.dataset.metadata,
+            self.dataset.train,
+            self.dataset.test,
+        )
         freq = metadata.freq
         prediction_length = metadata.prediction_length
         context_length = 10 * prediction_length
 
         # Retrieve estimator
-        estimator = self.estimator(freq=freq, prediction_length=prediction_length, context_length=context_length,
-                                   batch_size=self.batch_size, num_batches_per_epoch=self.num_batches_per_epoch,
-                                   epochs=self.epochs, lr=self.lr, gnn_name=self.gnn_name, nf=self.nf,
-                                   gnn_layers=self.gnn_layers, device=self.device)
+        estimator = self.estimator(
+            freq=freq,
+            prediction_length=prediction_length,
+            context_length=context_length,
+            batch_size=self.batch_size,
+            num_batches_per_epoch=self.num_batches_per_epoch,
+            epochs=self.epochs,
+            lr=self.lr,
+            gnn_name=self.gnn_name,
+            nf=self.nf,
+            gnn_layers=self.gnn_layers,
+            device=self.device,
+        )
         # Train
         predictor = estimator.train(train_dataset)
 
-
         # Evaluate Model
         evaluator = MultivariateEvaluator(quantiles=(np.arange(10) / 10)[1:])
-        forecast_it, ts_it = make_evaluation_predictions(dataset=test_dataset, predictor=predictor)
+        forecast_it, ts_it = make_evaluation_predictions(
+            dataset=test_dataset, predictor=predictor
+        )
         forecasts_pytorch = list(f.to_sample_forecast() for f in forecast_it)
         tss_pytorch = list(ts_it)
-        agg_metrics, item_metrics = evaluator(iter(tss_pytorch), iter(forecasts_pytorch), num_series=len(test_dataset))
+        agg_metrics, item_metrics = evaluator(
+            iter(tss_pytorch),
+            iter(forecasts_pytorch),
+            num_series=len(test_dataset),
+        )
 
         for name in ["mean_wQuantileLoss", "RMSE", "ND"]:
             print(f"{name}", agg_metrics[name])
 
 
-def retrieve_multivariate_dataset(dataset_name: str, regenerate: bool=False, max_num_nodes: int=100000) -> TrainDatasets:
+def retrieve_multivariate_dataset(
+    dataset_name: str, regenerate: bool = False, max_num_nodes: int = 100000
+) -> TrainDatasets:
     dataset = get_dataset(dataset_name, regenerate=regenerate)
     len_test, len_train = len(dataset.test), len(dataset.train)
-    num_test_dates = float(len_test)/float(len_train)
+    num_test_dates = float(len_test) / float(len_train)
     assert num_test_dates == int(num_test_dates)
     grouper_train = MultivariateGrouper(max_target_dim=max_num_nodes)
-    grouper_test = MultivariateGrouper(max_target_dim=max_num_nodes, num_test_dates=int(num_test_dates))
+    grouper_test = MultivariateGrouper(
+        max_target_dim=max_num_nodes, num_test_dates=int(num_test_dates)
+    )
     return TrainDatasets(
         metadata=dataset.metadata,
         train=grouper_train(dataset.train),
@@ -99,7 +125,9 @@ def retrieve_multivariate_dataset(dataset_name: str, regenerate: bool=False, max
 def test_gluonts_gnninf() -> None:
     dataset = retrieve_multivariate_dataset("exchange_rate")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    expt = GNNInfExperiment(dataset=dataset, estimator=GNNInfEstimator, device=device)
+    expt = GNNInfExperiment(
+        dataset=dataset, estimator=GNNInfEstimator, device=device
+    )
     expt.run()
 
 
@@ -111,10 +139,17 @@ def test_torch_gnninf() -> None:
     pred_length = 3
     num_nodes = 10
     bs = 16
-    agg_name = 'gnn'  # gnn or bpgnn
+    agg_name = "gnn"  # gnn or bpgnn
 
     # Model init
-    fcgnn = GNNInfNetwork(in_channels, out_channels, input_length, pred_length, num_nodes, agg_name=agg_name)
+    fcgnn = GNNInfNetwork(
+        in_channels,
+        out_channels,
+        input_length,
+        pred_length,
+        num_nodes,
+        agg_name=agg_name,
+    )
 
     # Data init
     batch = torch.zeros(bs, num_nodes, input_length, in_channels)
@@ -124,5 +159,3 @@ def test_torch_gnninf() -> None:
 
     # Print output size
     print("Output size %s" % str(out.size()))
-
-
