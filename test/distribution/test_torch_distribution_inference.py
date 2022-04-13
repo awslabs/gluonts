0475f56f5a80332012fed43564448c548a3672bd
Elena Ehrlich <7536460+elenaehrlich@users.noreply.github.com>
2022-04-12 22:59:38 -0400

    sbp pytest now passes in under the 60s timeout limit

--- test/distribution/test_torch_distribution_inference.py
+++ test/distribution/test_torch_distribution_inference.py
@@ -314,7 +314,9 @@ def test_splicedbinnedpareto_likelihood(
         ),
     )
 
-    samples = sbp_distr.sample(torch.tensor(range(0, NUM_SAMPLES // BATCH_SIZE)).shape)
+    samples = sbp_distr.sample(
+        torch.tensor(range(0, NUM_SAMPLES // BATCH_SIZE)).shape
+    )
     training_x = samples.numpy().flatten()
 
     # Maximum Likelihood Estimation on the DistributionOutput
