1f56cac78f77bc8a0f3f948c687a34f63fe07733
Lorenzo Stella <stellalo@amazon.com>
2022-04-13 23:04:13 +0200

    fix flake8

--- src/gluonts/mx/block/quantile_output.py
+++ src/gluonts/mx/block/quantile_output.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 from mxnet.gluon import nn
 from mxnet.gluon.loss import Loss
