f66e7a8ed58867d75caedb179835141e262f7814
Kashif Rasul <kashif.rasul@gmail.com>
2022-05-01 16:15:53 +0200

    added test

--- test/torch/model/test_estimators.py
+++ test/torch/model/test_estimators.py
@@ -26,6 +26,7 @@ from gluonts.torch.model.deepar import DeepAREstimator
 from gluonts.torch.model.mqf2 import MQF2MultiHorizonEstimator
 from gluonts.torch.model.simple_feedforward import SimpleFeedForwardEstimator
 from gluonts.torch.model.forecast import DistributionForecast
+from gluonts.torch.modules.loss import NegativeLogLikelihood
 
 
 @pytest.mark.parametrize(
@@ -37,6 +38,8 @@ from gluonts.torch.model.forecast import DistributionForecast
             batch_size=4,
             num_batches_per_epoch=3,
             trainer_kwargs=dict(max_epochs=2),
+            loss=NegativeLogLikelihood(beta=0.5),
+            scaling=False,
         ),
         lambda dataset: MQF2MultiHorizonEstimator(
             freq=dataset.metadata.freq,
