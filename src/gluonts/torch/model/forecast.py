536cd4e59952c047bfa9d6f594098249785494d4
Kashif Rasul <kashif.rasul@gmail.com>
2022-05-16 13:06:56 +0200

    flake8

--- src/gluonts/torch/model/forecast.py
+++ src/gluonts/torch/model/forecast.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Dict, Optional, Union, List
+from typing import Dict, Optional, Union
 
 import numpy as np
 import pandas as pd
