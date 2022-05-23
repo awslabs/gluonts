c32840f1d9cfc91a8c160d898fe6822dc4f4cf61
Hongqing-work <Shine_CHQ@outlook.com>
2022-05-23 18:25:43 +0800

    add method to download dataset and extract

--- src/gluonts/dataset/repository/_uber.py
+++ src/gluonts/dataset/repository/_uber.py
@@ -1,6 +1,21 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 import json
+import tempfile
+import zipfile
 from pathlib import Path
-from typing import Optional
+from urllib import request
 
 import pandas as pd
 
@@ -16,25 +31,39 @@ def generate_uber_dataset(
     ), f"invalid uber_freq='{uber_freq}'. Allowed values: {subsets.keys()}"
     freq_setting = subsets[uber_freq.lower()]
 
-    uber_url_path = (
-        "https://github.com/Hongqing-work/uber-tlc-foil-response/releases"
-        "/download/v1.0.0/uber-raw-data-janjune-15.zip"
-    )
-    uber_df = pd.read_csv(
-        uber_url_path,
-        header=0,
-        usecols=["Pickup_date", "locationID"],
-        index_col=0,
-    )
+    # download the dataset and read the data
+    with tempfile.TemporaryDirectory() as dir_path:
+        temp_dir_path = Path(dir_path)
+        temp_zip_path = temp_dir_path / "uber-dataset.zip"
+        uber_url_path = (
+            "https://raw.githubusercontent.com/fivethirtyeight/"
+            "uber-tlc-foil-response/master/uber-trip-data/"
+            "uber-raw-data-janjune-15.csv.zip"
+        )
+        request.urlretrieve(uber_url_path, temp_zip_path)
+        with zipfile.ZipFile(temp_zip_path) as zf:
+            zf.extractall(path=temp_dir_path)
+        uber_file_path = (
+            temp_dir_path / "uber-dataset" / "uber-raw-data-janjune-15.csv"
+        )
+        uber_df = pd.read_csv(
+            uber_file_path,
+            header=0,
+            usecols=["Pickup_date", "locationID"],
+            index_col=0,
+        )
+
     # We divide the raw data according to locationID. Each json line represents
     # a time series of a loacationID. The targets are numbers of pickup-events
     # during a day or an hour.
     time_series_of_locations = list(uber_df.groupby(by="locationID"))
+
     dataset_path.mkdir(exist_ok=True)
     train_path = dataset_path / "train"
     test_path = dataset_path / "test"
     train_path.mkdir(exist_ok=True)
     test_path.mkdir(exist_ok=True)
+
     train_file = train_path / "data.json"
     test_file = test_path / "data.json"
     with open(train_file, "w") as o_train, open(test_file, "w") as o_test:
