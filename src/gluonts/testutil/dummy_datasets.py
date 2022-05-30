8b9b34309789f11cdefd9be7c8787f6a83de55dc
Jasper Zschiegner <schjaspe@amazon.de>
2022-05-30 10:23:39 +0200

    Fixup.

--- src/gluonts/testutil/dummy_datasets.py
+++ src/gluonts/testutil/dummy_datasets.py
@@ -15,6 +15,7 @@ from random import randint
 from typing import List, Tuple
 
 import numpy as np
+import pandas as pd
 
 from gluonts.dataset.common import ListDataset
 from gluonts.dataset.field_names import FieldName
