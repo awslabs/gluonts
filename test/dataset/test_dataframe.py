dd1f36081a89c9fe2e704df3e8f0c833bab6c232
RSNirwan <rajbir.nirwan@gmail.com>
2022-06-16 17:06:32 +0200

    Add auto-inference of timestamp and freq in DataFramesDatasets

--- test/dataset/test_dataframe.py
+++ test/dataset/test_dataframe.py
@@ -19,6 +19,19 @@ from gluonts.dataset import dataframe
 
 
 @pytest.fixture()
+def my_series():
+    idx = pd.date_range("2021-01-01", freq="1D", periods=3)
+    return pd.Series(np.random.normal(size=3), index=idx, name="a")
+
+
+@pytest.fixture()
+def my_dataframe(my_series):
+    df = my_series.reset_index()
+    df.columns = ["timestamp", "target"]
+    return df
+
+
+@pytest.fixture()
 def long_dataframe():
     N, T = 2, 10
     df = pd.DataFrame(index=np.arange(N * T))
@@ -155,3 +168,29 @@ def test_check_timestamps():
 )
 def test_check_timestamps_fail(timestamps):
     assert not dataframe.check_timestamps(timestamps, freq="2H")
+
+
+def test_infer_timestamp(my_series):
+    ds = dataframe.DataFramesDataset(my_series, target="a", freq="1D")
+    assert isinstance(ds.timestamp, str)
+
+
+def test_infer_timestamp2(my_series):
+    seriess = {"A": my_series, "B": my_series}
+    ds = dataframe.DataFramesDataset(seriess, target="a", freq="1D")
+    assert isinstance(ds.timestamp, str)
+
+
+def test_infer_freq(my_dataframe):
+    ds = dataframe.DataFramesDataset(
+        my_dataframe, target="target", timestamp="timestamp"
+    )
+    assert ds.freq == "D"
+
+
+def test_infer_freq2(my_dataframe):
+    dfs = {"A": my_dataframe, "B": my_dataframe}
+    ds = dataframe.DataFramesDataset(
+        dfs, target="target", timestamp="timestamp"
+    )
+    assert ds.freq == "D"
