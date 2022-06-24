04223303da27cea7db297f34a2718807ff6f3937
稀奇喵喵 <52767165+npnv@users.noreply.github.com>
2022-06-24 16:33:27 +0800

    Update test/dataset/split/test_split.py
    Co-authored-by: Lorenzo Stella <lorenzostella@gmail.com>

--- test/dataset/split/test_split.py
+++ test/dataset/split/test_split.py
@@ -11,7 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from posixpath import split
 import pandas as pd
 
 from gluonts.dataset.common import ListDataset
