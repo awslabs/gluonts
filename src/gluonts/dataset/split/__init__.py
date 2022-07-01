14def926c14898c076eb2bf6b134e0535d40b4c0
稀奇喵喵 <52767165+npnv@users.noreply.github.com>
2022-07-01 22:44:12 +0800

    Breaking: Update data splitters to return `(input, output)` pairs in the test split (#2031)

--- src/gluonts/dataset/split/__init__.py
+++ src/gluonts/dataset/split/__init__.py
@@ -11,6 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-__all__ = ["DateSplitter", "OffsetSplitter"]
+__all__ = ["DateSplitter", "OffsetSplitter", "split"]
 
-from .splitter import DateSplitter, OffsetSplitter
+from .splitter import DateSplitter, OffsetSplitter, split
