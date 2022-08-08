377c4de85322838aab6c3c9fa75134e8bdc49b81
Jasper <schjaspe@amazon.de>
2022-08-08 14:33:04 +0200

    Fix dataset arrow writer tool. (#2196)
    Also fix `__len__` for `ArrowFile`.

--- src/gluonts/dataset/arrow/file.py
+++ src/gluonts/dataset/arrow/file.py
@@ -109,7 +109,11 @@ class ArrowFile(File):
             yield self.reader.get_batch(batch_no)
 
     def __len__(self):
-        return sum(self.batch_offsets)
+        if len(self.batch_offsets) > 0:
+            return self.batch_offsets[-1]
+
+        # empty file
+        return 0
 
     def __iter__(self):
         for batch in self.iter_batches():
