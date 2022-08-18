01bbcbffe70fbf5d7cda6a780ae5c86ee5aae271
Hongqing-work <Shine_CHQ@outlook.com>
2022-08-18 16:51:57 +0800

    add infer,name_mapping, get_field_type

--- src/gluonts/dataset/schema.py
+++ src/gluonts/dataset/schema.py
@@ -12,7 +12,7 @@
 # permissions and limitations under the License.
 
 from dataclasses import dataclass
-from typing import Any, Dict, Optional, Union, Generic, TypeVar, Type
+from typing import Any, Dict, Optional, Type
 
 import numpy as np
 import pandas as pd
@@ -20,11 +20,13 @@ from pandas.tseries.frequencies import to_offset
 
 from gluonts.exceptions import GluonTSDataError
 
-T = TypeVar("T")
 
+@dataclass
+class FieldType:
+    raw_type: Type
+    args: Optional[dict] = None
 
-class FieldType(Generic[T]):
-    def __call__(self, v: Any) -> T:
+    def __call__(self, v: Any):
         raise NotImplementedError()
 
     def __eq__(self, other):
@@ -35,9 +37,18 @@ class FieldType(Generic[T]):
 
 
 @dataclass
-class NumpyArrayField(FieldType[np.ndarray]):
-    dtype: Type = np.float32
-    ndim: Optional[int] = None
+class PlainFieldType(FieldType):
+    def __call__(self, v: Any):
+        return self.raw_type(v)
+
+
+@dataclass
+class NumpyArrayField(FieldType):
+    raw_type: Type = np.ndarray
+
+    def __post_init__(self):
+        self.dtype = self.args["dtype"] if "dtype" in self.args else np.float32
+        self.ndim = self.args["ndim"] if "ndim" in self.args else None
 
     def __call__(self, value: Any) -> np.ndarray:
         value = np.asarray(value, dtype=self.dtype)
@@ -75,12 +86,13 @@ class NumpyArrayField(FieldType[np.ndarray]):
         return self.ndim is None or x.ndim == self.ndim
 
 
-Freq = Union[str, pd.DateOffset]
+@dataclass
+class PandasPeriodField(FieldType):
+    raw_type: Type = pd.Period
 
-
-class PandasPeriodField(FieldType[pd.Period]):
-    def __init__(self, freq: Freq) -> None:
-        self.freq = to_offset(freq)
+    def __post_init__(self):
+        assert "freq" in self.args
+        self.freq = to_offset(self.args["freq"])
 
     def __eq__(self, other):
         if not isinstance(other, PandasPeriodField):
@@ -103,21 +115,20 @@ class PandasPeriodField(FieldType[pd.Period]):
         return True
 
 
-@dataclass
-class FieldWithDefault:
-    field_type: Any
-    default_value: Any
-
-    def __call__(self, value: Any) -> Any:
-        return self.field_type(value)
+def get_field_type(raw_type: Type, args: Optional[dict] = None) -> FieldType:
+    if raw_type == np.ndarray:
+        return NumpyArrayField(raw_type, args)
+    if raw_type == pd.Period:
+        return PandasPeriodField(raw_type, args)
+    return PlainFieldType(raw_type, args)
 
 
 @dataclass
 class Schema:
-    fields: Dict[str, Any]
+    fields: Dict[str, FieldType]
 
     def __call__(
-        self, d: Dict[str, Any], inplace: bool = True
+        self, d: Dict[str, Any], inplace: bool = True, name_mapping: Dict = {}
     ) -> Dict[str, Any]:
         """
         inplace
@@ -132,10 +143,18 @@ class Schema:
 
         for field_name, field_type in self.fields.items():
             try:
-                value = d[field_name]
+                if field_name in name_mapping:
+                    if isinstance(name_mapping[field_name], list):
+                        value = []
+                        for sub in name_mapping[field_name]:
+                            value.append(d[sub])
+                    else:
+                        value = d[name_mapping[field_name]]
+                else:
+                    value = d[field_name]
             except KeyError:
-                if isinstance(field_type, FieldWithDefault):
-                    value = field_type.default_value
+                if field_type.args and "default" in field_type.args:
+                    value = field_type.args["default"]
                 else:
                     raise GluonTSDataError(
                         f"field {field_name} does not occur in the data"
@@ -151,3 +170,43 @@ class Schema:
                     "{field_type}"
                 ) from e
         return out
+
+    @classmethod
+    def infer(cls, d: Dict[str, Any]) -> "Schema":
+        fields: Dict[str, FieldType] = {}
+        for field_name, value in d.items():
+            if isinstance(value, np.ndarray):
+                fields[field_name] = get_field_type(
+                    np.ndarray,
+                    {
+                        "dtype": value.dtype,
+                        "ndim": value.ndim,
+                        "shape": value.shape,
+                    },
+                )
+                continue
+            if isinstance(value, pd.Period):
+                fields[field_name] = get_field_type(
+                    pd.Period, args={"freq": value.freq}
+                )
+                continue
+            if isinstance(value, (list, tuple)):
+                try:
+                    array = np.array(value)
+                    fields[field_name] = get_field_type(
+                        type(value),
+                        {
+                            "dtype": array.dtype,
+                            "ndim": array.ndim,
+                            "shape": array.shape,
+                        },
+                    )
+                except Exception:
+                    fields[field_name] = get_field_type(
+                        type(value), {"ndim": None}
+                    )
+                continue
+
+            fields[field_name] = PlainFieldType(raw_type=type(value))
+
+        return Schema(fields)
