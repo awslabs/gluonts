08c992c4450b1bee7ca4cf9b2d73e8fb436209c6
Lorenzo Stella <stellalo@amazon.com>
2022-08-23 09:55:41 +0200

    fixup

--- test/mx/test_no_batches.py
+++ test/mx/test_no_batches.py
@@ -11,8 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from dataclasses import dataclass
-
 import pytest
 
 from gluonts.exceptions import GluonTSDataError
