dbfc8cb46edf777fada4da254309bd92d4684feb
Shubham Kapoor <kapooshu@amazon.com>
2022-09-09 17:30:29 +0200

    Added docstring for matching_arguments util

--- src/gluonts/core/utils.py
+++ src/gluonts/core/utils.py
@@ -3,4 +3,20 @@ from typing import Callable, Dict, Any
 
 
 def matching_arguments(f: Callable, args: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Extract valid keyword arguments for a Callable from a dictionary.
+
+    Parameters
+    ----------
+    f
+        Callable for which matching arguments are required.
+    args
+        A dictionary containing keyword arguments.
+
+    Returns
+    -------
+    Dict
+        A dictionary containing valid keyword arguments.
+
+    """
     return {p: args[p] for p in signature(f).parameters.keys() & args.keys()}
