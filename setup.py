e209f0aa74c616522fcf46ffad35d9cbb30204e0
Jasper <schjaspe@amazon.de>
2022-09-12 10:17:29 +0200

    Docs: Custom Pygments style for dark theme.  (#2290)

--- setup.py
+++ setup.py
@@ -254,6 +254,11 @@ setup_kwargs: dict = dict(
         "style_check": StyleCheckCommand,
         **version_cmdclass,
     },
+    entry_points={
+        "pygments.styles": [
+            "gluonts-dark=gluonts.meta.style:Dark",
+        ]
+    },
 )
 
 if HAS_SPHINX:
