fe91df924d5cb0d7a6ca5976fcb4f4d5472c6146
Jasper Zschiegner <schjaspe@amazon.de>
2022-09-12 16:38:47 +0200

    Adjust setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 # Standard library imports
 import distutils.cmd
-import distutils.log
-import itertools
-import logging
 import os
 import subprocess
-import sys
 from pathlib import Path
-from textwrap import dedent
 
 # Third-party imports
 from setuptools import find_namespace_packages, setup
@@ -24,17 +19,6 @@ GPU_SUPPORT = 0 == int(
         stderr=open(os.devnull, "w"),
     )
 )
-
-try:
-    from sphinx import apidoc, setup_command
-
-    HAS_SPHINX = True
-except ImportError:
-    logging.warning(
-        "Package 'sphinx' not found. You will not be able to build the docs."
-    )
-
-    HAS_SPHINX = False
 
 
 def read(*names, encoding="utf8"):
@@ -66,132 +50,6 @@ def get_version_and_cmdclass(version_file):
 version, version_cmdclass = get_version_and_cmdclass(
     "src/gluonts/meta/_version.py"
 )
-
-
-class TypeCheckCommand(distutils.cmd.Command):
-    """A custom command to run MyPy on the project sources."""
-
-    description = "run MyPy on Python source files"
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        # import here (after the setup_requires list is loaded),
-        # otherwise a module-not-found error is thrown
-        import mypy.api
-
-        mypy_opts = [
-            "--allow-redefinition",
-            "--follow-imports=silent",
-            "--ignore-missing-imports",
-        ]
-
-        folders = [str(p.parent.resolve()) for p in SRC.glob("**/.typesafe")]
-
-        print(
-            "The following folders contain a `.typesafe` marker file "
-            "and will be type-checked with `mypy`:"
-        )
-        for folder in folders:
-            print(f"  {folder}")
-
-        std_out, std_err, exit_code = mypy.api.run(mypy_opts + folders)
-
-        print(std_out, file=sys.stdout)
-        print(std_err, file=sys.stderr)
-
-        if exit_code:
-            error_msg = dedent(
-                f"""
-                Mypy command
-
-                    mypy {" ".join(mypy_opts + folders)}
-
-                returned a non-zero exit code. Fix the type errors listed above
-                and then run
-
-                    python setup.py type_check
-
-                in order to validate your fixes.
-                """
-            ).lstrip()
-
-            print(error_msg, file=sys.stderr)
-            sys.exit(exit_code)
-
-
-class StyleCheckCommand(distutils.cmd.Command):
-    """A custom command to run MyPy on the project sources."""
-
-    description = "run Black style check on Python source files"
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """Run command."""
-
-        # import here (after the setup_requires list is loaded),
-        # otherwise a module-not-found error is thrown
-        import click
-        import black
-
-        black_opts = []
-        black_args = [
-            str(ROOT / folder)
-            for folder in ["src", "test", "examples"]
-            if (ROOT / folder).is_dir()
-        ]
-
-        print(
-            "Python files in the following folders will be style-checked "
-            "with `black`:"
-        )
-        print("\n".join(["  " + arg for arg in black_args]))
-
-        # a more direct way to call black
-        # this bypasses the problematic `_verify_python3_env` call in
-        # `click.BaseCommand.main`, which brakes things on Brazil builds
-        ctx = black.main.make_context(
-            info_name="black", args=["--check"] + black_opts + black_args
-        )
-        try:
-            exit_code = black.main.invoke(ctx)
-        except SystemExit as e:
-            exit_code = e.code
-        except click.exceptions.Exit as e:
-            exit_code = e.exit_code
-
-        if exit_code:
-            error_msg = dedent(
-                f"""
-                Black command
-
-                    black {" ".join(['--check'] + black_opts + black_args)}
-
-                returned a non-zero exit code. Fix the files listed above with
-
-                    black {" ".join(black_opts + black_args)}
-
-                and then run
-
-                    python setup.py style_check
-
-                in order to validate your fixes.
-                """
-            ).lstrip()
-
-            print(error_msg, file=sys.stderr)
-            sys.exit(exit_code)
 
 
 arrow_require = find_requirements("requirements-arrow.txt")
@@ -251,37 +109,13 @@ setup_kwargs: dict = dict(
         "shell": shell_require,
         "torch": torch_require,
     },
-    cmdclass={
-        "type_check": TypeCheckCommand,
-        "style_check": StyleCheckCommand,
-        **version_cmdclass,
-    },
+    cmdclass=version_cmdclass,
     entry_points={
         "pygments.styles": [
             "gluonts-dark=gluonts.meta.style:Dark",
         ]
     },
 )
-
-if HAS_SPHINX:
-
-    class BuildApiDoc(setup_command.BuildDoc):
-        def run(self):
-            args = list(
-                itertools.chain(
-                    ["-f"],  # force re-generation
-                    ["-P"],  # include private modules
-                    ["--implicit-namespaces"],  # respect PEP420
-                    ["-o", str(ROOT / "docs" / "api" / "gluonts")],  # out path
-                    [str(SRC / "gluonts")],  # in path
-                    ["setup*", "test", "docs", "*pycache*"],  # excluded paths
-                )
-            )
-            apidoc.main(args)
-            super(BuildApiDoc, self).run()
-
-    for command in ["build_sphinx", "doc", "docs"]:
-        setup_kwargs["cmdclass"][command] = BuildApiDoc
 
 # -----------------------------------------------------------------------------
 # start of AWS-internal section (DO NOT MODIFY THIS SECTION)!
