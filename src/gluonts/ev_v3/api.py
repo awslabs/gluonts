2bebeffe36af9b3fae6838d36276c089cb01986f
Nils Kiele <nilsmlte@amazon.de>
2022-09-16 09:20:27 +0200

    Add second use case to usage_example.py

--- src/gluonts/ev_v3/api.py
+++ src/gluonts/ev_v3/api.py
@@ -13,12 +13,9 @@
 
 import copy
 from abc import abstractmethod, ABC
-from typing import Dict, Collection, Union, Iterator
+from typing import Dict, Collection, Union, Iterator, Optional
 
 import numpy as np
-
-from gluonts.dataset import DataEntry
-from gluonts.model.forecast import Quantile, Forecast
 
 
 class Metric(ABC):
@@ -47,32 +44,11 @@ class Metric(ABC):
         pass
 
 
-class PrimitiveForecastBatch:
-    def __init__(self, forecasts: Collection[Forecast]):
-        self.forecasts = forecasts
-        self.batch_size = len(forecasts)
-
-    def quantile(self, q: Union[Quantile, float, str]) -> np.ndarray:
-        result = [forecast.quantile(q) for forecast in self.forecasts]
-        return np.stack(result)
-
-    @property
-    def median(self):
-        return self.quantile(Quantile.parse(0.5))
-
-    @property
-    def mean(self) -> np.ndarray:
-        result = [forecast.mean for forecast in self.forecasts]
-        return np.stack(result)
-
-    def __len__(self):
-        return self.batch_size
-
-
-
-def evaluate_batch(
-    metrics: Collection[Metric], input_data: Dict[str, np.ndarray]
-):
+def evaluate(metrics: Collection[Metric], input_data: Dict[str, np.ndarray]):
+    """
+    This function evaluates `metrics` on `input_data`.
+    `input_data` might be an entire dataset or a batch.
+    """
     requested_metrics = set(metric.name for metric in metrics)
 
     result_data = copy.deepcopy(input_data)
@@ -89,24 +65,10 @@ def evaluate_batch(
     return result
 
 
-def aggregate(total_result, batch_result, metrics):
+def aggregate_batches(total_result, batch_result, metrics):
     for metric in metrics:
         if hasattr(metric, "axis") and metric.axis == 0:
-            # TODO: Aggregating partial (batched) results is the tricky part.
-            #  For now, batch results are always aggregated using mean which
-            #  doesn't make sense in many cases.
-            #  For very large dataset and metrics like MSE, it seems like
-            #  we have to keep around all values in memory anyway...
-
-            c1 = total_result["entry_count"]
-            c2 = batch_result["entry_count"]
-
-            weighted_total = total_result[metric.name] * c1
-            weighted_new = batch_result[metric.name] * c2
-
-            total_result[metric.name] = (weighted_total + weighted_new) / (
-                c1 + c2
-            )
+            pass
         else:
             total_result[metric.name] = np.concatenate(
                 (total_result[metric.name], batch_result[metric.name])
@@ -117,50 +79,18 @@ def aggregate(total_result, batch_result, metrics):
     return total_result
 
 
-def finalize(eval_results: dict):
-    return eval_results  # some final adjustments could be made here
+def finalize_evaluation(eval_results: dict):
+    # some final adjustments / additions to the metric results could be made here
+    return eval_results
 
 
-def evaluate(metrics: Collection[Metric], input_batches: Iterator[dict]):
-    eval_total = evaluate_batch(metrics, next(input_batches))
+def evaluate_batches(
+    metrics: Collection[Metric], input_batches: Iterator[dict]
+):
+    eval_total = evaluate(metrics, next(input_batches))
     for batch in input_batches:
-        eval_partial = evaluate_batch(metrics, batch)
-        eval_total = aggregate(eval_total, eval_partial, metrics)
+        eval_partial = evaluate(metrics, batch)
+        eval_total = aggregate_batches(eval_total, eval_partial, metrics)
 
-    result = finalize(eval_total)
+    result = finalize_evaluation(eval_total)
     return result
-
-
-def get_input_batches(
-    dataset_it: Iterator[DataEntry],
-    forecast_it: Iterator[Forecast],
-    batch_size: int,
-) -> Iterator[dict]:
-    done = False
-    while not done:
-        target_batch = []
-        past_data_batch = []
-        forecast_batch = []
-
-        try:
-            for _ in range(batch_size):
-                data_entry = next(dataset_it)
-                forecast = next(forecast_it)
-
-                target_batch.append(
-                    data_entry["target"][-forecast.prediction_length :]
-                )
-                past_data_batch.append(
-                    data_entry["target"][: -forecast.prediction_length]
-                )
-                forecast_batch.append(forecast)
-        except StopIteration:
-            done = True
-
-        if len(target_batch) > 0:
-            input_batch = {
-                "target": np.stack(target_batch),
-                "past_data": np.stack(past_data_batch),
-                "forecast": PrimitiveForecastBatch(forecast_batch),
-            }
-            yield input_batch
--- src/gluonts/ev_v3/usage_example.py
+++ src/gluonts/ev_v3/usage_example.py
@@ -14,10 +14,8 @@
 import numpy as np
 from toolz import take
 
-from gluonts.ev_v3.api import (
-    evaluate,
-    get_input_batches,
-)
+from gluonts.ev_v3.api import evaluate, evaluate_batches
+from gluonts.ev_v3.helpers import get_input_batches, PrimitiveForecastBatch
 from gluonts.ev_v3.metrics import (
     AbsError,
     QuantileLoss,
@@ -29,51 +27,150 @@ from gluonts.evaluation import make_evaluation_predictions
 from gluonts.model.npts import NPTSPredictor
 from gluonts.dataset.repository.datasets import get_dataset
 
-prediction_length = 12
-data_entry_count = 50
-eval_batch_size = 8
 
-# GET DATA
-npts = NPTSPredictor(prediction_length=prediction_length, freq="D")
-electricity = get_dataset("electricity")
-test_data = list(take(data_entry_count, electricity.test))
+# SCENARIO 1: Entire dataset fits into a single np.ndarray
+def scenario_1():
+    prediction_length = 12
+    data_entry_count = 50
 
-# PREPARING DATA FOR EVALUATION
-forecast_it, _ = make_evaluation_predictions(
-    dataset=test_data, predictor=npts, num_samples=10
-)
+    npts = NPTSPredictor(prediction_length=prediction_length, freq="D")
+    electricity = get_dataset("electricity")
+    test_data = list(take(data_entry_count, electricity.test))
 
-metrics_to_evaluate = [
-    MSE(axis=0),
-    AbsError(error_type="p90"),
-    MSE(axis=1),
-    ND(axis=1),
-    *(QuantileLoss(quantile=q) for q in (0.1, 0.5, 0.9)),
-    SeasonalError(freq=electricity.metadata.freq, axis=1),
-]
+    # PREPARE FOR EVALUATION
+    metrics_to_evaluate = [
+        MSE(axis=0),
+        AbsError(error_type="p90"),
+        MSE(axis=1),
+        ND(axis=1),
+        *(QuantileLoss(quantile=q) for q in (0.1, 0.5, 0.9)),
+        SeasonalError(freq=electricity.metadata.freq, axis=1),
+    ]
 
-input_batch_it = get_input_batches(
-    iter(test_data), forecast_it, batch_size=eval_batch_size
-)
+    target_batch = []
+    past_data_batch = []
+    for data_entry in test_data:
+        target_batch.append(data_entry["target"][-prediction_length:])
+        past_data_batch.append(data_entry["target"][:-prediction_length])
 
-result = evaluate(metrics_to_evaluate, input_batch_it)
-for metric_name, value in result.items():
-    print(metric_name, np.shape(value))
+    # TODO: not use make_evaluation_predictions to get forecasts
+    forecast_it, _ = make_evaluation_predictions(
+        dataset=test_data, predictor=npts, num_samples=10
+    )
 
-print(f"\nentry_count is: {result['entry_count']}")
+    input_data = {
+        "target": np.stack(target_batch),
+        "past_data": np.stack(past_data_batch),
+        "forecast": PrimitiveForecastBatch(list(forecast_it)),
+    }
+
+    # EVALUATE
+    result = evaluate(
+        metrics_to_evaluate, input_data
+    )  # everything is done in one batch
+
+    print("SHAPES OF METRICS:")
+    for metric_name, value in result.items():
+        print(f"{metric_name} has shape {np.shape(value)}")
+
+    # aggregating to "global" metrics has to be done by the user
+    print("\nGLOBAL METRICS:")
+    print(f"Mean MSE: {np.mean(result['mse[mean,axis=0]'])}")
+    print(f"Mean QuantileLoss[0.9]: {np.mean(result['QuantileLoss[0.9]'])}")
+    print("and so on...")
+
+
+# SCENARIO 2: The model works with some batch size. Evaluation should also
+# happen in batches of that size. At the end, the partial results need to
+# be combined.
+def scenario_2():
+    prediction_length = 12
+    data_entry_count = 50
+    eval_batch_size = 16
+
+    npts = NPTSPredictor(prediction_length=prediction_length, freq="D")
+    electricity = get_dataset("electricity")
+    test_data = list(take(data_entry_count, electricity.test))
+
+    # PREPARE FOR EVALUATION
+    metrics_to_evaluate = [
+        MSE(axis=0),
+        AbsError(error_type="p90"),
+        MSE(axis=1),
+        ND(axis=1),
+        *(QuantileLoss(quantile=q) for q in (0.1, 0.5, 0.9)),
+        SeasonalError(freq=electricity.metadata.freq, axis=1),
+    ]
+
+    target_batch = []
+    past_data_batch = []
+    for data_entry in test_data:
+        target_batch.append(data_entry["target"][-prediction_length:])
+        past_data_batch.append(data_entry["target"][:-prediction_length])
+
+    # TODO: not use make_evaluation_predictions to get forecasts
+    forecast_it, _ = make_evaluation_predictions(
+        dataset=test_data, predictor=npts, num_samples=10
+    )
+
+    input_batch_it = get_input_batches(
+        iter(test_data), forecast_it, batch_size=eval_batch_size
+    )
+
+    # EVALUATE
+    result = evaluate_batches(metrics_to_evaluate, input_batch_it)
+
+    print("SHAPES OF METRICS:")
+    for metric_name, value in result.items():
+        print(f"{metric_name} has shape {np.shape(value)}")
+
+    # aggregating to "global" metrics has to be done by the user
+    print("\nGLOBAL METRICS:")
+    print(f"Mean MSE: {np.mean(result['mse[mean,axis=0]'])}")
+    print(f"Mean QuantileLoss[0.9]: {np.mean(result['QuantileLoss[0.9]'])}")
+    print("and so on...")
+
+
+print("SCNEARIO #1 (entire dataset fits into a single np.ndarray):")
+scenario_1()
+print("\n" + "-" * 20)
+print("\nSCNEARIO #2 (evaluation in batches):")
+scenario_2()
 
 """
-RESULT:
+SCNEARIO #1 (entire dataset fits into a single np.ndarray):
+SHAPES OF METRICS:
+mse[mean,axis=0] has shape (12,)
+abs_error[p90] has shape (50, 12)
+mse[mean,axis=1] has shape (50,)
+ND[median,axis=1] has shape (50,)
+QuantileLoss[0.1] has shape (50, 12)
+QuantileLoss[0.5] has shape (50, 12)
+QuantileLoss[0.9] has shape (50, 12)
+season_error[seasonality=24,axis=1] has shape (50,)
+entry_count has shape ()
 
-mse[mean,axis=0] (12,)
-abs_error[p90] (50, 12)
-mse[mean,axis=1] (50,)
-ND[median,axis=1] (50,)
-QuantileLoss[0.1] (50, 12)
-QuantileLoss[0.5] (50, 12)
-QuantileLoss[0.9] (50, 12)
-season_error[seasonality=24,axis=1] (50,)
-entry_count ()
+GLOBAL METRICS:
+Mean MSE: 23.819333333333333
+Mean QuantileLoss[0.9]: 20.984999999999996
+and so on...
 
-entry_count is: 50
+--------------------
+
+SCNEARIO #2 (evaluation in batches):
+SHAPES OF METRICS:
+mse[mean,axis=0] has shape (12,)
+abs_error[p90] has shape (50, 12)
+mse[mean,axis=1] has shape (50,)
+ND[median,axis=1] has shape (50,)
+QuantileLoss[0.1] has shape (50, 12)
+QuantileLoss[0.5] has shape (50, 12)
+QuantileLoss[0.9] has shape (50, 12)
+season_error[seasonality=24,axis=1] has shape (50,)
+entry_count has shape ()
+
+GLOBAL METRICS:
+Mean MSE: 35.6125
+Mean QuantileLoss[0.9]: 20.040166666666664
+and so on...
 """
