Fix numerical bug in `BinnedUniforms` (#2344)
prob.cumsum() - prob is a numerically unsafe way to shift prob.cumsum() to the right. The numerical error will create small gaps or overlaps in the bins defined by lower and upper bounds. If a quantile falls in one of these overlap or gap the one hot bin indicator will have 0 or 2 hot bins and generate a shape mismatch error. The fix consists of doing a shift by copy.

src/gluonts/torch/distributions/binned_uniforms.py
@@ -268,7 +268,8 @@ class BinnedUniforms(Distribution):
         # including the bin (upper)
         incomplete_cdf_upper = bins_prob.cumsum(dim=-1)
         # incomplete_cdf_upper.shape: (*batch_shape, numb_bins)
-        incomplete_cdf_lower = bins_prob.cumsum(dim=-1) - bins_prob
+        incomplete_cdf_lower = torch.zeros_like(incomplete_cdf_upper)
+        incomplete_cdf_lower[..., 1:] = incomplete_cdf_upper[..., :-1]
         # incomplete_cdf_lower.shape: (*batch_shape, numb_bins)
 
         one_hot_bin_indicator = (incomplete_cdf_lower <= quantiles) * (