ce276f1582c68cfb395cd5d4ab649d8e287bf8f2
Nils Kiele <nilsmlte@amazon.de>
2022-10-12 11:30:51 +0200

    Make metric parameters explicit; add classes for base metrics

--- src/gluonts/ev_v5/metrics.py
+++ src/gluonts/ev_v5/metrics.py
@@ -11,35 +11,33 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Optional, Collection, Union
+from typing import Optional
 import numpy as np
-from dataclasses import dataclass, field
 from gluonts.ev_v5.api import Concat, Mean, SimpleMetric, Sum
 
-from gluonts.exceptions import GluonTSUserError
 from gluonts.time_feature import get_seasonality
 
 
-# METRIC FUNCTIONS (non-aggregating)
+# METRIC FUNCTIONS (these are non-aggregating and to be applied batch-wise)
 
 
 def abs_label(data: dict):
     return np.abs(data["label"])
 
 
-def error(data: dict, forecast_type: str = "mean"):
+def error(data: dict, forecast_type: str):
     return data["label"] - data[forecast_type]
 
 
-def abs_error(data: dict, forecast_type: str = "mean"):
+def abs_error(data: dict, forecast_type: str):
     return np.abs(error(data, forecast_type))
 
 
-def squared_error(data: dict, forecast_type: str = "mean"):
+def squared_error(data: dict, forecast_type: str):
     return np.square(error(data, forecast_type))
 
 
-def quantile_loss(data: dict, q: float = 0.5):
+def quantile_loss(data: dict, q: float):
     forecast_type = str(q)
     prediction = data[forecast_type]
 
@@ -48,47 +46,101 @@ def quantile_loss(data: dict, q: float = 0.5):
     )
 
 
-def coverage(data: dict, q: float = 0.5):
+def coverage(data: dict, q: float):
     forecast_type = str(q)
     return data["label"] < data[forecast_type]
 
 
 # METRICS USED IN EVALUATION
-
-# SIMPLE METRICS
-
 class AbsLabel(SimpleMetric):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
+    def __init__(self) -> None:
+        super().__init__()
         self.metric_fn = abs_label
         self.super_aggregate = Concat()
 
 
+class Error(SimpleMetric):
+    def __init__(self, forecast_type: str = "mean") -> None:
+        super().__init__(forecast_type=forecast_type)
+        self.metric_fn = error
+        self.super_aggregate = Concat()
+
+
+class AbsError(SimpleMetric):
+    def __init__(self, forecast_type: str = "mean") -> None:
+        super().__init__(forecast_type=forecast_type)
+        self.metric_fn = abs_error
+        self.super_aggregate = Concat()
+
+
+class SquaredError(SimpleMetric):
+    def __init__(self, forecast_type: str = "mean") -> None:
+        super().__init__(forecast_type=forecast_type)
+        self.metric_fn = squared_error
+        self.super_aggregate = Concat()
+
+
+class QuantileLoss(SimpleMetric):
+    def __init__(self, q: float = 0.5) -> None:
+        super().__init__(q=q)
+        self.metric_fn = squared_error
+        self.super_aggregate = Concat()
+
+
+class Coverage(SimpleMetric):
+    def __init__(self, q: float = 0.5) -> None:
+        super().__init__(q=q)
+        self.metric_fn = coverage
+        self.super_aggregate = Concat()
+
+
 class AbsLabelMean(SimpleMetric):
-    def __init__(self, axis, **kwargs) -> None:
-        super().__init__(**kwargs)
+    def __init__(self, axis: Optional[int] = None) -> None:
+        super().__init__()
         self.metric_fn = abs_label
         self.super_aggregate = Mean(axis=axis)
 
 
+class AbsLabelSum(SimpleMetric):
+    def __init__(self, axis: Optional[int] = None) -> None:
+        super().__init__()
+        self.metric_fn = abs_label
+        self.super_aggregate = Sum(axis=axis)
+
+
+class AbsErrorSum(SimpleMetric):
+    def __init__(
+        self, axis: Optional[int] = None, forecast_type: str = "0.5"
+    ) -> None:
+        super().__init__(forecast_type=forecast_type)
+        self.metric_fn = abs_error
+        self.super_aggregate = Sum(axis=axis)
+
+
 class MSE(SimpleMetric):
-    def __init__(self, axis: Optional[int] = None, **kwargs) -> None:
-        super().__init__(**kwargs)
+    def __init__(
+        self, axis: Optional[int] = None, forecast_type: str = "mean"
+    ) -> None:
+        super().__init__(forecast_type=forecast_type)
         self.metric_fn = squared_error
         self.super_aggregate = Mean(axis=axis)
 
 
 class QuantileLossSum(SimpleMetric):
-    def __init__(self, axis: Optional[int] = None, **kwargs) -> None:
-        super().__init__(**kwargs)
+    def __init__(self, axis: Optional[int] = None, q: float = 0.5) -> None:
+        super().__init__(q=q)
         self.metric_fn = quantile_loss
         self.super_aggregate = Sum(axis=axis)
 
+
 # DERIVED METRICS
 
+
 class RMSE:
-    def __init__(self, axis, **kwargs) -> None:
-        self.mse = MSE(axis=axis, **kwargs)
+    def __init__(
+        self, axis: Optional[int] = None, forecast_type: str = "mean"
+    ) -> None:
+        self.mse = MSE(axis=axis, forecast_type=forecast_type)
 
     def step(self, data):
         self.mse.step(data)
@@ -96,10 +148,12 @@ class RMSE:
     def get(self):
         return np.sqrt(self.mse.get())
 
+
 class NRMSE:
-    def __init__(self, axis:Optional[int], **kwargs) -> None:
-        # TODO: use actual kwargs here 
-        self.rmse = RMSE(axis=axis)
+    def __init__(
+        self, axis: Optional[int] = None, forecast_type: str = "mean"
+    ) -> None:
+        self.rmse = RMSE(axis=axis, forecast_type=forecast_type)
         self.abs_label_mean = AbsLabelMean(axis=axis)
 
     def step(self, data):
