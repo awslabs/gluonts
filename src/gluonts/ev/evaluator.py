1dc9d99b81becd3561ae9c81a4a85b5cadc4ee86
Nils Kiele <nilsmlte@amazon.de>
2022-10-19 10:36:32 +0200

    Add default metrics to 'Evaluator'

---
+++ src/gluonts/ev/evaluator.py
@@ -0,0 +1,114 @@
+from typing import Collection, Iterator, Optional
+
+from gluonts.model.forecast import Forecast
+from gluonts.dataset.split import TestData
+from gluonts.ev.api import DataProbe, Metric, MetricEvaluator, gather_inputs
+from gluonts.ev.helpers import EvalData
+from gluonts.ev.metrics import (
+    MAPE,
+    MASE,
+    MSE,
+    MSIS,
+    ND,
+    NRMSE,
+    RMSE,
+    SMAPE,
+    AbsErrorSum,
+    AbsLabelMean,
+    AbsLabelSum,
+    Coverage,
+    QuantileLoss,
+    SeasonalError,
+    WeightedQuantileLoss,
+)
+
+
+class Evaluator:
+    def __init__(self) -> None:
+        # TODO: better naming!
+        self.metric_evaluators: dict[str, MetricEvaluator] = dict()
+
+    def add_metric(self, metrics: Metric, axis: Optional[int] = None) -> None:
+        self.add_metrics([metrics], axis)
+
+    def add_metrics(
+        self, metrics: Collection[Metric], axis: Optional[int] = None
+    ) -> None:
+        for metric in metrics:
+            metric_evaluator = metric(axis=axis)
+            metric_name = f"{metric.__class__.__name__}[axis={axis}]"
+            self.metric_evaluators[metric_name] = metric_evaluator
+
+    def add_default_metrics(self):
+        metrics_per_entry = [
+            MSE(),
+            AbsErrorSum(),
+            AbsLabelSum(),
+            AbsLabelMean(),
+            SeasonalError(),
+            MASE(),
+            MAPE(),
+            SMAPE(),
+            ND(),
+            MSIS(),
+            *[QuantileLoss(q=q) for q in (0.1, 0.5, 0.9)],
+            *[Coverage(q=q) for q in (0.1, 0.5, 0.9)],
+        ]
+        global_metrics = [
+            MSE(),
+            AbsErrorSum(),
+            AbsLabelSum(),
+            AbsLabelMean(),
+            SeasonalError(),
+            MASE(),
+            MAPE(),
+            SMAPE(),
+            ND(),
+            MSIS(),
+            *[QuantileLoss(q=q) for q in (0.1, 0.5, 0.9)],
+            *[WeightedQuantileLoss(q=q) for q in (0.1, 0.5, 0.9)],
+            *[Coverage(q=q) for q in (0.1, 0.5, 0.9)],
+            RMSE(),
+            NRMSE(),
+        ]
+
+        self.add_metrics(metrics_per_entry, axis=1)
+        self.add_metrics(global_metrics, axis=None)
+
+    def get_required_quantile_levels(self, test_data: TestData):
+        data_probe = DataProbe(test_data)
+        for metric_evaluator in self.metric_evaluators.values():
+            metric_evaluator.reset()
+            metric_evaluator.update(data_probe)
+            metric_evaluator.get()
+        return data_probe.required_quantile_forecasts
+
+    def evaluate(
+        self, test_data: TestData, forecasts: Iterator[Forecast]
+    ) -> EvalData:
+        quantile_levels = self.get_required_quantile_levels(test_data)
+
+        batches = gather_inputs(
+            test_data=test_data,
+            forecasts=forecasts,
+            quantile_levels=quantile_levels,
+        )
+
+        # only NumPy arrays are used from here on
+
+        for metric_evaluator in self.metric_evaluators.values():
+            metric_evaluator.reset()
+
+        b = 0
+        for data in batches:
+            if b == 4:
+                print("UH")
+            print(f"batch {b}")
+            b += 1
+            for metric_evaluator in self.metric_evaluators.values():
+                metric_evaluator.update(data)
+
+        result = dict()
+        for metric_name, metric_evaluator in self.metric_evaluators.items():
+            result[metric_name] = metric_evaluator.get()
+        return result
--- src/gluonts/ev/usage_example.py
+++ src/gluonts/ev/usage_example.py
@@ -11,17 +11,10 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from toolz import take
 
-from gluonts.ev.api import Evaluator
+from toolz import take
+from gluonts.ev.evaluator import Evaluator
 from gluonts.dataset.split import TestTemplate, OffsetSplitter
-from gluonts.ev.metrics import (
-    MSE,
-    MSIS,
-    NRMSE,
-    QuantileLoss,
-    WeightedQuantileLoss,
-)
 from gluonts.model.npts import NPTSPredictor
 from gluonts.dataset.repository.datasets import get_dataset
 
@@ -46,27 +39,9 @@ forecast_it = predictor.predict(dataset=test_data.input, num_samples=100)
 # --- EVALUATION STARTS HERE ---
 
 evaluator = Evaluator()
-evaluator.add_metrics([MSE(), MSIS(), WeightedQuantileLoss()], axis=1)
-evaluator.add_metric(NRMSE())
-evaluator.add_metric(QuantileLoss(q=0.9), axis=0)
+evaluator.add_default_metrics()
 
 res = evaluator.evaluate(test_data, forecast_it)
 
 for name, value in res.items():
     print(f"\n{name}: {value}")
-
-
-"""
-
-MSE[axis=1]: [1.52131758e+02 6.88867542e+01 3.00020833e-01 1.15193784e+03
- 3.15931108e+02 2.63484380e+03 2.99962083e+00 4.66641007e+03
- 2.05939438e+03 4.50878403e+03]
-
-MSIS[axis=1]: [12.08145374  4.83207589  0.62075673  3.42150118  4.37868274  3.2297932
-  3.44080163  3.83119893 10.97729275  5.0347862 ]
-
-NRMSE[axis=None]: 0.14527278126172422
-
-QuantileLossSum[axis=0]: [ 58.3  54.5  33.7  35.   43.1  31.8  31.2  47.1  52.9  55.1  37.7  31.7
-  44.7  60.7 176.7 160.4  54.1  29.5  52.6  53.3  47.2  57.6  62.5 134.5]
-"""
