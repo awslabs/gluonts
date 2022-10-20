fix bugs, add iqf option, add output scaling

src/gluonts/torch/model/mqdnn/estimator.py
@@ -69,7 +69,7 @@ class MQDNNEstimator(PyTorchLightningEstimator):
         prediction_length: int,
         context_length: Optional[int] = None,
         quantiles: Optional[List[float]] = None,
-        scaling: bool = True,
+        is_iqf: bool = False,
         num_past_feat_dynamic_real: int = 0,
         num_feat_dynamic_real: int = 0,
         num_feat_static_real: int = 0,
@@ -110,7 +110,7 @@ class MQDNNEstimator(PyTorchLightningEstimator):
             if quantiles is not None
             else [0.025, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.975]
         )
-        self.scaling = scaling
+        self.is_iqf = is_iqf
         self.num_past_feat_dynamic_real = num_past_feat_dynamic_real
         self.num_feat_dynamic_real = num_feat_dynamic_real
         self.num_feat_static_real = num_feat_static_real
@@ -331,7 +331,9 @@ class MQDNNEstimator(PyTorchLightningEstimator):
             prediction_net=module,
             batch_size=self.batch_size,
             prediction_length=self.prediction_length,
-            forecast_generator=QuantileForecastGenerator(quantiles=self.quantiles),
+            forecast_generator=QuantileForecastGenerator(
+                quantiles=self.quantiles
+            ),
             device=torch.device(
                 "cuda" if torch.cuda.is_available() else "cpu"
             ),
@@ -365,6 +367,7 @@ class MQCNNEstimator(MQDNNEstimator):
             global_activation=self.global_activation,
             local_hidden_sizes=self.local_hidden_sizes,
             local_activation=self.local_activation,
+            is_iqf=self.is_iqf,
         )
 
         return MQDNNLightningModule(

src/gluonts/torch/model/mqdnn/lightning_module.py
@@ -54,11 +54,11 @@ class MQDNNLightningModule(pl.LightningModule):
 
         error = future_target.unsqueeze(1) - output
         pos_error = (error > 0) * error
-        neg_error = torch.logical_not(error <= 0) * error
+        neg_error = (error <= 0) * error
 
         q = self.quantiles.unsqueeze(0).unsqueeze(2)
 
-        loss_values = ((1 - q) * pos_error + q * neg_error).sum(axis=1)
+        loss_values = (q * pos_error + (q - 1) * neg_error).mean(axis=1)
         return weighted_average(loss_values, weights=observed_values)
 
     def training_step(self, batch, batch_idx: int):  # type: ignore

src/gluonts/torch/model/mqdnn/module.py
@@ -15,6 +15,7 @@ from typing import List, Optional
 
 import torch
 import torch.nn as nn
+from torch.nn.functional import softplus
 
 from gluonts.itertools import prod
 from gluonts.torch.modules.scaler import MeanScaler
@@ -35,6 +36,7 @@ class MQDNNModel(nn.Module):
         global_activation: nn.Module = nn.ReLU(),
         local_activation: nn.Module = nn.ReLU(),
         scaling: nn.Module = MeanScaler(dim=1, keepdim=True),
+        is_iqf: bool = False,
     ) -> None:
         super().__init__()
         self.prediction_length = prediction_length
@@ -53,6 +55,7 @@ class MQDNNModel(nn.Module):
         self.local_decoder = self._make_local_decoder()
 
         self.scaling = scaling
+        self.is_iqf = is_iqf
 
     def _make_global_decoder(self):
         input_length = self.encoder_output_length + (
@@ -89,6 +92,7 @@ class MQDNNModel(nn.Module):
         modules.append(
             nn.Linear(dimensions[-1], self.num_output_quantiles),
         )
+        modules.append(Transpose(1, 2))
 
         return nn.Sequential(*modules)
 
@@ -109,8 +113,7 @@ class MQDNNModel(nn.Module):
             past_target, past_observed_values
         )
 
-        # TODO add static features as well
-        # TODO add scale to model input
+        # TODO add static categorical features as well
 
         past_time_length = past_observed_values.shape[1]
 
@@ -147,10 +150,22 @@ class MQDNNModel(nn.Module):
         if future_feat_dynamic is not None:
             local_decoder_inputs.append(future_feat_dynamic)
 
-        local_decoder_output = self.local_decoder(torch.concat(local_decoder_inputs, dim=2))
-        local_decoder_output[:,:,1:] = local_decoder_output[:,:,1:].relu()
+        local_decoder_output = self.local_decoder(
+            torch.concat(local_decoder_inputs, dim=2)
+        )
 
-        return local_decoder_output.cumsum(dim=-1).swapaxes(1, 2)
+        if self.is_iqf:
+            quantiles = torch.concat(
+                (
+                    local_decoder_output[:, 0:1, :],
+                    softplus(local_decoder_output[:, 1:, :]),
+                ),
+                dim=1,
+            ).cumsum(dim=1) * scale.unsqueeze(-1)
+        else:
+            quantiles = local_decoder_output * scale.unsqueeze(-1)
+
+        return quantiles
 
 
 class Transpose(torch.nn.Module):
@@ -205,11 +220,14 @@ class MQCNNModel(MQDNNModel):
             )
         ]
 
-        encoder = torch.nn.Sequential(
-            Transpose(1, 2),
-            *conv_layers,
-            Transpose(1, 2),
-        )
+        encoder_layers = [Transpose(1, 2)]
+        for conv_layer in conv_layers[:-1]:
+            encoder_layers.append(conv_layer)
+            encoder_layers.append(torch.nn.ReLU())
+        encoder_layers.append(conv_layers[-1])
+        encoder_layers.append(Transpose(1, 2))
+
+        encoder = torch.nn.Sequential(*encoder_layers)
 
         super().__init__(
             num_feat_dynamic_real=num_feat_dynamic_real,