8ccf142aa8329899e8c2b5973da760f12aa7bf5d
Pedro Mercado <34275963+melopeo@users.noreply.github.com>
2022-10-28 15:26:29 +0200

    Add hierarchical time series reconciliation methods from R/hts: top-down and middle-out (#2396)

--- src/gluonts/ext/r_forecast/R/hierarchical_forecast_methods.R
+++ src/gluonts/ext/r_forecast/R/hierarchical_forecast_methods.R
@@ -12,3 +12,57 @@ naive_bottom_up <- function(hts, params) {
     )
     aggts(fcasts1.bu)
 }
+
+top_down_w_average_historical_proportions <- function(hts, params) {
+    h <- params$prediction_length
+    fmethod <- params$fmethod
+    fcasts1.td <- forecast(
+      hts,
+      h = h,
+      method="tdgsa",
+      fmethod = fmethod,
+      parallel = TRUE,
+    )
+    aggts(fcasts1.td)
+}
+
+top_down_w_proportions_of_the_historical_averages <- function(hts, params) {
+    h <- params$prediction_length
+    fmethod <- params$fmethod
+    fcasts1.td <- forecast(
+      hts,
+      h = h,
+      method="tdgsf",
+      fmethod = fmethod,
+      parallel = TRUE,
+    )
+    aggts(fcasts1.td)
+}
+
+top_down_w_forecasts_proportions <- function(hts, params) {
+    h <- params$prediction_length
+    fmethod <- params$fmethod
+    fcasts1.td <- forecast(
+      hts,
+      h = h,
+      method="tdfp",
+      fmethod = fmethod,
+      parallel = TRUE,
+    )
+    aggts(fcasts1.td)
+}
+
+middle_out_w_forecasts_proportions <- function(hts, params) {
+    h <- params$prediction_length
+    fmethod <- params$fmethod
+    level <- params$level
+    fcasts1.mo <- forecast(
+      hts,
+      h = h,
+      method="mo",
+      fmethod = fmethod,
+      parallel = TRUE,
+      level = level
+    )
+    aggts(fcasts1.mo)
+}
