6a096c2c927c6da1571d91e557018631c77b14cd
Pedro Eduardo Mercado Lopez <pedroml@amazon.com>
2022-10-28 14:18:56 +0200

    add mint hierarchical model

--- src/gluonts/ext/r_forecast/R/hierarchical_forecast_methods.R
+++ src/gluonts/ext/r_forecast/R/hierarchical_forecast_methods.R
@@ -66,3 +66,53 @@ middle_out_w_forecasts_proportions <- function(hts, params) {
     )
     aggts(fcasts1.mo)
 }
+
+mint <- function(hts, params) {
+    h <- params$prediction_length
+    fmethod <- params$fmethod
+    covariance <- params$covariance
+    nonnegative <- params$nonnegative
+    algorithm <- params$algorithm
+
+    print(c("covariance:", covariance))
+
+    ally <- aggts(hts)
+    n <- nrow(ally)
+    p <- ncol(ally)
+
+    allf <- matrix(NA, nrow = h, ncol = p)
+
+    if (tolower(covariance) != "ols") {
+        res <- matrix(NA, nrow = n, ncol = p)
+    }
+
+    for(i in 1:p)
+    {
+      fit <- if (fmethod == "arima") auto.arima(ally[, i]) else ets(ally[, i])
+      allf[, i] <- forecast(fit, h = h)$mean
+      if (tolower(covariance) != "ols") {
+          res[, i] <- na.omit(ally[, i] - fitted(fit))
+      }
+    }
+
+    if (tolower(covariance) == "ols") {
+        # OLS is a special case of MinT where the covariance matrix is identity.
+        if (is.hts(hts)){
+          y.f_cg <- combinef(allf, get_nodes(hts), weights = NULL,
+                             keep = "all", algorithms = algorithm, nonnegative = nonnegative, parallel = TRUE)
+        } else{
+          y.f_cg <- combinef(allf, groups = get_groups(hts), weights = NULL,
+                             keep = "all", algorithms = algorithm, nonnegative = nonnegative, parallel = TRUE)
+        }
+        return(y.f_cg)  # mean forecasts
+    } else {
+        if (is.hts(hts)){
+          y.f_cg <- MinT(allf, get_nodes(hts), residual = res, covariance = covariance,
+                         keep = "all", algorithms = algorithm, nonnegative = nonnegative, parallel = TRUE)
+        } else{
+          y.f_cg <- MinT(allf, groups = get_groups(hts), residual = res, covariance = covariance,
+                         keep = "all", algorithms = algorithm, nonnegative = nonnegative, parallel = TRUE)
+        }
+        return(y.f_cg)  # mean forecasts
+    }
+}
