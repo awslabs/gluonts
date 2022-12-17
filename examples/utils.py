e0fc73629c9b9220bce50c5897b8cd3203e1bdb8
xiyuanzh <zhangxiyuan@zju.edu.cn>
2022-12-17 01:07:30 -0800

    add TDformer

---
+++ examples/utils.py
@@ -0,0 +1,380 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+import math
+from torch.utils.data import Dataset, DataLoader
+from sklearn.preprocessing import StandardScaler
+import warnings
+
+warnings.filterwarnings('ignore')
+
+from typing import List
+from pandas.tseries import offsets
+from pandas.tseries.frequencies import to_offset
+
+from functools import partial
+
+from scipy.special import eval_legendre
+from sympy import Poly, legendre, Symbol, chebyshevt
+from torch import Tensor
+from typing import List, Tuple
+from functools import partial
+from einops import rearrange, reduce, repeat
+from torch import nn, einsum, diagonal
+from math import log2, ceil, sqrt
+import pdb
+
+class TimeFeature:
+    def __init__(self):
+        pass
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        pass
+
+    def __repr__(self):
+        return self.__class__.__name__ + "()"
+
+
+class SecondOfMinute(TimeFeature):
+    """Minute of hour encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.second / 59.0 - 0.5
+
+
+class MinuteOfHour(TimeFeature):
+    """Minute of hour encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.minute / 59.0 - 0.5
+
+
+class HourOfDay(TimeFeature):
+    """Hour of day encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.hour / 23.0 - 0.5
+
+
+class DayOfWeek(TimeFeature):
+    """Hour of day encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return index.dayofweek / 6.0 - 0.5
+
+
+class DayOfMonth(TimeFeature):
+    """Day of month encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.day - 1) / 30.0 - 0.5
+
+
+class DayOfYear(TimeFeature):
+    """Day of year encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.dayofyear - 1) / 365.0 - 0.5
+
+
+class MonthOfYear(TimeFeature):
+    """Month of year encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.month - 1) / 11.0 - 0.5
+
+
+class WeekOfYear(TimeFeature):
+    """Week of year encoded as value between [-0.5, 0.5]"""
+
+    def __call__(self, index: pd.DatetimeIndex) -> np.ndarray:
+        return (index.isocalendar().week - 1) / 52.0 - 0.5
+
+def time_features_from_frequency_str(freq_str: str) -> List[TimeFeature]:
+    """
+    Returns a list of time features that will be appropriate for the given frequency string.
+    Parameters
+    ----------
+    freq_str
+        Frequency string of the form [multiple][granularity] such as "12H", "5min", "1D" etc.
+    """
+
+    features_by_offsets = {
+        offsets.YearEnd: [],
+        offsets.QuarterEnd: [MonthOfYear],
+        offsets.MonthEnd: [MonthOfYear],
+        offsets.Week: [DayOfMonth, WeekOfYear],
+        offsets.Day: [DayOfWeek, DayOfMonth, DayOfYear],
+        offsets.BusinessDay: [DayOfWeek, DayOfMonth, DayOfYear],
+        offsets.Hour: [HourOfDay, DayOfWeek, DayOfMonth, DayOfYear],
+        offsets.Minute: [
+            MinuteOfHour,
+            HourOfDay,
+            DayOfWeek,
+            DayOfMonth,
+            DayOfYear,
+        ],
+        offsets.Second: [
+            SecondOfMinute,
+            MinuteOfHour,
+            HourOfDay,
+            DayOfWeek,
+            DayOfMonth,
+            DayOfYear,
+        ],
+    }
+
+    offset = to_offset(freq_str)
+
+    for offset_type, feature_classes in features_by_offsets.items():
+        if isinstance(offset, offset_type):
+            return [cls() for cls in feature_classes]
+
+    supported_freq_msg = f"""
+    Unsupported frequency {freq_str}
+    The following frequencies are supported:
+        Y   - yearly
+            alias: A
+        M   - monthly
+        W   - weekly
+        D   - daily
+        B   - business days
+        H   - hourly
+        T   - minutely
+            alias: min
+        S   - secondly
+    """
+    raise RuntimeError(supported_freq_msg)
+
+
+def time_features(dates, freq='h'):
+    return np.vstack([feat(dates) for feat in time_features_from_frequency_str(freq)])
+
+def legendreDer(k, x):
+    def _legendre(k, x):
+        return (2*k+1) * eval_legendre(k, x)
+    out = 0
+    for i in np.arange(k-1,-1,-2):
+        out += _legendre(i, x)
+    return out
+
+
+def phi_(phi_c, x, lb = 0, ub = 1):
+    mask = np.logical_or(x<lb, x>ub) * 1.0
+    return np.polynomial.polynomial.Polynomial(phi_c)(x) * (1-mask)
+
+
+def get_phi_psi(k, base):
+    x = Symbol('x')
+    phi_coeff = np.zeros((k,k))
+    phi_2x_coeff = np.zeros((k,k))
+    if base == 'legendre':
+        for ki in range(k):
+            coeff_ = Poly(legendre(ki, 2*x-1), x).all_coeffs()
+            phi_coeff[ki,:ki+1] = np.flip(np.sqrt(2*ki+1) * np.array(coeff_).astype(np.float64))
+            coeff_ = Poly(legendre(ki, 4*x-1), x).all_coeffs()
+            phi_2x_coeff[ki,:ki+1] = np.flip(np.sqrt(2) * np.sqrt(2*ki+1) * np.array(coeff_).astype(np.float64))
+        
+        psi1_coeff = np.zeros((k, k))
+        psi2_coeff = np.zeros((k, k))
+        for ki in range(k):
+            psi1_coeff[ki,:] = phi_2x_coeff[ki,:]
+            for i in range(k):
+                a = phi_2x_coeff[ki,:ki+1]
+                b = phi_coeff[i, :i+1]
+                prod_ = np.convolve(a, b)
+                prod_[np.abs(prod_)<1e-8] = 0
+                proj_ = (prod_ * 1/(np.arange(len(prod_))+1) * np.power(0.5, 1+np.arange(len(prod_)))).sum()
+                psi1_coeff[ki,:] -= proj_ * phi_coeff[i,:]
+                psi2_coeff[ki,:] -= proj_ * phi_coeff[i,:]
+            for j in range(ki):
+                a = phi_2x_coeff[ki,:ki+1]
+                b = psi1_coeff[j, :]
+                prod_ = np.convolve(a, b)
+                prod_[np.abs(prod_)<1e-8] = 0
+                proj_ = (prod_ * 1/(np.arange(len(prod_))+1) * np.power(0.5, 1+np.arange(len(prod_)))).sum()
+                psi1_coeff[ki,:] -= proj_ * psi1_coeff[j,:]
+                psi2_coeff[ki,:] -= proj_ * psi2_coeff[j,:]
+
+            a = psi1_coeff[ki,:]
+            prod_ = np.convolve(a, a)
+            prod_[np.abs(prod_)<1e-8] = 0
+            norm1 = (prod_ * 1/(np.arange(len(prod_))+1) * np.power(0.5, 1+np.arange(len(prod_)))).sum()
+
+            a = psi2_coeff[ki,:]
+            prod_ = np.convolve(a, a)
+            prod_[np.abs(prod_)<1e-8] = 0
+            norm2 = (prod_ * 1/(np.arange(len(prod_))+1) * (1-np.power(0.5, 1+np.arange(len(prod_))))).sum()
+            norm_ = np.sqrt(norm1 + norm2)
+            psi1_coeff[ki,:] /= norm_
+            psi2_coeff[ki,:] /= norm_
+            psi1_coeff[np.abs(psi1_coeff)<1e-8] = 0
+            psi2_coeff[np.abs(psi2_coeff)<1e-8] = 0
+
+        phi = [np.poly1d(np.flip(phi_coeff[i,:])) for i in range(k)]
+        psi1 = [np.poly1d(np.flip(psi1_coeff[i,:])) for i in range(k)]
+        psi2 = [np.poly1d(np.flip(psi2_coeff[i,:])) for i in range(k)]
+        
+    return phi, psi1, psi2
+
+
+def get_filter(base, k):
+    
+    def psi(psi1, psi2, i, inp):
+        mask = (inp<=0.5) * 1.0
+        return psi1[i](inp) * mask + psi2[i](inp) * (1-mask)
+    
+    if base not in ['legendre', 'chebyshev']:
+        raise Exception('Base not supported')
+    
+    x = Symbol('x')
+    H0 = np.zeros((k,k))
+    H1 = np.zeros((k,k))
+    G0 = np.zeros((k,k))
+    G1 = np.zeros((k,k))
+    PHI0 = np.zeros((k,k))
+    PHI1 = np.zeros((k,k))
+    phi, psi1, psi2 = get_phi_psi(k, base)
+    if base == 'legendre':
+        roots = Poly(legendre(k, 2*x-1)).all_roots()
+        x_m = np.array([rt.evalf(20) for rt in roots]).astype(np.float64)
+        wm = 1/k/legendreDer(k,2*x_m-1)/eval_legendre(k-1,2*x_m-1)
+        
+        for ki in range(k):
+            for kpi in range(k):
+                H0[ki, kpi] = 1/np.sqrt(2) * (wm * phi[ki](x_m/2) * phi[kpi](x_m)).sum()
+                G0[ki, kpi] = 1/np.sqrt(2) * (wm * psi(psi1, psi2, ki, x_m/2) * phi[kpi](x_m)).sum()
+                H1[ki, kpi] = 1/np.sqrt(2) * (wm * phi[ki]((x_m+1)/2) * phi[kpi](x_m)).sum()
+                G1[ki, kpi] = 1/np.sqrt(2) * (wm * psi(psi1, psi2, ki, (x_m+1)/2) * phi[kpi](x_m)).sum()
+                
+        PHI0 = np.eye(k)
+        PHI1 = np.eye(k)
+
+    H0[np.abs(H0)<1e-8] = 0
+    H1[np.abs(H1)<1e-8] = 0
+    G0[np.abs(G0)<1e-8] = 0
+    G1[np.abs(G1)<1e-8] = 0
+        
+    return H0, H1, G0, G1, PHI0, PHI1
+
+
+def RSE(pred, true):
+    return np.sqrt(np.sum((true - pred) ** 2)) / np.sqrt(np.sum((true - true.mean()) ** 2))
+
+
+def CORR(pred, true):
+    u = ((true - true.mean(0)) * (pred - pred.mean(0))).sum(0)
+    d = np.sqrt(((true - true.mean(0)) ** 2 * (pred - pred.mean(0)) ** 2).sum(0))
+    return (u / d).mean(-1)
+
+
+def MAE(pred, true):
+    return np.mean(np.abs(pred - true))
+
+
+def MSE(pred, true):
+    return np.mean((pred - true) ** 2)
+
+
+def RMSE(pred, true):
+    return np.sqrt(MSE(pred, true))
+
+
+def MAPE(pred, true):
+    return np.mean(np.abs((pred - true) / true))
+
+
+def MSPE(pred, true):
+    return np.mean(np.square((pred - true) / true))
+
+
+def metric(pred, true):
+    mae = MAE(pred, true)
+    mse = MSE(pred, true)
+    rmse = RMSE(pred, true)
+    mape = MAPE(pred, true)
+    mspe = MSPE(pred, true)
+
+    return mae, mse, rmse, mape, mspe
+
+def adjust_learning_rate(optimizer, epoch, args):
+    # lr = args.learning_rate * (0.2 ** (epoch // 2))
+    if args.lradj == 'type1':
+        lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
+    elif args.lradj == 'type2':
+        lr_adjust = {
+            2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
+            10: 5e-7, 15: 1e-7, 20: 5e-8
+        }
+    elif args.lradj =='type3':
+        lr_adjust = {epoch: args.learning_rate}
+    elif args.lradj == 'type4':
+        lr_adjust = {epoch: args.learning_rate * (0.9 ** ((epoch - 1) // 1))}
+    if epoch in lr_adjust.keys():
+        lr = lr_adjust[epoch]
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        print('Updating learning rate to {}'.format(lr))
+
+
+class EarlyStopping:
+    def __init__(self, patience=7, verbose=False, des='', delta=0):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.des = des
+
+    def __call__(self, val_loss, model, path='./checkpoints/'):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, path)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, path)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model, path):
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), path + '/' + self.des + 'checkpoint.pth')
+        self.val_loss_min = val_loss
+
+def visual(true, preds=None, context=96, name='./pic/test.pdf'):
+    """
+    Results visualization
+    """
+    plt.figure()
+    plt.plot(true, label='GroundTruth', linewidth=3, c='#ee775e')
+    if preds is not None:
+        plt.plot(np.arange(context, len(preds) + context), preds, label='Prediction', linewidth=3, c='#104c5f')
+    plt.axvline(context, linewidth=3, c='silver', linestyle='--')
+    plt.legend()
+    plt.savefig(name, bbox_inches='tight')
+
+def visual_stl(true, preds, tr_ori, se_ori, tr_pred, se_pred, context=96, name='./pic/test.pdf'):
+    """
+    Results visualization
+    """
+    plt.figure()
+    plt.plot(true, label='GroundTruth', linewidth=3, c='#ee775e')
+    if preds is not None:
+        plt.plot(np.arange(context, len(preds) + context), preds, label='Prediction', linewidth=3, c='#104c5f')
+    plt.plot(np.arange(context), tr_ori, label='Trend', linewidth=3, c='#f4c666')
+    plt.plot(np.arange(context), se_ori, label='Seasonality', linewidth=3, c='#8bb791')
+    plt.plot(np.arange(context, len(tr_pred) + context), tr_pred, linewidth=3, c='#f4c666')
+    plt.plot(np.arange(context, len(se_pred) + context), se_pred, linewidth=3, c='#8bb791')
+    plt.axvline(context, linewidth=3, c='silver', linestyle='--')
+    plt.legend()
+    plt.savefig(name, bbox_inches='tight')
+    
