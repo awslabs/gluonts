bc86e31d9f307b7f9491981a0309a05f20370707
Abdul Fatir Ansari <abdulfatirs@gmail.com>
2023-01-17 19:22:02 +0000

    Fix ruff issue

--- src/gluonts/ext/r_forecast/_predictor.py
+++ src/gluonts/ext/r_forecast/_predictor.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-import os
+
 from pathlib import Path
 from typing import Dict, Optional, List, Iterator, Tuple
 
