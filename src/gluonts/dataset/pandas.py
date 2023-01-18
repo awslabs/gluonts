33f1b674a11e1e81cee03e0572b7b9cbdfb8db90
Lorenzo Stella <stellalo@amazon.de>
2023-01-18 17:15:18 +0100

    fix style

--- src/gluonts/dataset/pandas.py
+++ src/gluonts/dataset/pandas.py
@@ -1,4 +1,17 @@
-from typing import Any, Dict, Union, List, Optional
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from typing import Dict, Union, List, Optional
 from dataclasses import dataclass
 
 import numpy as np
@@ -19,6 +32,7 @@ from gluonts.itertools import SizedIterable
 # - TODO add the "ignore_last_n_targets" in some way (I don't like it)
 # - TODO benchmark
 # - TODO adjust tests, especially add tests for static features
+
 
 @dataclass
 class PandasDataset:
