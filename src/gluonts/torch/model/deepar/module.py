006981008c5ba39dea3ef5487cca4ffd33c08543
Lorenzo Stella <stellalo@amazon.de>
2023-02-06 13:55:49 +0100

    add test and fix bug

--- src/gluonts/torch/model/deepar/module.py
+++ src/gluonts/torch/model/deepar/module.py
@@ -132,6 +132,7 @@ class DeepARModel(nn.Module):
             else [min(50, (cat + 1) // 2) for cat in cardinality]
         )
         self.lags_seq = lags_seq or get_lags_for_frequency(freq_str=freq)
+        self.lags_seq = [l - 1 for l in self.lags_seq]
         self.num_parallel_samples = num_parallel_samples
         self.past_length = self.context_length + max(self.lags_seq)
         self.embedder = FeatureEmbedder(
@@ -205,6 +206,82 @@ class DeepARModel(nn.Module):
     def _past_length(self) -> int:
         return self.context_length + max(self.lags_seq)
 
+    def input_shapes(self, batch_size=1) -> Dict[str, Tuple[int, ...]]:
+        return {
+            "feat_static_cat": (batch_size, self.num_feat_static_cat),
+            "feat_static_real": (batch_size, self.num_feat_static_real),
+            "past_time_feat": (
+                batch_size,
+                self._past_length,
+                self.num_feat_dynamic_real,
+            ),
+            "past_target": (batch_size, self._past_length),
+            "past_observed_values": (batch_size, self._past_length),
+            "future_time_feat": (
+                batch_size,
+                self.prediction_length,
+                self.num_feat_dynamic_real,
+            ),
+        }
+
+    def input_types(self) -> Dict[str, torch.dtype]:
+        return {
+            "feat_static_cat": torch.long,
+            "feat_static_real": torch.float,
+            "past_time_feat": torch.float,
+            "past_target": torch.float,
+            "past_observed_values": torch.float,
+            "future_time_feat": torch.float,
+        }
+
+    def prepare_rnn_input(
+        self,
+        feat_static_cat: torch.Tensor,
+        feat_static_real: torch.Tensor,
+        past_time_feat: torch.Tensor,
+        past_target: torch.Tensor,
+        past_observed_values: torch.Tensor,
+        future_time_feat: torch.Tensor,
+        future_target: Optional[torch.Tensor] = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor,]:
+        context = past_target[..., -self.context_length :]
+        observed_context = past_observed_values[..., -self.context_length :]
+
+        input, _, scale = self.scaler(context, observed_context)
+        future_length = future_time_feat.shape[-2]
+        if future_length > 1:
+            assert future_target is not None
+            input = torch.cat(
+                (input, future_target[..., : future_length - 1] / scale),
+                dim=-1,
+            )
+        prior_input = past_target[..., : -self.context_length] / scale
+
+        lags = lagged_sequence_values(
+            self.lags_seq, prior_input, input, dim=-1
+        )
+
+        time_feat = torch.cat(
+            (
+                past_time_feat[..., -self.context_length + 1 :, :],
+                future_time_feat,
+            ),
+            dim=-2,
+        )
+
+        embedded_cat = self.embedder(feat_static_cat)
+        static_feat = torch.cat(
+            (embedded_cat, feat_static_real, scale.log()),
+            dim=-1,
+        )
+        expanded_static_feat = unsqueeze_expand(
+            static_feat, dim=-2, size=time_feat.shape[-2]
+        )
+
+        features = torch.cat((expanded_static_feat, time_feat), dim=-1)
+
+        return torch.cat((lags, features), dim=-1), scale, static_feat
+
     def unroll_lagged_rnn(
         self,
         feat_static_cat: torch.Tensor,
@@ -212,7 +289,7 @@ class DeepARModel(nn.Module):
         past_time_feat: torch.Tensor,
         past_target: torch.Tensor,
         past_observed_values: torch.Tensor,
-        future_time_feat: Optional[torch.Tensor] = None,
+        future_time_feat: torch.Tensor,
         future_target: Optional[torch.Tensor] = None,
     ) -> Tuple[
         Tuple[torch.Tensor, ...],
@@ -242,7 +319,7 @@ class DeepARModel(nn.Module):
             Tensor of observed values indicators,
             shape: ``(batch_size, past_length)``.
         future_time_feat
-            (Optional) tensor of dynamic real features in the past,
+            Tensor of dynamic real features in the future,
             shape: ``(batch_size, prediction_length, num_feat_dynamic_real)``.
         future_target
             (Optional) tensor of future target values,
@@ -258,43 +335,15 @@ class DeepARModel(nn.Module):
             - Static input to the RNN
             - Output state from the RNN
         """
-        context = past_target[..., -self.context_length :]
-        observed_context = past_observed_values[..., -self.context_length :]
-        _, _, scale = self.scaler(context, observed_context)
-
-        prior_input = past_target[..., : -self.context_length] / scale
-        input = (
-            torch.cat((context, future_target[..., :-1]), dim=-1) / scale
-            if future_target is not None
-            else context / scale
+        rnn_input, scale, static_feat = self.prepare_rnn_input(
+            feat_static_cat,
+            feat_static_real,
+            past_time_feat,
+            past_target,
+            past_observed_values,
+            future_time_feat,
+            future_target,
         )
-
-        embedded_cat = self.embedder(feat_static_cat)
-        static_feat = torch.cat(
-            (embedded_cat, feat_static_real, scale.log()),
-            dim=-1,
-        )
-        expanded_static_feat = unsqueeze_expand(
-            static_feat, dim=-2, size=input.shape[-1]
-        )
-
-        time_feat = (
-            torch.cat(
-                (
-                    past_time_feat[..., -self.context_length + 1 :, :],
-                    future_time_feat,
-                ),
-                dim=-2,
-            )
-            if future_time_feat is not None
-            else past_time_feat[..., -self.context_length + 1 :, :]
-        )
-
-        features = torch.cat((expanded_static_feat, time_feat), dim=-1)
-        lags = lagged_sequence_values(
-            self.lags_seq, prior_input, input, dim=-1
-        )
-        rnn_input = torch.cat((lags, features), dim=-1)
 
         output, new_state = self.rnn(rnn_input)
 
