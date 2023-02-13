81949aabb2b5b93c9371181b03d4245e84e46aa6
Jasper Zschiegner <schjaspe@amazon.de>
2023-02-13 16:07:47 +0100

    Unalter dist forecast.

--- src/gluonts/model/forecast_generator.py
+++ src/gluonts/model/forecast_generator.py
@@ -199,7 +199,7 @@ class DistributionForecastGenerator(ForecastGenerator):
     ) -> Iterator[Forecast]:
         for batch in inference_data_loader:
             inputs = select(input_names, batch, ignore_missing=True)
-            outputs = predict_to_numpy(prediction_net, inputs)
+            outputs = prediction_net(*inputs.values())
 
             if output_transform:
                 log_once(OUTPUT_TRANSFORM_NOT_SUPPORTED_MSG)
