edd11bd0ed2a7555753f86b0f4743ed3c4eeeae6
Lorenzo Stella <stellalo@amazon.com>
2023-02-20 14:53:30 +0100

    Backports v0.12.3 (#2676)
    * Delay instantiation of `ScipyStudentT` object (#2660)
    
    * Faster index building in PandasDataset (#2663)
    
    * Speed up `PandasDataset.from_long_dataframe` (#2665)
    
    * Fix r-forecast doc strings (#2669)
    
    Co-authored-by: Abdul Fatir Ansari <ansarnd@amazon.com>
    Co-authored-by: Pedro Mercado <34275963+melopeo@users.noreply.github.com>
    
    * Fix `DateSplitter` when split date is before start (#2670)
    
    * Remove creation of ragged sequences in MultivariateGrouper (#2671)
    
    Co-authored-by: Abdul Fatir Ansari <ansarnd@amazon.com>
    Co-authored-by: Lorenzo Stella <stellalo@amazon.com>
    
    ---------
    
    Co-authored-by: Gerald Woo <woogerald@yahoo.com.sg>
    Co-authored-by: Huibin Shen <icdishb@gmail.com>
    Co-authored-by: Abdul Fatir <Abdulfatirs@gmail.com>
    Co-authored-by: Abdul Fatir Ansari <ansarnd@amazon.com>
    Co-authored-by: Pedro Mercado <34275963+melopeo@users.noreply.github.com>

--- src/gluonts/dataset/pandas.py
+++ src/gluonts/dataset/pandas.py
@@ -151,7 +151,9 @@ class PandasDataset:
             df = df.to_frame(name=self.target)
 
         if self.timestamp:
-            df.index = pd.PeriodIndex(df[self.timestamp], freq=self.freq)
+            df.index = pd.DatetimeIndex(df[self.timestamp]).to_period(
+                freq=self.freq
+            )
 
         if not isinstance(df.index, pd.PeriodIndex):
             df = df.to_period(freq=self.freq)
@@ -222,6 +224,7 @@ class PandasDataset:
         cls,
         dataframe: pd.DataFrame,
         item_id: str,
+        timestamp: Optional[str] = None,
         static_feature_columns: Optional[list[str]] = None,
         static_features: pd.DataFrame = pd.DataFrame(),
         **kwargs,
@@ -259,6 +262,9 @@ class PandasDataset:
         PandasDataset
             Dataset containing series data from the given long dataframe.
         """
+        if timestamp is not None:
+            dataframe.index = pd.to_datetime(dataframe[timestamp])
+
         if not isinstance(dataframe.index, DatetimeIndexOpsMixin):
             dataframe.index = pd.to_datetime(dataframe.index)
 
