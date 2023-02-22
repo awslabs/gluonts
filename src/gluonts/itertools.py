fefbb7fe73eea3c094e9b37f0d3eecef34f1f2a9
Lorenzo Stella <stellalo@amazon.com>
2023-02-22 10:22:51 +0100

    Add `itertools.power_set` (#2682)
    Co-authored-by: Jasper <jasper.b.schulz@googlemail.com>

--- src/gluonts/itertools.py
+++ src/gluonts/itertools.py
@@ -385,3 +385,17 @@ def pluck_attr(seq, name, default=_no_default):
         return [getattr(el, name) for el in seq]
 
     return [getattr(el, name, default) for el in seq]
+
+
+def power_set(iterable):
+    """
+    Generate all possible subsets of the given iterable, as tuples.
+
+    >>> list(power_set(["a", "b"]))
+    [(), ('a',), ('b',), ('a', 'b')]
+
+    Adapted from https://docs.python.org/3/library/itertools.html#itertools-recipes
+    """
+    return itertools.chain.from_iterable(
+        itertools.combinations(iterable, r) for r in range(len(iterable) + 1)
+    )
