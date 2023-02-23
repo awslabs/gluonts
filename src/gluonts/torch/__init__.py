b6c32cb2a0170b9d8a02ef3ebf47acf307a6a4e1
Jasper Zschiegner <schjaspe@amazon.de>
2023-02-23 14:49:36 +0100

    Fixup.

--- src/gluonts/torch/__init__.py
+++ src/gluonts/torch/__init__.py
@@ -11,12 +11,23 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-# !!! DO NOT MODIFY !!! (pkgutil-style namespace package)
+__all__ = [
+    "PyTorchLightningEstimator",
+    "DeepNPTSEstimator",
+    "DeepAREstimator",
+    "MQF2MultiHorizonEstimator",
+    "SimpleFeedForwardEstimator",
+    "TemporalFusionTransformerEstimator",
+]
 
 
-from pkgutil import extend_path
+from .estimator import PyTorchLightningEstimator
 
-__path__ = extend_path(__path__, __name__)  # type: ignore
+from .model.deep_npts import DeepNPTSEstimator
+from .model.deepar import DeepAREstimator
+from .model.mqf2 import MQF2MultiHorizonEstimator
+from .model.simple_feedforward import SimpleFeedForwardEstimator
+from .model.tft import TemporalFusionTransformerEstimator
 
 
 from . import prelude as _  # noqa
--- src/gluonts/torch/model/__init__.py
+++ src/gluonts/torch/model/__init__.py
@@ -10,22 +10,3 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-
-
-__all__ = [
-    "PyTorchLightningEstimator",
-    "DeepNPTSEstimator",
-    "DeepAREstimator",
-    "MQF2MultiHorizonEstimator",
-    "SimpleFeedForwardEstimator",
-    "TemporalFusionTransformerEstimator",
-]
-
-
-from .estimator import PyTorchLightningEstimator
-
-from .model.deep_npts import DeepNPTSEstimator
-from .model.deepar import DeepAREstimator
-from .model.mqf2 import MQF2MultiHorizonEstimator
-from .model.simple_feedforward import SimpleFeedForwardEstimator
-from .model.tft import TemporalFusionTransformerEstimator
