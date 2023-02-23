1e350450e93ab87193208f345e7283ef8c7fdf95
Jasper <schjaspe@amazon.de>
2023-02-23 15:21:38 +0100

    Export torch models in `torch` module directly. (#2685)

--- src/gluonts/torch/model/__init__.py
+++ src/gluonts/torch/model/__init__.py
@@ -10,9 +10,3 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-
-# !!! DO NOT MODIFY !!! (pkgutil-style namespace package)
-
-from pkgutil import extend_path
-
-__path__ = extend_path(__path__, __name__)  # type: ignore
