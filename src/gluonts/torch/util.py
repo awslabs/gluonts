Fix off-by-one in torch DeepAR (#2618)

src/gluonts/torch/util.py
@@ -120,7 +120,7 @@ def lagged_sequence_values(
     assert max(indices) <= prior_sequence.shape[dim], (
         f"lags cannot go further than prior sequence length, found lag"
         f" {max(indices)} while prior sequence is only"
-        f"{prior_sequence.shape[dim]}-long"
+        f" {prior_sequence.shape[dim]}-long"
     )
 
     full_sequence = torch.cat((prior_sequence, sequence), dim=dim)