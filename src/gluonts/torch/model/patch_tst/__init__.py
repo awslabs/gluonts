138e5d6c5634ca24ee2b0919f4ad22841e2de724
Kashif Rasul <kashif.rasul@gmail.com>
2023-03-22 16:02:54 +0100

    initial patch-TST model

---
+++ src/gluonts/torch/model/patch_tst/__init__.py
@@ -0,0 +1,9 @@
+from .module import PatchTSTModel
+from .lightning_module import PatchTSTLightningModule
+from .estimator import PatchTSTEstimator
+
+__all__ = [
+    "PatchTSTModel",
+    "PatchTSTLightningModule",
+    "PatchTSTEstimator",
+]
---
+++ src/gluonts/torch/model/patch_tst/module.py
@@ -0,0 +1,210 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from typing import List, Tuple, Optional
+
+import numpy as np
+import torch
+from torch import nn
+
+from gluonts.core.component import validated
+from gluonts.model import Input, InputSpec
+from gluonts.torch.distributions import StudentTOutput
+from gluonts.torch.scaler import StdScaler, MeanScaler, NOPScaler
+
+
+def make_linear_layer(dim_in, dim_out):
+    lin = nn.Linear(dim_in, dim_out)
+    torch.nn.init.uniform_(lin.weight, -0.07, 0.07)
+    torch.nn.init.zeros_(lin.bias)
+    return lin
+
+
+class SinusoidalPositionalEmbedding(nn.Embedding):
+    """This module produces sinusoidal positional embeddings of any length."""
+
+    def __init__(self, num_positions: int, embedding_dim: int) -> None:
+        super().__init__(num_positions, embedding_dim)
+        self.weight = self._init_weight(self.weight)
+
+    @staticmethod
+    def _init_weight(out: nn.Parameter) -> nn.Parameter:
+        """
+        Identical to the XLM create_sinusoidal_embeddings except features are not interleaved. The cos features are in
+        the 2nd half of the vector. [dim // 2:]
+        """
+        n_pos, dim = out.shape
+        position_enc = np.array(
+            [
+                [pos / np.power(10000, 2 * (j // 2) / dim) for j in range(dim)]
+                for pos in range(n_pos)
+            ]
+        )
+        # set early to avoid an error in pytorch-1.8+
+        out.requires_grad = False
+        sentinel = dim // 2 if dim % 2 == 0 else (dim // 2) + 1
+        out[:, 0:sentinel] = torch.FloatTensor(np.sin(position_enc[:, 0::2]))
+        out[:, sentinel:] = torch.FloatTensor(np.cos(position_enc[:, 1::2]))
+        out.detach_()
+        return out
+
+    @torch.no_grad()
+    def forward(
+        self, input_ids_shape: torch.Size, past_key_values_length: int = 0
+    ) -> torch.Tensor:
+        """`input_ids_shape` is expected to be [bsz x seqlen x ...]."""
+        _, seq_len = input_ids_shape[:2]
+        positions = torch.arange(
+            past_key_values_length,
+            past_key_values_length + seq_len,
+            dtype=torch.long,
+            device=self.weight.device,
+        )
+        return super().forward(positions)
+
+
+class PatchTSTModel(nn.Module):
+    """
+    Module implementing the PatchTST model for forecasting.
+
+    Parameters
+    ----------
+    prediction_length
+        Number of time points to predict.
+    context_length
+        Number of time steps prior to prediction time that the model.
+    hidden_dimensions
+        Size of hidden layers in the feed-forward network.
+    distr_output
+        Distribution to use to evaluate observations and sample predictions.
+        Default: ``StudentTOutput()``.
+    batch_norm
+        Whether to apply batch normalization. Default: ``False``.
+    """
+
+    @validated()
+    def __init__(
+        self,
+        prediction_length: int,
+        context_length: int,
+        patch_len: int,
+        stride: int,
+        padding_patch: str,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int,
+        dropout: float,
+        activation: str,
+        norm_first: bool,
+        num_encoder_layers: int,
+        scaling: str,
+        distr_output=StudentTOutput(),
+    ) -> None:
+        super().__init__()
+
+        assert prediction_length > 0
+        assert context_length > 0
+
+        self.prediction_length = prediction_length
+        self.context_length = context_length
+        self.patch_len = patch_len
+        self.stride = stride
+        self.d_model = d_model
+        self.padding_patch = padding_patch
+        self.distr_output = distr_output
+
+        if scaling == "mean":
+            self.scaler = MeanScaler(keepdim=True)
+        elif scaling == "std":
+            self.scaler = StdScaler(keepdim=True)
+        else:
+            self.scaler = NOPScaler(keepdim=True)
+
+        self.patch_num = int((context_length - patch_len) / stride + 1)
+        if padding_patch == "end":  # can be modified to general case
+            self.padding_patch_layer = nn.ReplicationPad1d((0, self.stride))
+            self.patch_num += 1
+
+        self.patch_proj = make_linear_layer(patch_len, d_model)
+
+        self.positional_encoding = SinusoidalPositionalEmbedding(
+            self.patch_num, d_model
+        )
+
+        layer_norm_eps: float = 1e-5
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            activation=activation,
+            layer_norm_eps=layer_norm_eps,
+            batch_first=True,
+            norm_first=norm_first,
+        )
+        encoder_norm = nn.LayerNorm(d_model, eps=layer_norm_eps)
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer, num_encoder_layers, encoder_norm
+        )
+
+        self.flatten = nn.Linear(
+            d_model * self.patch_num, prediction_length * d_model
+        )
+
+        self.args_proj = self.distr_output.get_args_proj(d_model)
+
+    def describe_inputs(self, batch_size=1) -> InputSpec:
+        return InputSpec(
+            {
+                "past_target": Input(
+                    shape=(batch_size, self.context_length), dtype=torch.float
+                ),
+                "past_observed_values": Input(
+                    shape=(batch_size, self.context_length), dtype=torch.float
+                ),
+            },
+            torch.zeros,
+        )
+
+    def forward(
+        self,
+        past_target: torch.Tensor,
+        past_observed_values: torch.Tensor,
+    ) -> Tuple[Tuple[torch.Tensor, ...], torch.Tensor, torch.Tensor]:
+        # scale the input
+        past_target_scaled, loc, scale = self.scaler(
+            past_target, past_observed_values
+        )
+
+        # do patching
+        if self.padding_patch == "end":
+            past_target_scaled = self.padding_patch_layer(past_target_scaled)
+        past_target_scaled = past_target_scaled.unfold(
+            dimension=1, size=self.patch_len, step=self.stride
+        )
+
+        # project patches
+        enc_in = self.patch_proj(past_target_scaled)
+        embed_pos = self.positional_encoding(enc_in.size())
+
+        # transformer encoder with positional encoding
+        enc_out = self.encoder(enc_in + embed_pos)
+
+        # flatten and project to prediction length * d_model
+        flatten_out = self.flatten(enc_out.flatten(start_dim=1))
+
+        # project to distribution arguments
+        distr_args = self.args_proj(
+            flatten_out.reshape(-1, self.prediction_length, self.d_model)
+        )
+        return distr_args, loc, scale
