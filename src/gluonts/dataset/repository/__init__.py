2c212cdbe95fad1a2081fbbe1f0bcfdc2798400b
Abdul Fatir Ansari <ansarnd@amazon.com>
2023-04-17 12:35:07 +0200

    Import get_download_path and dataset_recipes as well

--- src/gluonts/dataset/repository/__init__.py
+++ src/gluonts/dataset/repository/__init__.py
@@ -13,8 +13,16 @@
 
 from gluonts.dataset.repository.datasets import (
     get_dataset,
+    get_download_path,
+    dataset_recipes,
     default_dataset_path,
     dataset_names,
 )
 
-__all__ = ["get_dataset", "default_dataset_path", "dataset_names"]
+__all__ = [
+    "get_dataset",
+    "get_download_path",
+    "dataset_recipes",
+    "default_dataset_path",
+    "dataset_names",
+]
