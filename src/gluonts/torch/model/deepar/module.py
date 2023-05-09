6efcdd24d993c36d344e0625762e03fd968ad961
Lorenzo Stella <stellalo@amazon.com>
2023-05-09 09:35:06 +0200

    fix

--- src/gluonts/torch/model/deepar/module.py
+++ src/gluonts/torch/model/deepar/module.py
@@ -236,7 +236,7 @@ class DeepARModel(nn.Module):
 
         time_feat = torch.cat(
             (
-                take_last(past_time_feat, dim=-2, qty=self.context_length - 1),
+                take_last(past_time_feat, dim=-2, num=self.context_length - 1),
                 future_time_feat,
             ),
             dim=-2,
@@ -536,14 +536,14 @@ class DeepARModel(nn.Module):
         else:
             distr = self.output_distribution(params, scale)
             context_target = take_last(
-                past_target, dim=-1, qty=self.context_length - 1
+                past_target, dim=-1, num=self.context_length - 1
             )
             target = torch.cat(
                 (context_target, future_target_reshaped),
                 dim=1,
             )
             context_observed = take_last(
-                past_observed_values, dim=-1, qty=self.context_length - 1
+                past_observed_values, dim=-1, num=self.context_length - 1
             )
             observed_values = torch.cat(
                 (context_observed, future_observed_reshaped), dim=1
