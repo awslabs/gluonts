b78c1dbb18902faece705a4a44d04493479395ff
Jasper Zschiegner <schjaspe@amazon.de>
2023-05-16 15:48:43 +0200

    Simplify logging of forecasts.

--- src/gluonts/model/forecast.py
+++ src/gluonts/model/forecast.py
@@ -283,99 +283,41 @@ class Forecast:
 
     def plot(
         self,
-        prediction_intervals=(50.0, 90.0),
-        show_mean=False,
-        color="b",
-        label=None,
-        output_file=None,
-        *args,
-        **kwargs,
+        ax=None,
+        intervals=(0.5, 0.9),
+        color="blue",
+        name=None,
+        show_label=True,
     ):
-        """
-        Plots the median of the forecast as well as prediction interval bounds
-        (requires matplotlib and pandas).
-
-        Parameters
-        ----------
-        prediction_intervals : float or list of floats in [0, 100]
-            Prediction interval size(s). If a list, it will stack the error
-            plots for each prediction interval. Only relevant for error styles
-            with "ci" in the name.
-        show_mean : boolean
-            Whether to also show the mean of the forecast.
-        color : matplotlib color name or dictionary
-            The color used for plotting the forecast.
-        label : string
-            A label (prefix) that is used for the forecast
-        output_file : str or None, default None
-            Output path for the plot file. If None, plot is not saved to file.
-        args :
-            Other arguments are passed to main plot() call
-        kwargs :
-            Other keyword arguments are passed to main plot() call
-        """
-
-        # matplotlib==2.0.* gives errors in Brazil builds and has to be
-        # imported locally
         import matplotlib.pyplot as plt
 
-        label_prefix = "" if label is None else label + "-"
+        if ax is None:
+            ax = plt.gca()
 
-        for c in prediction_intervals:
-            assert 0.0 <= c <= 100.0
+        ax.plot(
+            self.index.to_timestamp(),
+            self.quantile(0.5),
+            color=color,
+            label=name if show_label else None,
+        )
 
-        ps = [50.0] + [
-            50.0 + f * c / 2.0
-            for c in prediction_intervals
-            for f in [-1.0, +1.0]
-        ]
-        percentiles_sorted = sorted(set(ps))
+        for interval in intervals:
+            if show_label:
+                if name is not None:
+                    label = f"{name}: {interval}"
+                else:
+                    label = interval
+            else:
+                label = None
 
-        def alpha_for_percentile(p):
-            return (p / 100.0) ** 0.3
-
-        ps_data = [self.quantile(p / 100.0) for p in percentiles_sorted]
-        i_p50 = len(percentiles_sorted) // 2
-
-        p50_data = ps_data[i_p50]
-        p50_series = pd.Series(data=p50_data, index=self.index)
-        p50_series.plot(color=color, ls="-", label=f"{label_prefix}median")
-
-        if show_mean:
-            mean_data = np.mean(self._sorted_samples, axis=0)
-            pd.Series(data=mean_data, index=self.index).plot(
-                color=color,
-                ls=":",
-                label=f"{label_prefix}mean",
-                *args,
-                **kwargs,
-            )
-
-        for i in range(len(percentiles_sorted) // 2):
-            ptile = percentiles_sorted[i]
-            alpha = alpha_for_percentile(ptile)
-            plt.fill_between(
-                self.index,
-                ps_data[i],
-                ps_data[-i - 1],
+            ax.fill_between(
+                self.index.to_timestamp(),
+                self.quantile(interval / 2),
+                self.quantile(1 - interval / 2),
+                alpha=0.3 + interval / 3,
                 facecolor=color,
-                alpha=alpha,
-                interpolate=True,
-                *args,
-                **kwargs,
+                label=label,
             )
-            # Hack to create labels for the error intervals. Doesn't actually
-            # plot anything, because we only pass a single data point
-            pd.Series(data=p50_data[:1], index=self.index[:1]).plot(
-                color=color,
-                alpha=alpha,
-                linewidth=10,
-                label=f"{label_prefix}{100 - ptile * 2}%",
-                *args,
-                **kwargs,
-            )
-        if output_file:
-            plt.savefig(output_file)
 
     @property
     def index(self) -> pd.PeriodIndex:
@@ -706,20 +648,3 @@ class QuantileForecast(Forecast):
                 f"info={self.info!r})",
             ]
         )
-
-    def plot(self, label=None, output_file=None, keys=None, *args, **kwargs):
-        import matplotlib.pyplot as plt
-
-        label_prefix = "" if label is None else label + "-"
-
-        if keys is None:
-            keys = self.forecast_keys
-
-        for k, v in zip(keys, self.forecast_array):
-            pd.Series(data=v, index=self.index).plot(
-                label=f"{label_prefix}q{k}",
-                *args,
-                **kwargs,
-            )
-        if output_file:
-            plt.savefig(output_file)
