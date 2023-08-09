a00db3ea7638880d505554f93ffaf1463fdd13c3
Pedro Eduardo Mercado Lopez <pedroml@amazon.com>
2023-08-09 16:01:52 +0200

    clean tests

--- test/mx/model/deepar/test_nonnegative_pred_samples.py
+++ test/mx/model/deepar/test_nonnegative_pred_samples.py
@@ -11,8 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from functools import partial
-
 import numpy as np
 import pytest
 
