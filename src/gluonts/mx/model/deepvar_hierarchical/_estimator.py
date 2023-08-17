6058ccbd7a62a8590fb57ef87dfede2efa177a88
Syama Sundar Rangapuram <r.shyamsundar@gmail.com>
2023-08-17 11:27:46 +0200

    [Hierarchical] Simplify computation of projection matrix using the standard summation matrix (#2964)

--- src/gluonts/mx/model/deepvar_hierarchical/_estimator.py
+++ src/gluonts/mx/model/deepvar_hierarchical/_estimator.py
@@ -73,20 +73,28 @@ def constraint_mat(S: np.ndarray) -> np.ndarray:
     return A
 
 
-def null_space_projection_mat(
-    A: np.ndarray,
+def projection_mat(
+    S: np.ndarray,
     D: Optional[np.ndarray] = None,
 ) -> np.ndarray:
-    """
-    Computes the projection matrix for projecting onto the null space of A.
+    r"""
+    Computes the projection matrix :math: P for projecting base forecasts
+    :math: \bar{y} on to the space of coherent forecasts: :math: P \bar{y}.
+
+    More precisely,
+
+    .. math::
+        P = S (S^T S)^{-1} S^T,      if D is None,\\
+        P = S (S^T D S)^{-1} S^TD,   otherwise.
 
     Parameters
     ----------
-    A
-        The constraint matrix A in the equation: Ay = 0 (y being the
-        values/forecasts of all time series in the hierarchy).
+    S
+        The summation or the aggregation matrix. Shape:
+        (total_num_time_series, num_bottom_time_series)
     D
         Symmetric positive definite matrix (typically a diagonal matrix).
+        Shape: (total_num_time_series, total_num_time_series)
         Optional.
         If provided then the distance between the reconciled and unreconciled
         forecasts is calculated based on the norm induced by D. Useful for
@@ -98,19 +106,15 @@ def null_space_projection_mat(
     Numpy ND array
         Projection matrix, shape (total_num_time_series, total_num_time_series)
     """
-    num_ts = A.shape[1]
     if D is None:
-        return np.eye(num_ts) - A.T @ np.linalg.pinv(A @ A.T) @ A
+        return S @ np.linalg.pinv(S.T @ S) @ S.T
     else:
         assert np.all(D == D.T), "`D` must be symmetric."
         assert np.all(
             np.linalg.eigvals(D) > 0
         ), "`D` must be positive definite."
 
-        D_inv = np.linalg.inv(D)
-        return (
-            np.eye(num_ts) - D_inv @ A.T @ np.linalg.pinv(A @ D_inv @ A.T) @ A
-        )
+        return S @ np.linalg.pinv(S.T @ D @ S) @ S.T @ D
 
 
 class DeepVARHierarchicalEstimator(DeepVAREstimator):
@@ -301,7 +305,7 @@ class DeepVARHierarchicalEstimator(DeepVAREstimator):
         ), "Cannot project only during training (and not during prediction)"
 
         A = constraint_mat(S.astype(self.dtype))
-        M = null_space_projection_mat(A=A, D=D)
+        M = projection_mat(S=S, D=D)
         ctx = self.trainer.ctx
         self.M = mx.nd.array(M, ctx=ctx)
         self.A = mx.nd.array(A, ctx=ctx)
---
+++ test/mx/model/deepvar_hierarchical/test_projection.py
@@ -0,0 +1,103 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from typing import Optional
+
+import numpy as np
+import pytest
+
+from gluonts.mx.model.deepvar_hierarchical._estimator import (
+    constraint_mat,
+    projection_mat,
+)
+
+
+TOL = 1e-12
+
+S = np.array(
+    [
+        [1, 1, 1, 1],
+        [1, 1, 0, 0],
+        [0, 0, 1, 1],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ],
+)
+
+num_bottom_ts = S.shape[1]
+A = constraint_mat(S)
+
+
+def null_space_projection_mat(
+    A: np.ndarray,
+    D: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    """
+    Computes the projection matrix for projecting onto the null space of A.
+
+    Parameters
+    ----------
+    A
+        The constraint matrix A in the equation: Ay = 0 (y being the
+        values/forecasts of all time series in the hierarchy).
+    D
+        Symmetric positive definite matrix (typically a diagonal matrix).
+        Optional.
+        If provided then the distance between the reconciled and unreconciled
+        forecasts is calculated based on the norm induced by D. Useful for
+        weighing the distances differently for each level of the hierarchy.
+        By default Euclidean distance is used.
+
+    Returns
+    -------
+    Numpy ND array
+        Projection matrix, shape (total_num_time_series, total_num_time_series)
+    """
+    num_ts = A.shape[1]
+    if D is None:
+        return np.eye(num_ts) - A.T @ np.linalg.pinv(A @ A.T) @ A
+    else:
+        assert np.all(D == D.T), "`D` must be symmetric."
+        assert np.all(
+            np.linalg.eigvals(D) > 0
+        ), "`D` must be positive definite."
+
+        D_inv = np.linalg.inv(D)
+        return (
+            np.eye(num_ts) - D_inv @ A.T @ np.linalg.pinv(A @ D_inv @ A.T) @ A
+        )
+
+
+@pytest.mark.parametrize(
+    "D",
+    [
+        None,
+        # Root gets the maximum weight and the two aggregated levels get
+        # more weight than the leaf level.
+        np.diag([4, 2, 2, 1, 1, 1, 1]),
+        # Random diagonal matrix
+        np.diag(np.random.rand(S.shape[0])),
+        # Random positive definite matrix
+        np.diag(np.random.rand(S.shape[0]))
+        + np.dot(
+            np.array([[4, 2, 2, 1, 1, 1, 1]]).T,
+            np.array([[4, 2, 2, 1, 1, 1, 1]]),
+        ),
+    ],
+)
+def test_projection_mat(D):
+    p1 = null_space_projection_mat(A=A, D=D)
+    p2 = projection_mat(S=S, D=D)
+    assert (np.abs(p1 - p2)).sum() < TOL
