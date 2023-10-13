bb70cc546c8ca937355a5ffb6473cb802a02f7d7
Oleksandr Shchur <shchuro@amazon.com>
2023-10-13 14:20:13 +0000

    Move from pytorch_lightning to lightning

--- docs/tutorials/advanced_topics/index.md
+++ docs/tutorials/advanced_topics/index.md
@@ -2,7 +2,7 @@
 
 ```{toctree}
 :maxdepth: 1
-howto_pytorch_lightning
+howto_lightning.pytorch
 hp_tuning_with_optuna
 trainer_callbacks
 ```
--- src/gluonts/torch/model/tft/lightning_module.py
+++ src/gluonts/torch/model/tft/lightning_module.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-import pytorch_lightning as pl
+import lightning.pytorch as pl
 import torch
 from gluonts.core.component import validated
 from gluonts.itertools import select
