bbe1eceb7f50c8923f38314d4c9755ce39cb07a5
Lorenzo Stella <stellalo@amazon.com>
2023-10-25 16:08:15 +0200

    fixup

--- src/gluonts/torch/model/lightning_util.py
+++ src/gluonts/torch/model/lightning_util.py
@@ -11,8 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from packaging import version
-
 import lightning.pytorch as pl
 
 
