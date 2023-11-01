Fix edge cases in metric computation (#3037)

src/gluonts/ev/aggregations.py
@@ -50,7 +50,7 @@ class Sum(Aggregation):
     def step(self, values: np.ndarray) -> None:
         assert self.axis is None or isinstance(self.axis, tuple)
 
-        summed_values = np.ma.sum(values, axis=self.axis)
+        summed_values = np.nansum(values, axis=self.axis)
 
         if self.axis is None or 0 in self.axis:
             if self.partial_result is None:
@@ -66,9 +66,11 @@ class Sum(Aggregation):
         assert self.axis is None or isinstance(self.axis, tuple)
 
         if self.axis is None or 0 in self.axis:
-            return np.ma.copy(self.partial_result)
+            assert isinstance(self.partial_result, np.ndarray)
+            return np.copy(self.partial_result)
 
-        return np.ma.concatenate(self.partial_result)
+        assert isinstance(self.partial_result, list)
+        return np.concatenate(self.partial_result)
 
 
 @dataclass
@@ -107,7 +109,7 @@ class Mean(Aggregation):
             if self.partial_result is None:
                 self.partial_result = []
 
-            mean_values = np.ma.mean(values, axis=self.axis)
+            mean_values = np.nanmean(values, axis=self.axis)
             assert isinstance(self.partial_result, list)
             self.partial_result.append(mean_values)
 
@@ -118,4 +120,5 @@ class Mean(Aggregation):
             assert isinstance(self.partial_result, np.ndarray)
             return self.partial_result / self.n
 
-        return np.ma.concatenate(self.partial_result)
+        assert isinstance(self.partial_result, list)
+        return np.concatenate(self.partial_result)