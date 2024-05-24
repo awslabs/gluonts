9d98d4f6b5a6672b35fb340d9a115684631eed86
Oleksandr Shchur <shchuro@amazon.com>
2024-05-24 10:55:31 +0000

    Fix black

--- test/time_feature/test_lag.py
+++ test/time_feature/test_lag.py
@@ -210,7 +210,8 @@ EXPECTED_LAGS = {
     + [335, 336, 337, 503, 504, 505, 671, 672, 673, 719, 720, 721],
     # centered around each of the last 7 days (delta = 1) + last 4 weeks (delta = 1) + last 1 month (delta = 1) +
     #  last 8th and 12th weeks (delta = 0)
-    "6" + H: [
+    "6"
+    + H: [
         1,
         2,
         3,
@@ -241,21 +242,24 @@ EXPECTED_LAGS = {
     + [224, 336],
     # centered around each of the last 7 days (delta = 1) + last 4 weeks (delta = 1) + last 1 month (delta = 1) +
     #  last 8th and 12th weeks (delta = 0) + last year (delta = 1)
-    "12" + H: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    "12"
+    + H: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     + [27, 28, 29, 41, 42, 43, 55, 56, 57]
     + [59, 60, 61]
     + [112, 168]
     + [727, 728, 729],
     # centered around each of the last 7 days (delta = 1) + last 4 weeks (delta = 1) + last 1 month (delta = 1) +
     #  last 8th and 12th weeks (delta = 0) + last 3 years (delta = 1)
-    "23" + H: [1, 2, 3, 4, 5, 6, 7, 8]
+    "23"
+    + H: [1, 2, 3, 4, 5, 6, 7, 8]
     + [13, 14, 15, 20, 21, 22, 28, 29]
     + [30, 31, 32]
     + [58, 87]
     + [378, 379, 380, 758, 759, 760, 1138, 1139, 1140],
     # centered around each of the last 7 days (delta = 1) + last 4 weeks (delta = 1) + last 1 month (delta = 1) +
     #  last 8th and 12th weeks (delta = 0) + last 3 years (delta = 1)
-    "25" + H: [1, 2, 3, 4, 5, 6, 7]
+    "25"
+    + H: [1, 2, 3, 4, 5, 6, 7]
     + [12, 13, 14, 19, 20, 21, 25, 26, 27]
     + [28, 29]
     + [53, 80]
