839907bc29a23d00390c08a8168e1755642d477b
Oleksandr Shchur <shchuro@amazon.com>
2024-05-31 14:27:36 +0000

    Fix parentheses

--- src/gluonts/model/forecast_generator.py
+++ src/gluonts/model/forecast_generator.py
@@ -87,7 +87,7 @@ def make_predictions(prediction_net, inputs: dict):
         # Feed inputs as positional arguments for MXNet predictors
         import mxnet as mx
 
-        if isinstance(prediction_net, (mx.gluon.Block)):
+        if isinstance(prediction_net, mx.gluon.Block):
             return prediction_net(*inputs.values())
     except ImportError:
         pass
