a164967047192bfeeaafcbb138b52db16a63141d
Lorenzo Stella <stellalo@amazon.com>
2025-04-08 15:47:35 +0200

    Backports for v0.16.1 (#3253)
    *Description of changes:* backporting changes
    - #3251 
    - #3252
    - #3254 
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    
    **Please tag this pr with at least one of these labels to make our
    release process faster:** BREAKING, new feature, bug fix, other change,
    dev setup
    
    ---------
    
    Co-authored-by: Oleksandr Shchur <shchuro@amazon.com>

--- src/gluonts/torch/model/tide/module.py
+++ src/gluonts/torch/model/tide/module.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import torch
 from torch import nn
@@ -243,7 +243,7 @@ class TiDEModel(nn.Module):
         num_layers_decoder: int,
         layer_norm: bool,
         distr_output: Output,
-        scaling: str,
+        scaling: Optional[str],
     ) -> None:
         super().__init__()
 
