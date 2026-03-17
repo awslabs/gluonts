482db5fb60a2d41bbbbeaafe0ab5d5e1df439c78
Lorenzo Stella <stellalo@amazon.com>
2026-03-17 10:20:17 +0100

    fixes

--- src/gluonts/dataset/repository/_tsf_reader.py
+++ src/gluonts/dataset/repository/_tsf_reader.py
@@ -22,11 +22,12 @@ from gluonts.exceptions import GluonTSDataError
 
 
 def strtobool(val: str) -> bool:
-    """Convert a string representation of truth to True or False.
+    """
+    Convert a string representation of truth to True or False.
 
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'.
-    False values are 'n', 'no', 'f', 'false', 'off', and '0'.
-    Raises ValueError if 'val' is anything else.
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'. False values are
+    'n', 'no', 'f', 'false', 'off', and '0'. Raises ValueError if 'val' is
+    anything else.
     """
     val = val.lower()
     if val in ("y", "yes", "t", "true", "on", "1"):
